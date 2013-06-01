64c214c6409e77f33c9cbd857603803c6a5f033a
Ethan Lowman <ethan.l.527@gmail.com>
2013-06-01 19:50:28 -0400

    Set up proper directory/project structure

---
+++ intranet/intranet/settings/production.py
@@ -0,0 +1,9 @@
+from .base import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+
+# add production database settings here... ldap will have to be custom written...
+# todo: figure out how to use more than one type of database
+# get secret key from environmental variable
