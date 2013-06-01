1aa3434bdca566ccfabdb88ae2de02dce7e43608
Ethan Lowman <ethan.l.527@gmail.com>
2013-06-01 19:17:56 -0400

    Set up settings module

--- intranet/settings/base.py
+++ intranet/settings/base.py
@@ -1,8 +1,5 @@
 # Django settings for intranet project.
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
@@ -28,7 +25,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
---
+++ intranet/settings/production.py
@@ -0,0 +1,4 @@
+from .base import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
