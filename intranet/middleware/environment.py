347fa0d346d4e2f86eed65441673d5eabd877412
Ethan Lowman <ethan.l.527@gmail.com>
2013-07-19 21:17:10 -0400

    Document SetKerberosCache middleware

--- intranet/middleware/environment.py
+++ intranet/middleware/environment.py
@@ -6,6 +6,23 @@ logger = logging.getLogger(__name__)
 
 
 class SetKerberosCache(object):
+    """Reloads the KRB5CCNAME environmental variable from the session
+    for potential use in future LDAP requests.
+
+    For a login request, the KRB5CCNAME environmental variable has
+    already been set in the authentication backend, but for all other
+    requests, it must be reset from the Kerberos cache stored in a
+    user's session. Otherwise all requests to a a particular Gunicorn
+    worker would use the Kerberos cache of the user who most recently
+    logged in through that worker.
+
+    The environmental variable must be set by middleware so it is
+    available for requests to any view and so each view does not have
+    to load the environmental variable. The LDAP wrapper
+    (intranet.db.ldap_db) cannot set the environmental variable because
+    it does not have access to the current session (request.session).
+
+    """
     def process_request(self, request):
         if "KRB5CCNAME" in request.session and "KRB5CCNAME" not in os.environ:
             logger.debug("Reloading KRB5CCNAME environmental \
