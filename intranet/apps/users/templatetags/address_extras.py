02a25956ce77d8b96df1dccd2f51ef49952ea0e1
Ethan Lowman <ethan.l.527@gmail.com>
2013-07-19 21:33:40 -0400

    Document address template filter

--- intranet/apps/users/templatetags/address_extras.py
+++ intranet/apps/users/templatetags/address_extras.py
@@ -6,6 +6,7 @@ register = template.Library()
 
 @register.filter
 def multiline(address):
+    """Returns the HTML for an address formatted in two lines."""
     result = "{}<br>{}, {} {}".format(address.street, address.city,
                                       address.state, address.postal_code)
     return mark_safe(result)
