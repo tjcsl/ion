b7da8fe964481f486775ec2a25beaf681ea8693d
Ethan Lowman <ethan.l.527@gmail.com>
2013-07-29 20:17:18 -0400

    Add nav and announcements to frontend

--- intranet/settings/local.py
+++ intranet/settings/local.py
@@ -28,12 +28,12 @@ for key in CACHE_AGE:
     CACHE_AGE[key] = 15
 
 
-class glob_list(list):
-    def __contains__(self, key):
-        for elt in self:
-            if fnmatch(key, elt):
-                return True
-        return False
+# class glob_list(list):
+#     def __contains__(self, key):
+#         for elt in self:
+#             if fnmatch(key, elt):
+#                 return True
+#         return False
 
 # INTERNAL_IPS = glob_list([
 #     '127.0.0.1',
