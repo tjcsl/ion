c04859231c44250a971d0be067f36c390545cbb2
Ethan Lowman <ethan.l.527@gmail.com>
2013-08-04 21:26:01 -0400

    Add runserver to fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,11 @@
 from __future__ import with_statement
 from fabric.api import *
 
-def test():
-    print "hi"
+def clean_pyc():
+    local("find . -name '*.pyc' -delete")
+
+def runserver(port=None):
+    if not port:
+        abort("You must specify a port.")
+    clean_pyc()
+    local("./manage.py runserver 0.0.0.0:{}".format(port))
