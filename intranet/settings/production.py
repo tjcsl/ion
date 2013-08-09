eb56ec3524763fb5f8cf64d0251860251c2d140b
Ethan Lowman <ethan.l.527@gmail.com>
2013-08-09 00:53:32 -0400

    Configure Postgres database for production

--- intranet/settings/production.py
+++ intranet/settings/production.py
@@ -1,8 +1,23 @@
+import urlparse
 from .base import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
 
 # get secret key from environmental variable
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 CACHES['default']['OPTIONS']['DB'] = 1
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': url.path[1:],
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname
+    }
+}
