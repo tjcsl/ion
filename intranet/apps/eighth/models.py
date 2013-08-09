97f332a6b2d56ec79a80b2ab96a888f7d0a89435
Ethan Lowman <ethan.l.527@gmail.com>
2013-08-09 11:08:15 -0400

    Add SignupAlert and Absence models

--- intranet/apps/eighth/models.py
+++ intranet/apps/eighth/models.py
@@ -7,11 +7,21 @@ class EighthBlock(models.Model):
     block = models.CharField(null=False, max_length=1)
     locked = models.BooleanField(null=False)
 
+    def __str__(self):
+        return "{}: {}".format(str(self.date), self.block)
+
 
 class EighthActivity(models.Model):
     name = models.CharField(null=False, max_length=63)
     # sponsors = models.ManyToManyField(User)
     members = models.ManyToManyField(User, through="EighthSignup")
+
+    # Groups allowed
+    # Single students allowed
+
+    def __str__(self):
+        return self.name
+
 
 
 class EighthScheduledActivity(models.Model):
@@ -19,12 +29,26 @@ class EighthScheduledActivity(models.Model):
 
 
 class EighthSignup(models.Model):
-    user = models.ForeignKey(User)
-    block = models.ForeignKey(EighthBlock)
-    activity = models.ForeignKey(EighthActivity)
+    user = models.ForeignKey(User, null=False)
+    block = models.ForeignKey(EighthBlock, null=False)
+    activity = models.ForeignKey(EighthActivity, null=False)
 
     class Meta:
         unique_together = (("user", "block"),)
         index_together = [
             ["user", "block"],
         ]
+
+
+class SignupAlert(models.Model):
+    user = models.ForeignKey(User, null=False)
+    night_before = models.BooleanField(null=False)
+    day_of = models.BooleanField(null=False)
+
+
+class EighthAbsence(models.Model):
+    block = models.ForeignKey(EighthBlock)
+    user = models.ForeignKey(User)
+
+    class Meta:
+        unique_together = (("block", "user"),)
