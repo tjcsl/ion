838fef32d3cf6c1f0e1611edfa47c2540e662728
Ethan Lowman <ethan.l.527@gmail.com>
2013-08-09 00:24:01 -0400

    Fix some settings for production

--- intranet/settings/base.py
+++ intranet/settings/base.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ion.tjhsst.edu"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
