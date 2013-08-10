4326e4e88fed6c1c16b32d8260be66c75740218f
James Woglom <2016jwoglom@tjhsst.edu>
2013-08-10 13:42:20 -0400

    add beginnings of basic newspost  model

--- intranet/apps/announcements/models.py
+++ intranet/apps/announcements/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from intranet.apps.users.models import User
 
-# Create your models here.
+class NewsPost(models.Model):
+	"""Represents a news post.
+
+	Attributes:
+		- title -- The title of the news post
+		- content -- The content (in HTML) of the news post
+		- authors -- The :class:`User<intranet.apps.users.models.User>`\
+					objects of the people who submitted the newspost. 
+		- groups -- The :class:`Group<intranet.apps.groups.models.Group>`\
+					the newspost is visible to. Default is all users.
+
+	"""
+	name = models.CharField(null=False, max_length=128)
+	content = models.CharField(max_length=10000)
+	authors = models.ManyToManyField(User)
+	# groups = models.ManyToManyField(Group)
