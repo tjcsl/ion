914e8537dc97d6a6d17c155a7583077210cfb699
Ethan Lowman <ethan.l.527@gmail.com>
2013-08-16 14:32:59 -0400

    Format fuzzy dates with template tag

---
+++ intranet/apps/templatetags/dates.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def fuzzy_date(date):
+    """Formats a :class:`datetime.datetime` object relative to the current time"""
+    diff = datetime.now() - date
+    if diff.seconds <= 60:
+        return "Moments ago"
+    elif diff.seconds <= 60 * 60:
+        return "{} minutes ago".format(diff.minutes)
+    elif diff.seconds <= 60 * 60 * 23:
+        return "{} hours ago".format(diff.hours)
+    elif diff.days == 1:
+        return "Yesterday"
+    elif diff.days < 7:
+        return "{} days ago".format(diff.days)
+    elif diff.days < 14:
+        return date.strftime("Last %A")
+    else:
+        return date.strftime("%A, %B %d, %Y")
