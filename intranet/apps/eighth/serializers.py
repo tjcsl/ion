e356bc8c8daed9ccb0f01e0a9814ff82ac956ed8
Ethan Lowman <ethan.l.527@gmail.com>
2013-08-16 19:33:57 -0400

    Show activity info in block detail

--- intranet/apps/eighth/serializers.py
+++ intranet/apps/eighth/serializers.py
@@ -1,26 +1,6 @@
-from .models import EighthBlock, EighthActivity
 from rest_framework import serializers
-
-
-class EighthBlockDetailSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = EighthBlock
-        fields = ("id",
-                  "url",
-                  "date",
-                  "block",
-                  "locked",
-                  "activities")
-
-class EighthBlockListSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = EighthBlock
-        # Omit activities so people can't kill the database
-        fields = ("id",
-                  "url",
-                  "date",
-                  "block",
-                  "locked")
+from .models import EighthBlock, EighthActivity
+from intranet.apps.users.serializers import UserSerializer
 
 class EighthActivitySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -35,6 +15,29 @@ class EighthActivitySerializer(serializers.HyperlinkedModelSerializer):
                   "both_blocks",
                   "sticky",
                   "special")
+
+class EighthBlockListSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = EighthBlock
+        # Omit activities so people can't kill the database
+        fields = ("id",
+                  "url",
+                  "date",
+                  "block",
+                  "locked")
+
+class EighthBlockDetailSerializer(serializers.HyperlinkedModelSerializer):
+    activities = EighthActivitySerializer()
+    class Meta:
+        model = EighthBlock
+        fields = ("id",
+                  "url",
+                  "date",
+                  "block",
+                  "locked",
+                  "activities")
+
+class EighthSponsorSerializer(models.Model):
 
 
 
