107d69bab95af812c421d2831eea212c4cea3ea9
Ethan Lowman <ethan.l.527@gmail.com>
2013-08-24 14:37:46 -0400

    Add template filter to round numbers

---
+++ intranet/apps/templatetags/numbers.py
@@ -0,0 +1,21 @@
+import logging
+from django import template
+
+register = template.Library()
+logger = logging.getLogger(__name__)
+
+
+@register.filter
+def round(number, precision=0):
+    """Rounds a number to a given precision in decimal digits
+    (default 0 digits) and returns the integer value.
+
+    Precision may be negative. A precision of 1 will round to the tenths
+    place and a precision of -1 will round to the tens place.
+
+    Returns:
+        Float
+
+    """
+
+    return round(number, precision)
