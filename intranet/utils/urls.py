8caf8e23e63773db441b1171b28d15b39873392c
Ethan Lowman <ethan.l.527@gmail.com>
2013-08-24 13:54:04 -0400

    Add util module and function for adding GET parameters to a url

---
+++ intranet/utils/urls.py
@@ -0,0 +1,14 @@
+import urllib
+import urlparse
+
+def add_get_parameters(url, parameters, percent_encode=True):
+    url_parts = list(urlparse.urlparse(url))
+    query = dict(urlparse.parse_qs(url_parts[4]))
+    query.update(parameters)
+
+    if percent_encode:
+        url_parts[4] =urllib.urlencode(query)
+    else:
+        url_parts[4] = "&".join([key + "=" + value for key, value in query.items()])
+
+    return urlparse.urlunparse(url_parts)
