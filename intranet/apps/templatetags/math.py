23c00a9958302dd77a47a176a393ba4eade31acc
Ethan Lowman <ethan.l.527@gmail.com>
2013-08-24 15:15:09 -0400

    Add minimum and maximum filters

--- intranet/apps/templatetags/math.py
+++ intranet/apps/templatetags/math.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 
 @register.filter
-def round_to_places(number, precision=0):
+def round_num(number, precision=0):
     """Rounds a number to a given precision in decimal digits
     (default 0 digits) and returns the integer value.
 
@@ -43,4 +43,18 @@ def multiply(num1, num2):
     """Returns the product of the arguments."""
 
     return num1 * num2
+
+
+@register.filter
+def minimum(num1, num2):
+    """Returns smaller of two numbers."""
+
+    return min(num1, num2)
+
+
+@register.filter
+def maximum(num1, num2):
+    """Returns smaller of two numbers."""
+
+    return max(num1, num2)
 
