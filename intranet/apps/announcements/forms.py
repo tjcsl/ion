463a8e6a4f9145aa2d54195349637cfdf1288b7a
Samuel Damashek <samuel.damashek@gmail.com>
2013-08-27 22:25:48 -0400

    Rewrite announcements views to split them up, fix deleting announcements

--- intranet/apps/announcements/forms.py
+++ intranet/apps/announcements/forms.py
@@ -4,7 +4,6 @@ from .models import Announcement
 
 
 class AnnouncementForm(ModelForm):
-    author = forms.CharField(max_length=63, widget=forms.HiddenInput())
 
     class Meta:
         model = Announcement
