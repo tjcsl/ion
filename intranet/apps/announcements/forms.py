da613f7e71b1c3e00aa79cc3a2458faa7078298a
Ethan Lowman <ethan.l.527@gmail.com>
2014-02-28 14:44:10 -0500

    Start announcements form

--- intranet/apps/announcements/forms.py
+++ intranet/apps/announcements/forms.py
@@ -1,9 +1,7 @@
 from django.forms import ModelForm
-from django import forms
 from .models import Announcement
 
 
 class AnnouncementForm(ModelForm):
-
     class Meta:
         model = Announcement
--- intranet/static/js/dashboard/announcements.js
+++ intranet/static/js/dashboard/announcements.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     // TODO: Move the logic from jQuery to separate functions,
     // e.g. announcements.delete(id)
 
@@ -22,41 +21,6 @@ $(document).ready(function() {
             }
         );
     });
-
-    $(".announcement-add").click(function() {
-        /* TODO: Make the add announcement textbox on this same page */
-        $('.announcement-add-div').slideDown();
-        var today = new Date();
-        $('.announcement-add-today').html(
-            (['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'][today.getDay()]) + ", " +
-            (['January','February','March','April','May','June','July','August','September','October','November','December'][today.getMonth()]) + " " +
-            today.getDate() + ", " + today.getFullYear()
-            );
-        $('.announcement-add-title, .announcement-add-content').click(function() {
-            /*var h = $(this).html(), p = $(this).attr('placeholder');
-            if(trim(h) == trim(p)) {
-                $(this).html('');
-                this.select();
-            } */
-        });
-
-        $('.announcement-add-submit').click(function() {
-            // .announcement-add-title, .announcement-add-author, .announcement-add-content
-            var name = $('.announcement-add-title').html(),
-                author = $('.announcement-add-author').html(),
-                content = $('.announcement-add-content').html();
-
-            $.post('/announcements/add', {
-                'name': name,
-                'author': author,
-                'content': content
-            }, function(d) {
-                $('.announcement-add-div').html('Announcement added.');
-            });
-        })
-        // location.href = "/announcements/add";
-    });
-});
 
 $(function() {
     $('div[data-placeholder]').on('keydown keypress input', function() {
