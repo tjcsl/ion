33ab6d4905299d415d716de32f636876a70991ae
Ethan Lowman <ethan.l.527@gmail.com>
2014-03-03 13:44:41 -0500

    Add search page

--- intranet/apps/search/views.py
+++ intranet/apps/search/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from intranet import settings
 
-# Create your views here.
+
+def search_view(request):
+    q = request.GET.get("q", "").strip()
+    if q:
+        if q.isdigit() and len(q) == settings.FCPS_STUDENT_ID_LENGTH:
+            pass
+
+        return render(request, "search/search_results.html", {"search_query": q})
+    else:
+        return render(request, "search/search_results.html", {"search_query": "No results"})
