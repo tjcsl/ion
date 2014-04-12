709f27b5d24dc1394e7b0af490dd9eb8586da7d9
James Woglom <j@wogloms.net>
2014-04-12 12:12:49 -0400

    unicode

--- intranet/apps/groups/models.py
+++ intranet/apps/groups/models.py
@@ -4,3 +4,6 @@ class Group(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000, blank=True)
     users = models.ManyToManyField('users.User', null=True)
+
+    def __unicode__(self):
+        return "{} ({} users)".format(self.name, len(self.users.all()))
