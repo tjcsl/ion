d0726dcdd7b4abcb5f7a0e02da7cbee8e9c5a35f
James Woglom <j@wogloms.net>
2014-04-12 09:36:53 -0400

    simple groups model

--- intranet/apps/groups/models.py
+++ intranet/apps/groups/models.py
@@ -1,2 +1,6 @@
 from django.db import models
 
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000, blank=True)
+    users = models.ManyToManyField('users.User', null=True)
