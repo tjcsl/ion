ac75286e8f845a0faf20e6c6fdf06e07f3f3412c
James Woglom <j@wogloms.net>
2014-04-12 12:35:57 -0400

    oops didn't actually need that

--- intranet/apps/groups/models.py
+++ intranet/apps/groups/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-
-class Group(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000, blank=True)
-    users = models.ManyToManyField('users.User', null=True)
-
-    def __unicode__(self):
-        return "{} ({} users)".format(self.name, len(self.users.all()))
