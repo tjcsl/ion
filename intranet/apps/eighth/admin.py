0b6bcf56973781bc9b3c08fbedec3ca2b42fa72a
Ethan Lowman <ethan.l.527@gmail.com>
2014-07-06 13:48:23 -0400

    Remove .fuse files

--- intranet/apps/eighth/admin.py
+++ intranet/apps/eighth/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from intranet.apps.eighth.models import *
+
+
 admin.site.register([
     EighthSponsor, EighthRoom, EighthActivity, EighthBlock, EighthScheduledActivity, EighthSignup, SignupAlert, EighthAbsence
 ])
