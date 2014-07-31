deefc0e08dffd8d29a3bc1d45b92590349672f81
Ethan Lowman <ethan.l.527@gmail.com>
2014-07-31 00:25:05 -0400

    Add admin index

--- intranet/apps/eighth/views/routers.py
+++ intranet/apps/eighth/views/routers.py
@@ -1,21 +1,21 @@
 """
-Views that redirect to different places depending on the user that
+Views that render different pages depending on the user that
 requests them.
 """
 
+from django.core.urlresolvers import reverse, resolve
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
 
 
 @login_required
 def eighth_redirect_view(request):
     if request.user.is_student:
         view = "eighth_signup"
-    if request.user.is_eighth_admin:
-        view = "eighth_admin"
+    elif request.user.is_eighth_admin:
+        view = "eighth_admin_index"
     elif request.user.is_teacher or request.user.is_attendance_user:
         view = "eighth_attendance"
     else:
         view = "index"  # should never happen
 
-    return redirect(view)
+    return resolve(reverse(view)).func(request)
