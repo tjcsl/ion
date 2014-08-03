cfb88f1c6562539837b56c7cabacea583a2efd1e
Ethan Lowman <ethan.l.527@gmail.com>
2014-08-03 16:13:50 -0400

    Implement dynamic-link buttons

--- intranet/static/js/eighth/eighth.admin.js
+++ intranet/static/js/eighth/eighth.admin.js
@@ -1,4 +1,20 @@
 $(function() {
-    $("select").selectize({});
+    $("select.selectize").selectize({});
     $("input.datepicker").datepicker();
+
+
+    $(".dynamic-link").each(function() {
+        var $anchor = $(this);
+        var $select = $("#" + $anchor.data("select"));
+        var hrefPattern = $anchor.data("href-pattern");
+
+        var update = function(e) {
+            var href = hrefPattern.replace(window.urlIDPlaceholder, $select.val());
+            $anchor.attr("href", href);
+        }
+
+        $select.on("change", update);
+        update();
+    });
+
 });
