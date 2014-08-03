4323143772ff4801f10c90868c3c1bec99b0d3df
Ethan Lowman <ethan.l.527@gmail.com>
2014-08-03 04:10:59 -0400

    Fix issue with announcements form

--- intranet/apps/announcements/views.py
+++ intranet/apps/announcements/views.py
@@ -1,6 +1,6 @@
 import logging
 from django.contrib import messages
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import render, redirect
 from ..auth.decorators import announcements_admin_required
 from .models import Announcement
@@ -13,10 +13,12 @@ logger = logging.getLogger(__name__)
 def add_announcement_view(request):
     if request.method == "POST":
         form = AnnouncementForm(request.POST)
-        if form is not None and form.is_valid():
+        if form.is_valid():
             form.save()
             messages.success(request, "Successfully added announcement.")
             return redirect("index")
+        else:
+            messages.error("Error adding announcement")
     else:
         form = AnnouncementForm()
     return render(request, "announcements/add_modify.html", {"form": form, "action": "add"})
@@ -27,10 +29,12 @@ def modify_announcement_view(request, id=None):
     if request.method == "POST":
         announcement = Announcement.objects.get(id=id)
         form = AnnouncementForm(request.POST, instance=announcement)
-        if form is not None and form.is_valid():
+        if form.is_valid():
             form.save()
             messages.success(request, "Successfully modified announcement.")
             return redirect("index")
+        else:
+            messages.error("Error adding announcement")
     else:
         announcement = Announcement.objects.get(id=id)
         form = AnnouncementForm(instance=announcement)
@@ -39,11 +43,17 @@ def modify_announcement_view(request, id=None):
 
 @announcements_admin_required
 def delete_announcement_view(request):
-    post_id = None
-    try:
-        post_id = request.POST["id"]
-    except AttributeError:
+    if request.method == "POST":
         post_id = None
-    Announcement.objects.get(id=post_id).delete()
+        try:
+            post_id = request.POST["id"]
+        except AttributeError:
+            post_id = None
 
-    return HttpResponse(status=204)
+        # Silently fail if announcement with given id doesn't exist
+        # by using .filter instead of .get
+        Announcement.objects.filter(id=post_id).delete()
+
+        return HttpResponse(status=204)
+    else:
+        return HttpResponseNotAllowed(["POST"])
