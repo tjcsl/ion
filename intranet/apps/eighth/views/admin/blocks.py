cfc0403fff98b15fed8a80c3c72ec9421a5ef8e5
Ethan Lowman <ethan.l.527@gmail.com>
2014-08-03 17:00:05 -0400

    Implement block form

--- intranet/apps/eighth/views/admin/blocks.py
+++ intranet/apps/eighth/views/admin/blocks.py
@@ -24,16 +24,16 @@ def add_block_view(request):
 
 @eighth_admin_required
 def edit_block_view(request, block_id=None):
-    # activity = EighthActivity.objects.get(id=activity_id)
-    # if request.method == "POST":
-    #     form = ActivityForm(request.POST, instance=activity)
-    #     if form.is_valid():
-    #         form.save()
-    #         messages.success(request, "Successfully edited announcement.")
-    #         return redirect("eighth_admin_dashboard")
-    #     else:
-    #         messages.error(request, "Error adding announcement.")
-    # else:
-    #     form = ActivityForm(instance=activity)
+    block = EighthBlock.objects.get(id=block_id)
+    if request.method == "POST":
+        form = BlockForm(request.POST, instance=block)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Successfully edited block.")
+            return redirect("eighth_admin_dashboard")
+        else:
+            messages.error(request, "Error adding block.")
+    else:
+        form = BlockForm(instance=block)
 
-    return render(request, "eighth/admin/edit_activity.html", {"form": 123})
+    return render(request, "eighth/admin/edit_block.html", {"form": form})
