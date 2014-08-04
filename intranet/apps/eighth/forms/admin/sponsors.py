3b4b56ac77b57da14c1d85619fded3a9448868b8
Ethan Lowman <ethan.l.527@gmail.com>
2014-08-04 01:01:43 -0400

    Add sponsors forms

--- intranet/apps/eighth/forms/admin/sponsors.py
+++ intranet/apps/eighth/forms/admin/sponsors.py
@@ -1,3 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django import forms
+from ...models import EighthSponsor
+
+
+class SponsorForm(forms.ModelForm):
+    class Meta:
+        model = EighthSponsor
+        widgets = {
+            "user": forms.TextInput()
+        }
+        labels = {
+            "user": "User ID:"
+        }
--- intranet/apps/eighth/views/admin/sponsors.py
+++ intranet/apps/eighth/views/admin/sponsors.py
@@ -1,3 +1,47 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import cPickle
+from django import http
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from ....auth.decorators import eighth_admin_required
+from ...forms.admin.sponsors import SponsorForm
+from ...models import EighthSponsor
+
+
+@eighth_admin_required
+def add_sponsor_view(request):
+    if request.method == "POST":
+        form = SponsorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Successfully added sponsor.")
+            return redirect("eighth_admin_dashboard")
+        else:
+            messages.error(request, "Error adding sponsor.")
+            request.session["add_sponsor_form"] = cPickle.dumps(form)
+            return redirect("eighth_admin_dashboard")
+    else:
+        return http.HttpResponseNotAllowed(["POST"], "HTTP 405: METHOD NOT ALLOWED")
+
+
+@eighth_admin_required
+def edit_sponsor_view(request, sponsor_id=None):
+    try:
+        sponsor = EighthSponsor.objects.get(id=sponsor_id)
+    except EighthSponsor.DoesNotExist:
+        return http.HttpResponseNotFound()
+
+    if request.method == "POST":
+        form = SponsorForm(request.POST, instance=sponsor)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Successfully edited sponsor.")
+            return redirect("eighth_admin_dashboard")
+        else:
+            messages.error(request, "Error adding sponsor.")
+    else:
+        form = SponsorForm(instance=sponsor)
+
+    return render(request, "eighth/admin/edit_form.html", {"form": form})
