516e99e349376a31c3443421c984843da609d013
Ethan Lowman <ethan.l.527@gmail.com>
2014-08-06 16:31:34 -0400

    Add set start date util

--- intranet/apps/eighth/utils.py
+++ intranet/apps/eighth/utils.py
@@ -6,3 +6,7 @@ from datetime import datetime
 
 def get_start_date(request):
     return request.session.get("start_date", datetime.now().date())
+
+
+def set_start_date(request, start_date):
+    request.session.set("start_date", start_date)
