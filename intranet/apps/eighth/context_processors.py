ddff9278ceb72666adc9301491d8f521649d315d
Ethan Lowman <ethan.l.527@gmail.com>
2014-08-06 16:30:55 -0400

    Add start date context processor

---
+++ intranet/apps/eighth/context_processors.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from .utils import get_start_date
+
+
+def start_date(request):
+    """Add the start date to the context for eighth admin views
+    """
+
+    if request.user.is_eighth_admin:
+        return {"start_date": get_start_date(request)}
+
+    return {}
