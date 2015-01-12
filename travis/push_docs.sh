4f065b540e79207fa701ab7a34c3056596a28c26
Ethan Lowman <ethan.l.527@gmail.com>
2015-01-11 23:11:49 -0500

    Actualy fix git author issue

--- travis/push_docs.sh
+++ travis/push_docs.sh
@@ -1,6 +1,9 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Pushing docs to Github Pages"
 
+  git config --global user.email "<>"
+  git config --global user.name "Travis"
+
   cp -R docs/build/html $HOME/html_docs
   cd $HOME
   git clone --branch=gh-pages https://${GH_TOKEN}@github.com/tjcsl/ion.git gh-pages
@@ -8,15 +11,6 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   cd gh-pages
   cp -R $HOME/html_docs/* .
   git add -A .
-  git commit --author "Travis <>" -m "Travis build $TRAVIS_BUILD_NUMBER"
-  git push -f origin gh-pages
-  push_status=$?
-
-  if [ $push_status -ne 0 ]; then
-      echo "Pushed docs to Github Pages"
-  else
-      echo "Failed to push docs to Github Pages"
-  fi
-
-  exit $push_status
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
+  git push -f origin gh-pages && echo "Pushed docs to Github Pages" || echo "Failed to push docs to Github Pages"
 fi
