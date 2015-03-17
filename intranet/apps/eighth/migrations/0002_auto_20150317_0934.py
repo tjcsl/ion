1e3e9e510aa6ba8723b359090605375456053064
Ethan Lowman <ethan.l.527@gmail.com>
2015-03-17 09:36:01 -0400

    Use ForeignKey instead of OneToOne for sponsor -> user relation because eighth office uses 7004 for multiple sponsors

---
+++ intranet/apps/eighth/migrations/0002_auto_20150317_0934.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('eighth', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='eighthsponsor',
+            name='user',
+            field=models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True),
+            preserve_default=True,
+        ),
+    ]
