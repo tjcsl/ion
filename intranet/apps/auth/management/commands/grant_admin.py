b7fac523cbbe91a7d215a8e89f80b95780067841
Ethan Lowman <ethan.l.527@gmail.com>
2015-03-24 22:50:15 -0400

    Add management command to grant admin rights

---
+++ intranet/apps/auth/management/commands/grant_admin.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import Group
+from ....eighth.models import User
+
+
+class Command(BaseCommand):
+    args = "<username> <admin_group>"
+    help = "Adds the specified user to the specified admin group"
+
+    def handle(self, *args, **options):
+        if len(args) != 2:
+            raise CommandError("Command expects 2 arguments")
+
+        username, admin_group = args
+        g = Group.objects.get_or_create(name="admin_" + admin_group)[0]
+        User.get_user(username=username).groups.add(g)
