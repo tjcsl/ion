635c6d8bfe7354f96de9c187cacf8257f08a7606
Ethan Lowman <ethan.l.527@gmail.com>
2015-03-25 21:45:00 -0400

    Accept utf-8 html in strip newlines middleware

--- intranet/middleware/templates.py
+++ intranet/middleware/templates.py
@@ -16,7 +16,9 @@ class StripNewlinesMiddleware(object):
 
     def process_response(self, request, response):
         """Process the response and strip extra newlines from HTML."""
-        if response["Content-Type"] == "text/html" and settings.DEBUG:
+        is_html = (response["Content-Type"] == "text/html" or
+                   response["Content-Type"].startswith("text/html;"))
+        if is_html and settings.DEBUG:
             response.content = re.sub(r'\n(\s*)\n', '\n', response.content)
             response.content = re.sub(r'^(\s*)\n', '', response.content)
         return response
