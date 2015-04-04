60923a06639f44b1d817125a66b1be0682a6979b
Peter Foley <pefoley2@pefoley.com>
2015-04-04 16:26:50 -0400

    commit user migration

---
+++ intranet/apps/users/migrations/0002_auto_20150404_1622.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import intranet.apps.users.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelManagers(
+            name='user',
+            managers=[
+                ('objects', intranet.apps.users.models.UserManager()),
+            ],
+        ),
+        migrations.AlterField(
+            model_name='user',
+            name='groups',
+            field=models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups', related_query_name='user', to='auth.Group', related_name='user_set'),
+        ),
+        migrations.AlterField(
+            model_name='user',
+            name='last_login',
+            field=models.DateTimeField(null=True, blank=True, verbose_name='last login'),
+        ),
+    ]
