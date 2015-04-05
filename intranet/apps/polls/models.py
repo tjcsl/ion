4f6cc6d62f03a4d678975d1e761b17968ce60cea
ctrlshiftq <2017sbradbur@tjhsst.edu>
2015-04-05 15:11:58 -0400

    Create polls models

--- intranet/apps/polls/models.py
+++ intranet/apps/polls/models.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=500)
+    question_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=500)
+    votes = models.IntegerField(default=0)
