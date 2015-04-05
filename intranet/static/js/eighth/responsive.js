0ae20dce0988a42e02fa8a76e856d98c79fcddb1
Ethan Lowman <ethan.l.527@gmail.com>
2015-04-05 01:08:41 -0400

    Fix mobile day-picker toggle issue

--- intranet/static/js/eighth/responsive.js
+++ intranet/static/js/eighth/responsive.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
     $("#activity-list li[data-activity-id]").click(function() {
         if(is_small()) {
-            $("#activity-detail").addClass("visible");
+            $("#activity-picker").addClass("visible");
             setTimeout(function() {
                 $("#activity-picker .backbtn").addClass("visible");
             }, 200);
@@ -13,32 +13,10 @@ $(document).ready(function() {
     })
 
     $("#activity-picker .backbtn").click(function() {
-        $("#activity-detail").removeClass("visible");
+        $("#activity-picker").removeClass("visible");
         $("#activity-picker .backbtn").removeClass("visible");
         $("#activity-list li[data-activity-id]").removeClass("selected");
     })
-
-    $(".search-wrapper input").focus(function() {
-        if (is_small()) {
-            $("#activity-list li[data-activity-id]").removeClass("selected");
-        }
-        $("#activity-detail").removeClass("visible");
-        $("#activity-picker .backbtn").removeClass("hidden");
-    })
-
-    eighthResize = function() {
-        var height = document.documentElement.clientHeight;
-        if(height < 650 && height - 90 > 250) {
-            $("#activity-picker").css("height",  height - 90);
-            $(".primary-content").addClass("viewing");
-
-        }
-    }
-    $(document).bind("resize", eighthResize);
-    eighthResize();
-    /*$("html,body").animate({
-        scrollTop: $("#activity-picker").offset().top
-    }, 500);*/
 
     $(".middle .switch").click(function() {
         $(".primary-content").toggleClass("viewing");
