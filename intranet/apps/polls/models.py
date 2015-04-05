520904d4a997962ff5e30903c51e084052610ff6
ctrlshiftq <2017sbradbur@tjhsst.edu>
2015-04-05 18:30:32 -0400

    Update polls models

--- intranet/apps/polls/models.py
+++ intranet/apps/polls/models.py
@@ -3,11 +3,24 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Poll(models.Model):
+    group = models.ForeignKey(Group)
+    question_name = models.CharField(max_length=100)
+    question_info = models.CharField(max_length=500)
+    question_start_time = models.DateTimeField()
+    question_end_time = models.DateTimeField()
+    question_visible = models.BooleanField(default=False)
+
+class Group(models.Model):
+    group_name = models.CharField(max_length=100)
+    group_vote = models.BooleanField(default=True)
+    group_modify = models.BooleanField(default=True)
+    group_view = models.BooleanField(default=True)
+
 class Question(models.Model):
     question_text = models.CharField(max_length=500)
-    question_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
-    choice_text = models.CharField(max_length=500)
+    #choice_info = models.CharField(max_length=500)
     votes = models.IntegerField(default=0)
