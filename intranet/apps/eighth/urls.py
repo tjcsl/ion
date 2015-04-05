87d59c9f4de322f8fdd59230b8673162d7927ab7
Ethan Lowman <ethan.l.527@gmail.com>
2015-04-05 01:33:59 -0400

    Implement delete block (closes #4)

--- intranet/apps/eighth/urls.py
+++ intranet/apps/eighth/urls.py
@@ -33,6 +33,7 @@ eighth_admin_patterns = [
     url(r"^blocks/add$", blocks.add_block_view, name="eighth_admin_add_block"),
     url(r"^blocks/print_rosters/(?P<block_id>\d+)$", general.not_implemented_view, name="eighth_admin_print_block_rosters"),
     url(r"^blocks/edit/(?P<block_id>\d+)$", blocks.edit_block_view, name="eighth_admin_edit_block"),
+    url(r"^blocks/delete/(?P<block_id>\d+)$", blocks.delete_block_view, name="eighth_admin_delete_block"),
 
     # Scheduling
     url(r"^scheduling/schedule$", scheduling.schedule_activity_view, name="eighth_admin_schedule_activity"),
--- intranet/apps/eighth/views/admin/blocks.py
+++ intranet/apps/eighth/views/admin/blocks.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from six.moves import cPickle as pickle
 from django import http
 from django.contrib import messages
+from django.core.urlresolvers import reverse
 from django.shortcuts import redirect, render
 from ....auth.decorators import eighth_admin_required
 from ...forms.admin.blocks import QuickBlockForm, BlockForm
@@ -46,6 +47,30 @@ def edit_block_view(request, block_id):
 
     context = {
         "form": form,
+        "delete_url": reverse("eighth_admin_delete_block",
+                              args=[block_id]),
         "admin_page_title": "Edit Block"
     }
     return render(request, "eighth/admin/edit_form.html", context)
+
+
+@eighth_admin_required
+def delete_block_view(request, block_id):
+    try:
+        block = EighthBlock.objects.get(id=block_id)
+    except EighthBlock.DoesNotExist:
+        raise http.Http404
+
+    if request.method == "POST":
+        block.delete()
+        messages.success(request, "Successfully deleted block.")
+        return redirect("eighth_admin_dashboard")
+    else:
+        context = {
+            "admin_page_title": "Delete Block",
+            "item_name": str(block),
+            "help_text": "Deleting this block will remove all records "
+                         "of it related to eighth period."
+        }
+
+        return render(request, "eighth/admin/delete_form.html", context)
