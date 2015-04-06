dd843a4a498304c4cb9ef5f1e818a2417313fdf7
ctrlshiftq <2017sbradbur@tjhsst.edu>
2015-04-06 00:09:50 -0400

    Update polls models

--- intranet/apps/polls/models.py
+++ intranet/apps/polls/models.py
@@ -3,28 +3,59 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-
-class Group(models.Model):
-    group_name = models.CharField(max_length=100)
-    group_vote = models.BooleanField(default=True)
-    group_modify = models.BooleanField(default=True)
-    group_view = models.BooleanField(default=True)
-
-
 class Poll(models.Model):
     group = models.ForeignKey(Group)
-    question_name = models.CharField(max_length=100)
-    question_info = models.CharField(max_length=500)
-    question_start_time = models.DateTimeField()
-    question_end_time = models.DateTimeField()
-    question_visible = models.BooleanField(default=False)
+    name = models.CharField(max_length=100)
+    info = models.CharField(max_length=500)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    visible = models.BooleanField(default=False)
 
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    vote = models.BooleanField(default=True)
+    modify = models.BooleanField(default=True)
+    view = models.BooleanField(default=True)
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=500)
-
+    poll = models.ForeignKey(Poll)
+    question = models.CharField(max_length=500)
+    STD = 'STD'
+    APP = 'APP'
+    SPLIT_APP = 'SAP'
+    FREE_RESP = 'FRE'
+    SHORT_RESP = 'SRE'
+    STD_OTHER = 'STO'
+    TYPE = (
+        (STD, 'Standard'),
+        (APP, 'Approval'),
+        (SPLIT_APP, 'Split approval'),
+        (FREE_RESP, 'Free response'),
+        (SHORT_RESP, 'Short response'),
+        (STD_OTHER = 'Standard other'),
+    )
+    type = models.CharField(max_length=3, choices=TYPE, default=std)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
-    # choice_info = models.CharField(max_length=500)
-    votes = models.IntegerField(default=0)
+    info = models.CharField(max_length=500)
+    std = models.CharField(max_length=100) #group types?
+    app = models.CharField(max_length=100)
+    #split_app = models.
+    free_resp = models.CharField(max_length=1000)
+    short_resp = models.CharField(max_length=100)
+    std_other = models.CharField(max_length=100)
+
+class Answer(models.Model): #determine field based on question type
+    question = models.ForeignKey(Question)
+    user = models.ForeignKey(User)
+    choice = models.ForeignKey(Choice)
+    std = models.CharField(max_length=100) #group types? get values in final form from choice class?
+    app = models.CharField(max_length=100)
+    #split_app = models.
+    free_resp = models.CharField(max_length=1000)
+    short_resp = models.CharField(max_length=100)
+    std_other = models.CharField(max_length=100)
+    """def __unicode__( #plaintext?
+    )"""
+    #votes = models.IntegerField(default=0)
