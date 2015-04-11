557996bf0c4dc43b999ff8dd7baa1c63a1ab7e7e
James Woglom <2016jwoglom@tjhsst.edu>
2015-04-11 19:34:22 -0400

    users migrations

---
+++ intranet/apps/users/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0002_auto_20150402_1847'),
+        ('users', '0002_auto_20150404_1622'),
+    ]
+
+    operations = [
+    ]
