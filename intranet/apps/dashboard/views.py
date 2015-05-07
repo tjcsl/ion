f9da33eae7ebd41271f4e51ff3fc2de2c9953e95
James Woglom <2016jwoglom@tjhsst.edu>
2015-05-07 19:09:02 -0400

    show current signups in widget

--- intranet/apps/dashboard/views.py
+++ intranet/apps/dashboard/views.py
@@ -5,7 +5,7 @@ import logging
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from ..announcements.models import Announcement
-from ..eighth.models import EighthBlock
+from ..eighth.models import EighthBlock, EighthSignup
 
 logger = logging.getLogger(__name__)
 
@@ -15,13 +15,32 @@ def dashboard_view(request):
     """Process and show the dashboard."""
     announcements = Announcement.objects.order_by("-updated").all()
 
-    blk = EighthBlock.objects.get_first_upcoming_block()
-    if not blk:
-        blk = EighthBlock.objects.all().reverse()[0]
-    blks = blk.get_surrounding_blocks()
-    
+    schedule = []
+
+    block = EighthBlock.objects.get_first_upcoming_block()
+    if block is None:
+        schedule = None
+    else:
+        surrounding_blocks = block.next_blocks()
+        signups = EighthSignup.objects.filter(user=request.user).select_related("scheduled_activity__block", "scheduled_activity__activity")
+        block_signup_map = {s.scheduled_activity.block.id: s.scheduled_activity for s in signups}
+
+        for b in surrounding_blocks:
+            info = {
+                "id": b.id,
+                "block_letter": b.block_letter,
+                "current_signup": getattr(block_signup_map.get(b.id, {}), "activity", None),
+                "current_signup_cancelled": getattr(block_signup_map.get(b.id, {}), "cancelled", False),
+                "locked": b.locked
+            }
+            day = {}
+            day["date"] = b.date
+            day["blocks"] = []
+            day["blocks"].append(info)
+            schedule.append(day)
+        
     context = {
         "announcements": announcements,
-        "blocks": blks
+        "schedule": schedule
     }
     return render(request, "dashboard/dashboard.html", context)
