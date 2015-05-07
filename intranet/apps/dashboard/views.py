a36f571d6292fc68fadfae7d21f574cbed8ee519
James Woglom <2016jwoglom@tjhsst.edu>
2015-05-07 18:58:09 -0400

    show eighth pd blocks on dashboard

--- intranet/apps/dashboard/views.py
+++ intranet/apps/dashboard/views.py
@@ -5,6 +5,7 @@ import logging
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from ..announcements.models import Announcement
+from ..eighth.models import EighthBlock
 
 logger = logging.getLogger(__name__)
 
@@ -12,9 +13,15 @@ logger = logging.getLogger(__name__)
 @login_required
 def dashboard_view(request):
     """Process and show the dashboard."""
-    announcements = Announcement.objects.order_by("-updated").all()[:10]
+    announcements = Announcement.objects.order_by("-updated").all()
 
+    blk = EighthBlock.objects.get_first_upcoming_block()
+    if not blk:
+        blk = EighthBlock.objects.all().reverse()[0]
+    blks = blk.get_surrounding_blocks()
+    
     context = {
-        "announcements": announcements
+        "announcements": announcements,
+        "blocks": blks
     }
     return render(request, "dashboard/dashboard.html", context)
