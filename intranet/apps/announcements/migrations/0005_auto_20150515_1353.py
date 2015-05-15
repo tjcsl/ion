de15330e8d283dc391c39cc56ed0bf5849f6c5ed
James Woglom <2016jwoglom@tjhsst.edu>
2015-05-15 13:54:43 -0400

    announcements migration

---
+++ intranet/apps/announcements/migrations/0005_auto_20150515_1353.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('announcements', '0004_auto_20150512_2010'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='announcement',
+            options={'ordering': ['-added']},
+        ),
+    ]
