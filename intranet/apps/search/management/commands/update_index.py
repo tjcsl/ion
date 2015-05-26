f54f6bb7fb348262b80b354855dc2464f4c1da04
Ethan Lowman <ethan.l.527@gmail.com>
2015-05-26 01:14:14 -0400

    Fix new search fields

--- intranet/apps/search/management/commands/update_index.py
+++ intranet/apps/search/management/commands/update_index.py
@@ -15,22 +15,25 @@ class Command(BaseCommand):
         # Destroy kerberos tickets to force simple auth
         os.system("kdestroy")
 
-        attributes = ["iodineUidNumber",
-                      "iodineUid",
-                      "cn",
-                      "displayName",
-                      "givenName",
-                      "middlename",
-                      "sn",
-                      "nickname",
-                      "gender",
-                      "graduationYear",
-                      "mobile",
-                      "homePhone",
-                      "postalCode",
-                      "l",
-                      "street",
-                      "emails"]
+        attributes = [
+            "iodineUidNumber",
+            "iodineUid",
+            "cn",
+            "displayName",
+            "givenName",
+            "middlename",
+            "sn",
+            "nickname",
+            "gender",
+            "graduationYear",
+            "mobile",
+            "homePhone",
+            "telephoneNumber",
+            "postalCode",
+            "l",
+            "street",
+            "mail"
+        ]
 
         l = ldap_db.LDAPConnection()
         users = l.user_attributes(settings.USER_DN, attributes).results_array()
@@ -38,9 +41,35 @@ class Command(BaseCommand):
         def get_attr(user, a):
             return user[1].get(a, [None])[0]
 
+        def get_attr_list(user, a):
+            return user[1].get(a, [])
+
         self.stdout.write("Indexing all users...")
         i = 0
         for user in users:
+            # Determine some attributes
+            if "displayName" not in user[1]:
+                name = get_attr(user, "cn")
+            else:
+                name = get_attr(user, "displayName")
+
+            if "gender" not in user[1]:
+                sex = None
+            elif user[1]["gender"][0] == "F":
+                sex = "female"
+            else:
+                sex = "male"
+
+            if "graduationYear" in user[1]:
+                grad_year = int(user[1]["graduationYear"][0])
+            else:
+                grad_year = None
+
+            phones = (get_attr_list(user, "homePhone") +
+                      get_attr_list(user, "mobile") +
+                      get_attr_list(user, "telephoneNumber"))
+
+            # Index the user
             es = elasticsearch.Elasticsearch()
             es.index(
                 index=settings.ELASTICSEARCH_INDEX,
@@ -49,32 +78,42 @@ class Command(BaseCommand):
                 body={
                     "ion_id": get_attr(user, "iodineUidNumber"),
                     "id": get_attr(user, "iodineUidNumber"),
+
                     "ion_username": get_attr(user, "iodineUid"),
                     "username": get_attr(user, "iodineUid"),
-                    "name": get_attr(user, "cn") if "displayName" not in user[1] else get_attr(user, "displayName"),
+
+                    "name": name,
                     "common_name": get_attr(user, "cn"),
                     "display_name": get_attr(user, "displayName"),
+
+                    "first": get_attr(user, "givenName"),
                     "firstname": get_attr(user, "givenName"),
-                    "fname": get_attr(user, "givenName"),
                     "first_name": get_attr(user, "givenName"),
+
+                    "middle": get_attr(user, "middlename"),
                     "middlename": get_attr(user, "middlename"),
-                    "mname": get_attr(user, "middlename"),
                     "middle_name": get_attr(user, "middlename"),
+
+                    "last": get_attr(user, "sn"),
                     "lastname": get_attr(user, "sn"),
-                    "lname": get_attr(user, "sn"),
                     "last_name": get_attr(user, "sn"),
+
                     "nickname": get_attr(user, "nickname"),
                     "nick": get_attr(user, "nickname"),
-                    "sex": None if "gender" not in user[1] else "female" if user[1]["gender"][0] == "F" else "male",
-                    "gender": None if "gender" not in user[1] else "female" if user[1]["gender"][0] == "F" else "male",
-                    "graduation_year": int(user[1]["graduationYear"][0]) if "graduationYear" in user[1] else None,
-                    "gradyear": int(user[1]["graduationYear"][0]) if "graduationYear" in user[1] else None,
-                    "phone": get_attr(user, "mobile"),
-                    "homephone": get_attr(user, "homePhone"),
+
+                    "sex": sex,
+
+                    "gradyear": grad_year,
+                    "graduation_year": grad_year,
+
+                    "phone": phones,
+                    "telephone": phones,
+
                     "zip": get_attr(user, "postalCode"),
                     "city": get_attr(user, "l"),
                     "address": get_attr(user, "street"),
-                    "email": None if user[1].get("emails", None) is None else " ".join(user[1].get("emails", [None]))
+
+                    "email": get_attr_list(user, "mail")
                 }
             )
             i += 1
