fe9868322f4d6c21a9534ad7d301db14a89bf8f8
Ethan Lowman <ethan.l.527@gmail.com>
2015-05-25 22:51:14 -0400

    Merge migrations

---
+++ intranet/apps/announcements/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('announcements', '0005_auto_20150515_1355'),
+        ('announcements', '0005_auto_20150515_1353'),
+    ]
+
+    operations = [
+    ]
