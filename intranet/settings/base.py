e26e873263e126abcd19a6c8c585a422f23ac340
Ethan Lowman <ethan.l.527@gmail.com>
2015-05-28 00:41:19 -0400

    Fix Unicode bug in version display. Let's test it ∆˚ß¬ß∆ß˚¬

--- intranet/settings/base.py
+++ intranet/settings/base.py
@@ -302,7 +302,7 @@ def _get_current_commit_long_hash():
 
 def _get_current_commit_info():
     cmd = "git show -s --format=medium HEAD"
-    lines = subprocess.check_output(cmd, shell=True).decode().splitlines()
+    lines = subprocess.check_output(cmd, shell=True).splitlines()
     return "\n".join([lines[0][:14].capitalize(), lines[2][8:]]).replace("   ", " ")
 
 
