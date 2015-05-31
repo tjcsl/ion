592e016631ef1d8bf05f0abf96698725bdfba238
James Woglom <2016jwoglom@tjhsst.edu>
2015-05-30 20:49:58 -0400

    add eighth display on eighth profile, styling

--- intranet/apps/eighth/views/profile.py
+++ intranet/apps/eighth/views/profile.py
@@ -6,20 +6,34 @@ from django.http import Http404
 from django.shortcuts import redirect, render
 from ...auth.decorators import eighth_admin_required
 from ...users.models import User
-
+from ..models import EighthBlock, EighthSignup
 logger = logging.getLogger(__name__)
 
 @eighth_admin_required
 def profile_view(request, user_id=None):
     if user_id:
         try:
-            user = User.objects.get(id=user_id)
+            profile_user = User.objects.get(id=user_id)
         except User.DoesNotExist:
             raise Http404
     else:
-        user = request.user
+        profile_user = request.user
+
+    eighth_schedule = []
+    start_block = EighthBlock.objects.get_first_upcoming_block()
+    blocks = [start_block] + list(start_block.next_blocks(5))
+
+    for block in blocks:
+        sch = {}
+        sch["block"] = block
+        try:
+            sch["signup"] = EighthSignup.objects.get(scheduled_activity__block=block, user=profile_user)
+        except EighthSignup.DoesNotExist:
+            sch["signup"] = None
+        eighth_schedule.append(sch)
 
     context = {
-        "profile_user": user
+        "profile_user": profile_user,
+        "eighth_schedule": eighth_schedule
     }
     return render(request, "eighth/profile.html", context)
