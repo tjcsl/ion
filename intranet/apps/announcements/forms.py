4d64d91fc4cbd276496fb3e7ed286021509d79d8
James Woglom <2016jwoglom@tjhsst.edu>
2015-06-03 13:54:14 -0400

    add help text

--- intranet/apps/announcements/forms.py
+++ intranet/apps/announcements/forms.py
@@ -18,6 +18,11 @@ class AnnouncementForm(forms.ModelForm):
 
 class AnnouncementRequestForm(forms.ModelForm):
 
+    content = forms.CharField(help_text="The text you wish to have in the announcement.", widget=forms.Textarea)
+    notes = forms.CharField(help_text="Any information about this announcement you wish to "\
+                                      "share with the Intranet administrators. You should include information "\
+                                      "regarding group restrictions here.", widget=forms.Textarea)
+    #teachers_requested = forms.MultipleChoiceField(help_text="The faculty member(s) who will approve the announcement.")
     class Meta:
         model = AnnouncementRequest
         fields = [
