923e09bd8276117230af4e6fc305d241454ae44f
James Woglom <2016jwoglom@tjhsst.edu>
2015-06-03 13:59:26 -0400

    filter teachers in request form

--- intranet/apps/announcements/forms.py
+++ intranet/apps/announcements/forms.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django import forms
 from .models import Announcement, AnnouncementRequest
-
+from ..users.models import User
 
 class AnnouncementForm(forms.ModelForm):
 
@@ -22,7 +22,13 @@ class AnnouncementRequestForm(forms.ModelForm):
     notes = forms.CharField(help_text="Any information about this announcement you wish to "\
                                       "share with the Intranet administrators. You should include information "\
                                       "regarding group restrictions here.", widget=forms.Textarea)
-    #teachers_requested = forms.MultipleChoiceField(help_text="The faculty member(s) who will approve the announcement.")
+    
+    def __init__(self, *args, **kwargs):
+        super(AnnouncementRequestForm, self).__init__(*args, **kwargs)
+
+        self.fields["teachers_requested"].queryset = (User.objects
+                                                          .exclude(username__startswith="2"))
+
     class Meta:
         model = AnnouncementRequest
         fields = [
