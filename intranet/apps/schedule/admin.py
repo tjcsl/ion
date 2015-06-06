43ac3052278efb3b026f91da8b72ae0a91b46cf9
Ethan Lowman <ethan.l.527@gmail.com>
2015-06-06 12:04:50 -0400

    Add time to admin

--- intranet/apps/schedule/admin.py
+++ intranet/apps/schedule/admin.py
@@ -2,9 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Day, Block, DayType, CodeName
+from .models import Day, Block, DayType, CodeName, Time
 
 
 admin.site.register([
-    Day, Block, DayType, CodeName
+    Day, Block, DayType, CodeName, Time
 ])
