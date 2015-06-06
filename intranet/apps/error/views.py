de27359a4d12020a1be3dd9935fb2ce6d6c13971
James Woglom <2016jwoglom@tjhsst.edu>
2015-06-06 15:05:17 -0400

    actually throw HTTP 404 and 500

--- intranet/apps/error/views.py
+++ intranet/apps/error/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 
 def handle_404_view(request):
-    return render(request, "error/404.html")
+    return render(request, "error/404.html", status=404)
 
 
 def handle_500_view(request):
-    return render(request, "error/500.html")
+    return render(request, "error/500.html", status=500)
