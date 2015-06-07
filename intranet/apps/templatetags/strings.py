005d66d8d7215beedb3850f77ae8a89ab1023a18
Ethan Lowman <ethan.l.527@gmail.com>
2015-06-07 14:45:38 -0400

    Improve display of blocks names with digits

---
+++ intranet/apps/templatetags/strings.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import logging
+from django import template
+
+register = template.Library()
+logger = logging.getLogger(__name__)
+
+
+@register.filter
+def contains_digit(s):
+    return any(c.isdigit() for c in s)
