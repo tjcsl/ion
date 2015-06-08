1c04ecf72be7929e5e155a1cc856f65179541de0
James Woglom <2016jwoglom@tjhsst.edu>
2015-06-07 23:14:00 -0400

    use textarea

--- intranet/apps/feedback/forms.py
+++ intranet/apps/feedback/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class FeedbackForm(forms.Form):
     """A form for sending comments to the Intranet team."""
-    comments = forms.CharField(label='Comments', max_length=50000)
+    comments = forms.CharField(label='Comments', max_length=50000, widget=forms.Textarea)
