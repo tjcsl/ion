0f033f61976de1684a112bca0e13dfab1610c421
Samuel Damashek <samuel.damashek@gmail.com>
2015-06-09 23:14:36 -0400

    Permissions for announcements API

--- intranet/apps/announcements/api.py
+++ intranet/apps/announcements/api.py
@@ -2,16 +2,32 @@
 from __future__ import unicode_literals
 import logging
 
-from rest_framework import generics, views, status
-from .models import Announcement
+from rest_framework import generics, views, status, permissions
+from .models import Announcement, AnnouncementManager
 from .serializers import AnnouncementSerializer
 
 logger = logging.getLogger(__name__)
 
+class IsAnnouncementAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated() and
+            (request.method in permissions.SAFE_METHODS or
+            request.user and
+            request.user.is_announcements_admin))
+
 class ListCreateAnnouncement(generics.ListCreateAPIView):
-    queryset = Announcement.objects.prefetch_related("groups")
     serializer_class = AnnouncementSerializer
+    permission_classes = (IsAnnouncementAdminOrReadOnly,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return Announcement.objects.visible_to_user(user).prefetch_related("groups")
 
 class RetrieveUpdateDestroyAnnouncement(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Announcement.objects.prefetch_related("groups")
     serializer_class = AnnouncementSerializer
+    permission_classes = (IsAnnouncementAdminOrReadOnly,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return Announcement.objects.visible_to_user(user).prefetch_related("groups")
