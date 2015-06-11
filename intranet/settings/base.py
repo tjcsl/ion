895c52491f94f956fc657bb4db79278438f7f880
Samuel Damashek <samuel.damashek@gmail.com>
2015-06-10 22:58:55 -0400

    Use HTTP basic auth, and don't allow rest login

--- intranet/settings/base.py
+++ intranet/settings/base.py
@@ -222,7 +222,12 @@ REST_FRAMEWORK = {
 
     "EXCEPTION_HANDLER": "intranet.apps.api.utils.custom_exception_handler",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 50
+    "PAGE_SIZE": 50,
+
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "intranet.apps.api.authentication.KerberosBasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication"
+    )
 }
 
 INSTALLED_APPS = (
