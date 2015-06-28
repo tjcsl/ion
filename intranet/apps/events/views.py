819892fd1314fb9fb83ec51b40bb1ef2f5a6ae41
James Woglom <2016jwoglom@tjhsst.edu>
2015-06-28 12:19:07 -0400

    this week/month

--- intranet/apps/events/views.py
+++ intranet/apps/events/views.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 import logging
+import datetime
+from calendar import monthrange
 from .models import Event
 from .forms import EventForm
 from django.contrib import messages
@@ -12,6 +14,18 @@ from django.utils import timezone
 logger = logging.getLogger(__name__)
 
 
+def get_week_days(today=None):
+    if today is None:
+        today = datetime.date.today()
+    start_delta = datetime.timedelta(days=today.weekday())
+    return [today - start_delta + datetime.timedelta(days=i) for i in range(7)]
+
+def get_month_days(today=None):
+    if today is None:
+        today = datetime.date.today()
+    start_delta = datetime.timedelta(days=today.day-1)
+    return [today - start_delta + datetime.timedelta(days=i) for i in range(monthrange(today.year, today.month)[1])]
+
 @login_required
 def events_view(request):
 
@@ -19,8 +33,13 @@ def events_view(request):
                             .visible_to_user(request.user)
                             .prefetch_related("groups"))
 
+    # get date objects for week and month
+    this_week = get_week_days()
+    this_month = get_month_days()
+
     context = {
-        "events": viewable_events
+        "this_week": viewable_events.filter(time__lt=this_week[-1], time__gt=this_week[0]),
+        "this_month": viewable_events.filter(time__lt=this_month[-1], time__gt=this_week[-1] + datetime.timedelta(days=1))
     }
     return render(request, "events/home.html", context)
 
