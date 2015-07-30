7f85e5a6078b7f7c565323fce76b5c3c0c119f13
James Woglom <2016jwoglom@tjhsst.edu>
2015-07-29 23:06:42 -0400

    add GCMNotification model

--- intranet/apps/notifications/models.py
+++ intranet/apps/notifications/models.py
@@ -12,3 +12,12 @@ class NotificationConfig(models.Model):
 
     def __unicode__(self):
         return "{}".format(self.user)
+
+class GCMNotification(models.Model):
+    multicast_id = models.CharField(max_length=250)
+    num_success = models.IntegerField(default=0)
+    num_failure = models.IntegerField(default=0)
+    sent_data = models.CharField(max_length=10000)
+    sent_to = models.ManyToManyField(NotificationConfig)
+    time = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User)
