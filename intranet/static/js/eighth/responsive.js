affe4b0e9021a4407cb004e50fa2fcf1cd50b3b7
James Woglom <2016jwoglom@tjhsst.edu>
2015-08-04 11:44:36 -0400

    eighth signup: never have double scroll with activities list

--- intranet/static/js/eighth/responsive.js
+++ intranet/static/js/eighth/responsive.js
@@ -21,6 +21,46 @@ $(document).ready(function() {
 
         $(".middle .switch").click(function() {
             $(".primary-content").toggleClass("viewing");
-        })
+            activityPickerResize();
+        });
+
+        var width = $(window).width();
+        var height = $(window).height();
+        if(width <= 500 && height <= 745) {
+            setTimeout(function() {
+                $(".eighth-signup .switch").click();
+
+                setTimeout(function() {
+                    activityPickerResize();
+                }, 450);
+            }, 450);
+        } else {
+            setTimeout(function() {
+                activityPickerResize();
+            }, 250);
+        }
+
+        /*
+         * resize #activity-picker to correct size
+         * to avoid scrollbar
+         */
+        $(window).on("resize", function() {
+            activityPickerResize();
+        });
     }
 })
+
+function activityPickerResize() {
+    console.debug("activity picker resize")
+    var h = $(window).height();
+    var w = $(window).width();
+    var p = $("#activity-picker");
+    var topInfo = $(".middle-wrapper").height() + $(".header").height() + 15; // 163
+    var btnDown = $(".primary-content").hasClass("viewing") && w <= 500 ? 0 : 120; // 120
+    var pixels = h - topInfo - btnDown;
+    if(pixels < 300) {
+        // minimum usable height
+        pixels = 300;
+    }
+    p.css({ height: (pixels)+"px" });
+}
