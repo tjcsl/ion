3bffebebcd78b3f83696797e3e0733eef605c743
James Woglom <2016jwoglom@tjhsst.edu>
2015-08-08 23:29:18 -0400

    files authentication; password encrypted with aes

--- intranet/apps/files/urls.py
+++ intranet/apps/files/urls.py
@@ -7,6 +7,7 @@ from . import views
 
 urlpatterns = [
     url(r"^$", views.files_view, name="files"),
+    url(r"^/auth$", views.files_auth, name="files_auth"),
     url(r"^/(?P<fstype>\w+)$", views.files_type, name="files_type"),
     url(r"^/(?P<fstype>\w+)?dir=(?P<fsdir>\w+)$", views.files_type, name="files_type_dir"),
     
--- intranet/apps/files/views.py
+++ intranet/apps/files/views.py
@@ -5,11 +5,15 @@ import logging
 import pysftp
 import tempfile
 import os
+import base64
 from os.path import normpath
+from Crypto import Random
+from Crypto.Cipher import AES
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import StreamingHttpResponse
 from django.core.servers.basehttp import FileWrapper
+from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
 from .models import Host
 from . import cred
@@ -35,6 +39,48 @@ def files_view(request):
     return render(request, "files/home.html", context)
 
 @login_required
+def files_auth(request):
+    """Display authentication for filecenter."""
+    if "password" in request.POST:
+        key = Random.new().read(32)
+        iv = Random.new().read(16)
+        obj = AES.new(key, AES.MODE_CFB, iv)
+        message = request.POST.get("password")
+        ciphertext = obj.encrypt(message)
+        request.session["files_iv"] = base64.b64encode(iv)
+        request.session["files_text"] = base64.b64encode(ciphertext)
+        cookie_key = base64.b64encode(key)
+
+        nexturl = request.GET.get("next", None)
+        if nexturl and nexturl.startswith("/files"):
+            response = redirect(nexturl)
+        else:
+            response = redirect("files")
+        response.set_cookie(key="files_key", value=cookie_key)
+        return response
+    else:
+        return render(request, "files/auth.html", {})
+
+
+def get_authinfo(request):
+    if (("files_iv" not in request.session) or 
+        ("files_text" not in request.session) or 
+        ("files_key" not in request.COOKIES)):
+        return False
+
+    iv = base64.b64decode(request.session["files_iv"])
+    text = base64.b64decode(request.session["files_text"])
+    key = base64.b64decode(request.COOKIES["files_key"])
+
+    obj = AES.new(key, AES.MODE_CFB, iv)
+    password = obj.decrypt(text)
+
+    return {
+        "username": request.user.username,
+        "password": password
+    }
+
+@login_required
 def files_type(request, fstype=None):
     
     try:
@@ -43,8 +89,13 @@ def files_type(request, fstype=None):
         messages.error(request, "Could not find host in database.")
         return redirect("files")
 
+    authinfo = get_authinfo(request)
+
+    if not authinfo:
+        return redirect("{}?next={}".format(reverse("files_auth"), request.get_full_path()))
+
     try:
-        sftp = create_session(host.address, cred.USER, cred.PASS)
+        sftp = create_session(host.address, authinfo["username"], authinfo["password"])
     except pysftp.SSHException as e:
         messages.error(request, e)
         return redirect("files")
