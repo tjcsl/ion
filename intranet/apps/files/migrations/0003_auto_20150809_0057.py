c5169288aa76933f8a5284f8ff74b89e174f4de9
James Woglom <2016jwoglom@tjhsst.edu>
2015-08-09 00:57:56 -0400

    migration file

---
+++ intranet/apps/files/migrations/0003_auto_20150809_0057.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('files', '0002_auto_20150808_2134'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='host',
+            options={'ordering': ['-linux', 'name']},
+        ),
+    ]
