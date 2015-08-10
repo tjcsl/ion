531da3f43f254d8518b1e9e014162c7778160592
James Woglom <2016jwoglom@tjhsst.edu>
2015-08-09 20:24:25 -0400

    eighth signup status email

--- intranet/apps/eighth/notifications.py
+++ intranet/apps/eighth/notifications.py
@@ -9,7 +9,7 @@ from .models import EighthBlock, EighthSignup
 
 logger = logging.getLogger(__name__)
 
-def signup_status_email(user, blocks):
+def signup_status_email(user, next_blocks):
 
     subject = "Eighth Period Signup Status"
 
@@ -19,8 +19,43 @@ def signup_status_email(user, blocks):
     else:
         return False
 
+    blocks = []
+    issues = 0
+    for blk in next_blocks:
+        try:
+            signup = EighthSignup.objects.get(user=user, scheduled_activity__block=blk)
+        except EighthSignup.DoesNotExist:
+            signup = None
+
+        cancelled = False
+
+        if not signup:
+            issues += 1
+
+        if signup and signup.scheduled_activity.cancelled:
+            issues += 1
+            cancelled = True
+        blocks.append({
+            "block": blk,
+            "signup": signup,
+            "cancelled": cancelled
+        })
+
+    block_date = next_blocks[0].date
+    block_signup_time = next_blocks[0].signup_time
+    if block_signup_time:
+        block_signup_time = "{}:{}".format(block_signup_time.hour, block_signup_time.minute)
+
+    # We can't build an absolute URI because this isn't being executed
+    # in the context of a Django request
+    base_url = "https://ion.tjhsst.edu/" #request.build_absolute_uri(reverse('index'))
     data = {
-        "user": user
+        "user": user,
+        "blocks": blocks,
+        "block_date": block_date,
+        "block_signup_time": block_signup_time,
+        "base_url": base_url,
+        "issues": issues
     }
 
     email_send("eighth/emails/signup_status.txt",
