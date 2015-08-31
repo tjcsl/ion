fab564d5a114edd2a70e7bd139b91e60994d0865
James Woglom <2016jwoglom@tjhsst.edu>
2015-08-31 11:15:38 -0400

    indentation

--- intranet/apps/groups/migrations/0001_initial.py
+++ intranet/apps/groups/migrations/0001_initial.py
@@ -6,7 +6,7 @@ import intranet.apps.groups.models
 
 
 class Migration(migrations.Migration):
-""" NOTE: You may need to run ./manage.py fake """
+    """ NOTE: You may need to run ./manage.py fake """
 
     dependencies = [
         ('auth', '0006_require_contenttypes_0002'),
