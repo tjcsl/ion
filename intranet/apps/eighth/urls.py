51e71740b09b885edba201b9fdbe3791499cb5aa
James Woglom <2016jwoglom@tjhsst.edu>
2015-09-12 15:56:16 -0400

    allow students to view activity info, including when an activity is scheduled by clicking on the name of the activity in the details

--- intranet/apps/eighth/urls.py
+++ intranet/apps/eighth/urls.py
@@ -2,10 +2,11 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import include, url
-from .views import routers, signup, attendance, profile
+from .views import routers, signup, attendance, activities, profile
 from .views.admin import (
-    general, activities, blocks, groups, rooms, sponsors, scheduling)
+    general, blocks, groups, rooms, sponsors, scheduling)
 from .views.admin import attendance as admin_attendance
+from .views.admin import activities as admin_activities
 
 urlpatterns = [
     url(r"^$", routers.eighth_redirect_view, name="eighth_redirect"),
@@ -29,19 +30,22 @@ urlpatterns = [
     url(r"^/profile(?:/(?P<user_id>\d+))/signup/(?P<block_id>\d+)?$", profile.profile_signup_view, name="eighth_profile_signup"),
     url(r"^/profile/edit(?:/(?P<user_id>\d+))?$", profile.edit_profile_view, name="eighth_edit_profile"),
 
-    # Roster (for students)
+    # Roster (for students/teachers)
     url(r"^/roster/(?P<scheduled_activity_id>\d+)$", attendance.roster_view, name="eighth_roster"),
     url(r"^/roster/raw/(?P<scheduled_activity_id>\d+)$", attendance.raw_roster_view, name="eighth_raw_roster"),
+
+    # Activity Info (for students/teachers)
+    url(r"^/activity/(?P<activity_id>\d+)$", activities.activity_view, name="eighth_activity"),
 
     # Admin
     url(r"^/admin$", general.eighth_admin_dashboard_view, name="eighth_admin_dashboard"),
 ]
 
 eighth_admin_patterns = [
-    # Activities
-    url(r"^activities/add$", activities.add_activity_view, name="eighth_admin_add_activity"),
-    url(r"^activities/edit/(?P<activity_id>\d+)$", activities.edit_activity_view, name="eighth_admin_edit_activity"),
-    url(r"^activities/delete/(?P<activity_id>\d+)$", activities.delete_activity_view, name="eighth_admin_delete_activity"),
+    # admin_activities
+    url(r"^activities/add$", admin_activities.add_activity_view, name="eighth_admin_add_activity"),
+    url(r"^activities/edit/(?P<activity_id>\d+)$", admin_activities.edit_activity_view, name="eighth_admin_edit_activity"),
+    url(r"^activities/delete/(?P<activity_id>\d+)$", admin_activities.delete_activity_view, name="eighth_admin_delete_activity"),
 
     # Blocks
     url(r"^blocks/add$", blocks.add_block_view, name="eighth_admin_add_block"),
---
+++ intranet/apps/eighth/views/activities.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import logging
+from django import http
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from ....utils.serialization import safe_json
+from ...users.models import User
+from ..exceptions import SignupException
+from ..models import (
+    EighthBlock, EighthSignup, EighthScheduledActivity, EighthActivity
+)
+from ..serializers import EighthBlockDetailSerializer
+
+
+logger = logging.getLogger(__name__)
+
+@login_required
+def activity_view(request, activity_id=None):
+    activity = get_object_or_404(EighthActivity, id=activity_id)
+
+    first_block = EighthBlock.objects.get_first_upcoming_block()
+
+    scheduled_activities = EighthScheduledActivity.objects.filter(
+        activity=activity
+    )
+
+    if first_block:
+        scheduled_activities = scheduled_activities.filter(block__date__gte=first_block.date)
+
+    context = {
+        "activity": activity,
+        "scheduled_activities": scheduled_activities
+    }
+
+    return render(request, "eighth/activity.html", context)
