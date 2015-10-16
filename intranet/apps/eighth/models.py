e883e1e006e744b786a6b06c0b2cc5a0e59c1f78
James Woglom <2016jwoglom@tjhsst.edu>
2015-10-16 15:23:49 -0400

    settings import

--- intranet/apps/eighth/models.py
+++ intranet/apps/eighth/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from itertools import chain
 import logging
 import datetime
+from intranet import settings
 from django.db import models
 from django.db.models import Manager, Q
 from django.core.exceptions import ValidationError, NON_FIELD_ERRORS
