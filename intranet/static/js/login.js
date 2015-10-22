5ac66f1127b3ee5ae86d1ca5b54776d9a73bf864
James Woglom <2016jwoglom@tjhsst.edu>
2015-10-22 09:26:38 -0400

    login fixed footer

--- intranet/static/js/login.js
+++ intranet/static/js/login.js
@@ -42,8 +42,8 @@ $(document).ready(function() {
     }
 
     $(window).resize(function() {
-        footerCheck();
+        //footerCheck();
     });
-    footerCheck();
+    //footerCheck();
 
 });
