b31b1b9eb6beccbaaf520c6fb2ebffb156e22a25
James Woglom <2016jwoglom@tjhsst.edu>
2015-10-27 10:09:43 -0400

    transfer attendance/absence data

---
+++ intranet/apps/eighth/management/commands/import_attendance.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import csv
+from django.core.management.base import BaseCommand
+import elasticsearch
+from intranet import settings
+from intranet.apps.users.models import User
+from intranet.apps.eighth.models import EighthScheduledActivity, EighthBlock, EighthActivity, EighthSignup
+
+
+class Command(BaseCommand):
+    help = "Transfer attendance data"
+
+    def handle(self, **options):
+        """
+            Exported "eighth_absentees" table in CSV format
+        """
+
+        with open('eighth_absentees.csv', 'r') as absopen:
+            absences = csv.reader(absopen)
+            for row in absences:
+                bid, uid = row
+                try:
+                    usr = User.objects.get(id=uid)
+                except User.DoesNotExist:
+                    self.stdout.write("User {} doesn't exist, bid {}".format(usr, bid))
+                else:
+                    try:
+                        blk = EighthBlock.objects.get(id=bid)
+                    except EighthBlock.DoesNotExist:
+                        self.stdout.write("Block {} doesn't exist, with user {}".format(bid, uid))
+                    else:
+                        usr_signup = EighthSignup.objects.filter(user=usr, scheduled_activity__block=blk)
+                        self.stdout.write("{} signup: {}".format(usr, usr_signup))
+                        if usr_signup.count() == 0:
+                            other_abs_act, _ = EighthActivity.objects.get_or_create(name="z-OTHER ABSENCE (transferred from Iodine)",
+                                                                                    administrative=True)
+                            other_abs_sch, _ = EighthScheduledActivity.objects.get_or_create(block=blk,
+                                                                                             activity=other_abs_act)
+                            other_abs_su = EighthSignup.objects.create(user=usr,
+                                                                       scheduled_activity=other_abs_sch,
+                                                                       was_absent=True)
+                            self.stdout.write("{} Signup created: {}".format(usr, other_abs_su))
+                        else:
+                            usr_signup[0].was_absent = True
+                            usr_signup[0].save()
+                            self.stdout.write("{} Signup modified: {}".format(usr, usr_signup[0]))
+
+
+        
+        self.stdout.write("Done.")
