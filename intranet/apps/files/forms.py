018a3d73796a6383372095b54e4a28d9a384a9fe
James Woglom <2016jwoglom@tjhsst.edu>
2015-11-08 23:29:32 -0500

    fix file upload

--- intranet/apps/files/forms.py
+++ intranet/apps/files/forms.py
@@ -2,13 +2,15 @@
 from __future__ import unicode_literals
 
 from django import forms
+import logging
 from intranet import settings
 
+logger = logging.getLogger(__name__)
 
 class UploadFileForm(forms.Form):
 
     def validate_size(obj):
-        filesize = obj.file.size
+        filesize = obj.file.__sizeof__()
         if filesize > settings.FILES_MAX_UPLOAD_SIZE:
             raise forms.ValidationError("The file uploaded is above the maximum upload size ({}MB). " +
                                         "Use a desktop client to upload this file.".format(
