b7400425b7e0a79f632b016491395efd55282da5
James Woglom <2016jwoglom@tjhsst.edu>
2015-11-18 10:41:18 -0500

    group view page

--- intranet/apps/groups/views.py
+++ intranet/apps/groups/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .forms import GroupForm
 from .models import Group
+from ..users.models import User
 logger = logging.getLogger(__name__)
 
 
@@ -14,9 +15,15 @@ logger = logging.getLogger(__name__)
 # students can only add themselves to non-admin groups unless they are already an admin
 @login_required
 def groups_view(request):
+    group_admin = request.user.has_admin_permission("groups")
+    if group_admin and "user" in request.GET:
+        user = User.objects.get(id=request.GET.get("user"))
+    else:
+        user = request.user
     return render(request, "groups/groups.html", {
+        "user": user,
         "all_groups": Group.objects.all(),
-        "group_admin": request.user.has_admin_permission("groups")
+        "group_admin": group_admin
     })
 
 
