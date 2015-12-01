2556aff5313fd223a8ef77364bd6949967e78dba
jacobajit <jacobvajit@gmail.com>
2015-11-30 21:42:32 -0500

    Fix service worker path

--- intranet/static/js/swregistration.js
+++ intranet/static/js/swregistration.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function() {
   var outputElement = document.getElementById('output');
-  navigator.serviceWorker.register('service-worker.js', { scope: './' })//correct scope?
+  navigator.serviceWorker.register('/static/js/service-worker.js', { scope: './' })//correct scope?
     .then(function(r) {
       console.log('registered service worker');
     })
