9ffea2910ae58963497c07afc76b9d235ddb5cea
James Woglom <2016jwoglom@tjhsst.edu>
2015-11-30 22:41:27 -0500

    fix serviceworker configuration

--- intranet/static/js/swregistration.js
+++ intranet/static/js/swregistration.js
@@ -1,15 +1,14 @@
 window.addEventListener('load', function() {
-  var outputElement = document.getElementById('output');
-  navigator.serviceWorker.register('/static/js/service-worker.js', { scope: './' })//correct scope?
-    .then(function(r) {
-      console.log('registered service worker');
-    })
-    .catch(function(whut) {
-      console.error('uh oh... ');
-      console.error(whut);
+    var outputElement = document.getElementById('output');
+    navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
+             .then(function(r) {
+                 console.log('registered service worker');
+             })
+             .catch(function(whut) {
+                 console.error('service worker failed to register');
+                 console.error(whut);
+             });
+    window.addEventListener('beforeinstallprompt', function(e) {
+        /* */
     });
-   
-  window.addEventListener('beforeinstallprompt', function(e) {
-    
-  });
 });
