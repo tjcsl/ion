d6c19a30136b5620d515d2b243ed5470a32163a7
James Woglom <2016jwoglom@tjhsst.edu>
2015-11-30 23:50:31 -0500

    cron

--- cron/eighth-evening.sh
+++ cron/eighth-evening.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+export VIRTUAL_ENV="/usr/local/virtualenvs/ion"
+cd /usr/local/www/intranet3
 /usr/local/virtualenvs/ion/bin/python /usr/local/www/intranet3/manage.py signup_status_email --only-tomorrow --silent
