a819420791cd2c6ba0f5ea58bc7f3a313f08bb6a
James Woglom <2016jwoglom@tjhsst.edu>
2015-12-01 21:47:42 -0500

    don't pass production=true to avoid erroring debuglog

--- cron/env.sh
+++ cron/env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /usr/local/www/intranet3
-PRODUCTION=TRUE DEBUG=FALSE VIRTUAL_ENV='/usr/local/virtualenvs/ion' /usr/local/virtualenvs/ion/bin/python "$@"
+DEBUG=FALSE VIRTUAL_ENV='/usr/local/virtualenvs/ion' /usr/local/virtualenvs/ion/bin/python "$@"
