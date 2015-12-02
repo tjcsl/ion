86acc7a48b7b3767a2503223fe7f94ce5c98bf60
James Woglom <2016jwoglom@tjhsst.edu>
2015-12-01 21:35:18 -0500

    cron env

--- cron/eighth-morning.sh
+++ cron/eighth-morning.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /usr/local/www/intranet3
-VIRTUAL_ENV="/usr/local/virtualenvs/ion" /usr/local/virtualenvs/ion/bin/python /usr/local/www/intranet3/manage.py signup_status_email --only-today --silent
+./cron/env.sh ./manage.py signup_status_email --only-today --silent
