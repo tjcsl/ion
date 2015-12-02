2afaa7aec8a5599b3e9ab21debfcf9ccf568394b
James Woglom <2016jwoglom@tjhsst.edu>
2015-12-02 10:03:56 -0500

    prod=true

--- cron/env.sh
+++ cron/env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /usr/local/www/intranet3
-DEBUG=FALSE VIRTUAL_ENV='/usr/local/virtualenvs/ion' /usr/local/virtualenvs/ion/bin/python "$@"
+PRODUCTION=TRUE DEBUG=FALSE VIRTUAL_ENV='/usr/local/virtualenvs/ion' /usr/local/virtualenvs/ion/bin/python "$@"
