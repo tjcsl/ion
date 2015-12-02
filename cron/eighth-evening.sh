f99f9cb3cdfd32f4d6332a38bf719a35463a02e9
James Woglom <2016jwoglom@tjhsst.edu>
2015-12-01 21:42:22 -0500

    email log

--- cron/eighth-evening.sh
+++ cron/eighth-evening.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+timestamp=$(date +"%Y-%m-%d-%H%M")
 cd /usr/local/www/intranet3
 ./cron/env.sh ./manage.py signup_status_email --only-tomorrow --silent
+echo "Evening signup email sent at $timestamp." >> /var/log/ion/email.log
--- cron/eighth-morning.sh
+++ cron/eighth-morning.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+timestamp=$(date +"%Y-%m-%d-%H%M")
 cd /usr/local/www/intranet3
 ./cron/env.sh ./manage.py signup_status_email --only-today --silent
+echo "Morning signup email sent at $timestamp." >> /var/log/ion/email.log
