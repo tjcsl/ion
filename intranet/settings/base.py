f7fc66ba2677384f885743053c83fd40421184d6
James Woglom <2016jwoglom@tjhsst.edu>
2015-12-10 13:55:22 -0500

    file upload handlers

--- intranet/settings/base.py
+++ intranet/settings/base.py
@@ -32,6 +32,11 @@ ADMINS = (
 FEEDBACK_EMAIL = "intranet@lists.tjhsst.edu"
 APPROVAL_EMAIL = "intranet-approval@lists.tjhsst.edu"
 
+FILE_UPLOAD_HANDLERS = [
+    "django.core.files.uploadhandler.MemoryFileUploadHandler",
+    "django.core.files.uploadhandler.TemporaryFileUploadHandler"
+]
+
 FILES_MAX_UPLOAD_SIZE = 200 * 1024 * 1024
 FILES_MAX_DOWNLOAD_SIZE = 200 * 1024 * 1024
 
