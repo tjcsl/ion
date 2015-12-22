9929ae5c752973fc8310ef9decbcfbb5e6e81420
James Woglom <2016jwoglom@tjhsst.edu>
2015-12-21 20:24:25 -0500

    separate chrome GCM functionality

--- intranet/apps/notifications/views.py
+++ intranet/apps/notifications/views.py
@@ -87,7 +87,7 @@ def gcm_list_view(request):
 
 
 @login_required
-def gcm_post_view(request):
+def gcm_android_post_view(request):
     if not request.user.has_admin_permission("notifications"):
         return redirect("index")
     try:
@@ -162,4 +162,83 @@ def gcm_post_view(request):
             messages.success(request, "Delivered message: {} success, {} failure".format(resp["success"], resp["failure"]))
         else:
             messages.error(request, "Failed. {}".format(req.text))
-    return render(request, "notifications/gcm_post.html", context)
+    return render(request, "notifications/gcm_android_post.html", context)
+
+
+@login_required
+def gcm_chrome_post_view(request):
+    if not request.user.has_admin_permission("notifications"):
+        return redirect("index")
+    try:
+        has_tokens = (settings.GCM_AUTH_KEY and settings.GCM_PROJECT_ID)
+    except AttributeError:
+        has_tokens = False
+
+    if not has_tokens:
+        messages.error(request, "GCM tokens not installed.")
+        return redirect("index")
+
+    # exclude those with no GCM token
+    nc_all = NotificationConfig.objects.exclude(chrome_gcm_token=None)
+    context = {
+        "nc_all": nc_all,
+        "has_tokens": has_tokens
+    }
+
+    if request.method == "POST":
+        nc_objs = []
+        reg_ids = []
+        fail_ids = []
+        nc_users = request.POST.getlist("nc_users")
+        logger.debug(nc_users)
+        for ncid in nc_users:
+            try:
+                nc = NotificationConfig.objects.get(id=ncid)
+            except NotificationConfig.DoesNotExist:
+                continue
+
+            if nc.chrome_gcm_token:
+                reg_ids.append(nc.chrome_gcm_token)
+                nc_objs.append(nc)
+            else:
+                fail_ids.append(ncid)
+
+        logger.debug(nc_objs)
+        logger.debug(reg_ids)
+        headers = {
+            "Content-Type": "application/json",
+            "project_id": settings.GCM_PROJECT_ID,
+            "Authorization": "key={}".format(settings.GCM_AUTH_KEY)
+        }
+        data = {
+            "registration_ids": reg_ids,
+            "data": {
+                "title": request.POST.get("title", "Title"),
+                "text": request.POST.get("text", "Text"),
+                "url": request.POST.get("url", None),
+                "sound": request.POST.get("sound", False),
+                "wakeup": request.POST.get("wakeup", False),
+                "vibrate": request.POST.get("vibrate", 0)
+            }
+        }
+        logger.debug(json.dumps(data))
+        req = requests.post("https://android.googleapis.com/gcm/send", headers=headers, data=json.dumps(data))
+        logger.debug(req.text)
+        resp = req.json()
+        # example output:
+        # {"multicast_id":123456,"success":1,"failure":0,"canonical_ids":0,"results":[{"message_id":"0:142%771acd"}]}
+        logger.debug(resp)
+        if "multicast_id" in resp:
+            n = GCMNotification.objects.create(multicast_id=resp["multicast_id"],
+                                               num_success=resp["success"],
+                                               num_failure=resp["failure"],
+                                               user=request.user,
+                                               sent_data=json.dumps(data))
+            for nc in nc_objs:
+                n.sent_to.add(nc)
+            n.save()
+            logger.debug(n)
+            messages.success(request, "Delivered message: {} success, {} failure".format(resp["success"], resp["failure"]))
+        else:
+            messages.error(request, "Failed. {}".format(req.text))
+    return render(request, "notifications/gcm_chrome_post.html", context)
