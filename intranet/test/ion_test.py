95810ee0e52c9a121bffd3312763f025fd993902
Peter Foley <pefoley2@pefoley.com>
2016-01-27 17:15:27 -0500

    test cleanups

--- intranet/test/ion_test.py
+++ intranet/test/ion_test.py
@@ -9,21 +9,19 @@ from ..db.ldap_db import LDAPConnection
 
 
 class IonTestCase(TestCase):
-    """
-    We don't want to actually call out to ldap for testing, so mock it out here.
-    """
-
     def login(self):
         # We need to add the user to the db before trying to login as them.
-        user = User.get_user(username='awilliam')
-        user.save()
+        User.get_user(username='awilliam').save()
         with self.settings(MASTER_PASSWORD='pbkdf2_sha256$24000$qp64pooaIEAc$j5wiTlyYzcMu08dVaMRus8Kyfvn5ZfaJ/Rn+Z/fH2Bw='):
             self.client.login(username='awilliam', password='dankmemes')
 
     @classmethod
     def setUpClass(cls):
+        super().setUpClass()
+        # We don't want to actually call out to ldap for testing, so mock it out here.
         cls.ldap_mock = mock.patch.object(LDAPConnection, 'conn', new=MockLDAPConnection()).start()
 
     @classmethod
     def tearDownClass(cls):
         cls.ldap_mock.stop()
+        super().tearDownClass()
