80f4e15a43136df2776a7c0e2c4dfa0bdc9119c9
James Woglom <j@wogloms.net>
2016-02-01 23:14:27 -0500

    fix find duplicates script

--- intranet/apps/eighth/management/commands/find_duplicates.py
+++ intranet/apps/eighth/management/commands/find_duplicates.py
@@ -23,13 +23,17 @@ class Command(BaseCommand):
         for u in User.objects.all():
             for b in EighthBlock.objects.all():
                 su = EighthSignup.objects.filter(user=u, scheduled_activity__block=b)
-                if su.count() not in [0, 1]:
+                if su.count() != 0 and su.count() != 1:
                     print("Duplicate: {} {}".format(u.id, b.id))
-                    print("Scheduled activities:")
-                    print(su)
+                    print("Scheduled activities:", su)
                     if fix:
-                        if su[0].scheduled_activity.both_blocks:
+                        if su[0].scheduled_activity.activity.both_blocks:
                             sibling = su[0].scheduled_activity.get_both_blocks_sibling()
                             if EighthSignup.objects.filter(user=u, scheduled_activity=sibling).exists():
+                                print("Deleted su1 {}".format(su[1]))
                                 su[1].delete()
-                                print("Deleted {}".format(su[1]))
+                        elif su[1].scheduled_activity.activity.both_blocks:
+                            sibling = su[1].scheduled_activity.get_both_blocks_sibling()
+                            if EighthSignup.objects.filter(user=u, scheduled_activity=sibling).exists():
+                                print("Deleted su0 {}".format(su[0]))
+                                su[0].delete()
