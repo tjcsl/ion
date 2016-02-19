428ad6d356994bec6c42c8f7bd9b91b006802014
TCYRUS <tcyrus@users.noreply.github.com>
2016-02-19 12:54:28 -0500

    Minor Changes to serviceworker.js
    Trying to make a new commit to trigger Coveralls

--- intranet/templates/serviceworker.js
+++ intranet/templates/serviceworker.js
@@ -4,7 +4,7 @@ self.addEventListener('push', function(event) {
     console.log('Received a push message', event);
 
     var data = {};
-    if(event.data) {
+    if (event.data) {
         data = event.data.json();
         console.debug(data);
         showNotif(event, data)
@@ -20,14 +20,14 @@ self.addEventListener('push', function(event) {
             showNotif(evt, j);
         });
     }
-    
-    
+
+
     showNotif = function(event, data) {
         //replace with message data fetched from Ion API/DB based on logged in user's UID
         var title = data.title || "Intranet Notification";
         var body = data.text || "Click here to view."
         var icon = '/static/img/logos/touch/touch-icon192.png';
-        var tag = data.url ? "url="+data.url : (data.tag || 'ion-notification');
+        var tag = data.url ? "url=" + data.url : (data.tag || 'ion-notification');
 
         self.registration.showNotification(title, {
             body: body,
@@ -38,7 +38,7 @@ self.addEventListener('push', function(event) {
 
 });
 
-self.addEventListener('notificationclick', function (event) {
+self.addEventListener('notificationclick', function(event) {
     var tag = event.notification.tag;
     console.log('Notification click: ', tag);
 
@@ -46,14 +46,14 @@ self.addEventListener('notificationclick', function (event) {
 
     tagUrl = '/?src=sw';
     var tags = tag.split("=");
-    if(tags[0] == "url") {
+    if (tags[0] == "url") {
         tagUrl = "/"+tags[1];
-        if(tagUrl.indexOf("?") == -1) {
+        if (tagUrl.indexOf("?") == -1) {
             tagUrl += "?src=sw";
         } else {
             tagUrl += "&src=sw";
         }
-        if(tagUrl.substring(0,2) == "//") {
+        if (tagUrl.substring(0, 2) == "//") {
             tagUrl = "/" + tagUrl.substring(2);
         }
     }
@@ -64,7 +64,7 @@ self.addEventListener('notificationclick', function (event) {
         clients.matchAll({
             type: 'window'
         })
-            .then(function(clientList){
+            .then(function(clientList) {
                 for (var i = 0; i < clientList.length; i++) {
                     var client = clientList[i];
                     if (client.url === tagUrl && 'focus' in client) {
