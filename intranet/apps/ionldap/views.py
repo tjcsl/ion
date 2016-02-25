5378e29324ba1129b8c09c73c0d32d006d6735eb
James Woglom <2016jwoglom@tjhsst.edu>
2016-02-25 13:49:38 -0500

    ionldap view class list

--- intranet/apps/ionldap/views.py
+++ intranet/apps/ionldap/views.py
@@ -2,8 +2,10 @@
 
 import logging
 
+from django import http
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import LDAPCourse
 
 logger = logging.getLogger(__name__)
 
@@ -19,4 +21,36 @@ def main_view(request):
         "user": user,
         "courses": courses
     }
+
     return render(request, "ionldap/main.html", context)
+
+@login_required
+def class_section_view(request, section_id):
+    try:
+        course = LDAPCourse.objects.get(section_id=section_id)
+    except LDAPCourse.DoesNotExist:
+        raise http.Http404
+
+    in_class = (course.users.filter(id=request.user.id).count() == 1)
+    can_view_students = (request.user.is_teacher)# or request.user.is_eighth_admin)
+    teacher_classes = LDAPCourse.objects.filter(teacher_name=course.teacher_name)
+    section_classes = LDAPCourse.objects.filter(course_id=course.course_id)
+
+    context = {
+        "course": course,
+        "in_class": in_class,
+        "can_view_students": can_view_students,
+        "teacher_user": course.teacher_user,
+        "teacher_classes": teacher_classes,
+        "section_classes": section_classes
+    }
+
+    return render(request, "ionldap/class.html", context)
+
+@login_required
+def class_room_view(request, room):
+    pass
+
+@login_required
+def all_classes_view(request):
+    pass
