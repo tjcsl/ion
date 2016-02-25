460a89b75256d3393b39220f073ee03510efb674
Peter Foley <pefoley2@pefoley.com>
2016-02-25 12:28:03 -0500

    nuke local.py

--- intranet/settings/__init__.py
+++ intranet/settings/__init__.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
-import os
 import sys
 
 if sys.version_info < (3, 3):
     # dependency on ipaddress module
     raise Exception("Python 3.3 or higher is required.")
 
-if os.getenv("PRODUCTION") == "TRUE":
-    from .base import *  # noqa
-else:
-    from .local import *  # noqa
+from .base import *  # noqa
