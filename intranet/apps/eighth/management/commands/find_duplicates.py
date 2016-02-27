fd698fa9d71d7c14638c8d70a31f6e6f47cb3378
Peter Foley <pefoley2@pefoley.com>
2016-02-26 20:18:51 -0500

    make find_duplicates actually performant

--- intranet/apps/eighth/management/commands/find_duplicates.py
+++ intranet/apps/eighth/management/commands/find_duplicates.py
@@ -15,24 +15,24 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         signup_list = collections.defaultdict(int)
-        for x in EighthSignup.objects.all():
+        for x in EighthSignup.objects.all().prefetch_related('scheduled_activity__block'):
             signup_list[(x.user_id, x.scheduled_activity.block_id)] += 1
         duplicates = [signup for signup, count in signup_list.items() if count > 1]
         if not duplicates:
             print("No duplicate signups found.")
             return
         for uid, bid in duplicates:
-            su = EighthSignup.objects.filter(user__id=uid, scheduled_activity__block_id=bid)
+            su = EighthSignup.objects.filter(user_id=uid, scheduled_activity__block_id=bid)
             print("Duplicate: {} {}".format(uid, bid))
             print("Scheduled activities:", su)
             if options['fix']:
                 if su[0].scheduled_activity.activity.both_blocks:
                     sibling = su[0].scheduled_activity.get_both_blocks_sibling()
-                    if EighthSignup.objects.filter(user__id=uid, scheduled_activity=sibling).exists():
+                    if EighthSignup.objects.filter(user_id=uid, scheduled_activity=sibling).exists():
                         print("Deleted su1 {}".format(su[1]))
                         su[1].delete()
                 elif su[1].scheduled_activity.activity.both_blocks:
                     sibling = su[1].scheduled_activity.get_both_blocks_sibling()
-                    if EighthSignup.objects.filter(user__id=uid, scheduled_activity=sibling).exists():
+                    if EighthSignup.objects.filter(user_id=uid, scheduled_activity=sibling).exists():
                         print("Deleted su0 {}".format(su[0]))
                         su[0].delete()
