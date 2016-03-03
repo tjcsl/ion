4ad93816a4937c9410620f0d7d7f106fc9c6ef02
Peter Foley <pefoley2@pefoley.com>
2016-03-02 23:57:49 -0500

    refactor poll_results_view

--- intranet/apps/polls/views.py
+++ intranet/apps/polls/views.py
@@ -158,6 +158,172 @@ def poll_vote_view(request, poll_id):
     return render(request, "polls/vote.html", context)
 
 
+def fmt(num):
+    return int(100 * num) / 100
+
+
+def perc(num, den):
+    if den == 0:
+        return 0
+    return int(10000 * num / den) / 100
+
+
+def handle_sap(q):
+    question_votes = votes = Answer.objects.filter(question=q)
+    users = q.get_users_voted()
+    num_users_votes = {u.id: votes.filter(user=u).count() for u in users}
+    user_scale = {u.id: (1 / num_users_votes[u.id]) for u in users}
+    choices = []
+    for c in q.choice_set.all().order_by("num"):
+        votes = question_votes.filter(choice=c)
+        vote_users = set([v.user for v in votes])
+        choice = {
+            "choice": c,
+            "votes": {
+                "total": {
+                    "all": len(vote_users),
+                    "all_percent": perc(
+                        len(vote_users), users.count()),
+                    "male": fmt(sum([v.user.is_male * user_scale[v.user.id] for v in votes])),
+                    "female": fmt(sum([v.user.is_female * user_scale[v.user.id] for v in votes]))
+                }
+            },
+            "users": [v.user for v in votes]
+        }
+        for yr in range(9, 14):
+            yr_votes = [v.user if v.user.grade and v.user.grade.number == yr else None for v in votes]
+            yr_votes = list(filter(None, yr_votes))
+            choice["votes"][yr] = {
+                "all": len(set(yr_votes)),
+                "male": fmt(sum([u.is_male * user_scale[u.id] for u in yr_votes])),
+                "female": fmt(sum([u.is_female * user_scale[u.id] for u in yr_votes])),
+            }
+        logger.debug(choice)
+        choices.append(choice)
+    """ Clear vote """
+    votes = question_votes.filter(clear_vote=True)
+    clr_users = set([v.user for v in votes])
+    choice = {
+        "choice": "Clear vote",
+        "votes": {
+            "total": {
+                "all": len(clr_users),
+                "all_percent": perc(
+                    len(clr_users), users.count()),
+                "male": fmt(sum([v.user.is_male * user_scale[v.user.id] for v in votes])),
+                "female": fmt(sum([v.user.is_female * user_scale[v.user.id] for v in votes]))
+            }
+        },
+        "users": clr_users
+    }
+    for yr in range(9, 14):
+        yr_votes = [v.user if v.user.grade and v.user.grade.number == yr else None for v in votes]
+        yr_votes = list(filter(None, yr_votes))
+        choice["votes"][yr] = {"all": len(yr_votes),
+                               "male": fmt(sum([u.is_male * user_scale[u.id] for u in yr_votes])),
+                               "female": fmt(sum([u.is_female * user_scale[u.id] for u in yr_votes]))}
+    logger.debug(choice)
+    choices.append(choice)
+
+    choice = {
+        "choice": "Total",
+        "votes": {
+            "total": {
+                "all": users.count(),
+                "votes_all": question_votes.count(),
+                "all_percent": perc(users.count(), users.count()),
+                "male": sum([u.is_male for u in users]),
+                "female": sum([u.is_female for u in users]),
+            }
+        }
+    }
+    for yr in range(9, 14):
+        yr_votes = [u if u.grade and u.grade.number == yr else None for u in users]
+        yr_votes = list(filter(None, yr_votes))
+        choice["votes"][yr] = {
+            "all": len(set(yr_votes)),
+            "male": fmt(sum([u.is_male * user_scale[u.id] for u in yr_votes])),
+            "female": fmt(sum([u.is_female * user_scale[u.id] for u in yr_votes]))
+        }
+
+    choices.append(choice)
+
+    return {"question": q, "choices": choices, "user_scale": user_scale}
+
+
+def handle_choice(q):
+    question_votes = votes = Answer.objects.filter(question=q)
+    users = q.get_users_voted()
+    choices = []
+    for c in q.choice_set.all().order_by("num"):
+        votes = question_votes.filter(choice=c)
+        choice = {
+            "choice": c,
+            "votes": {
+                "total": {
+                    "all": votes.count(),
+                    "all_percent": perc(votes.count(), question_votes.count()),
+                    "male": sum([v.user.is_male for v in votes]),
+                    "female": sum([v.user.is_female for v in votes])
+                }
+            },
+            "users": [v.user for v in votes]
+        }
+        for yr in range(9, 14):
+            yr_votes = [v.user if v.user.grade and v.user.grade.number == yr else None for v in votes]
+            yr_votes = list(filter(None, yr_votes))
+            choice["votes"][yr] = {"all": len(yr_votes),
+                                   "male": sum([u.is_male for u in yr_votes]),
+                                   "female": sum([u.is_female for u in yr_votes])}
+        logger.debug(choice)
+        choices.append(choice)
+    """ Clear vote """
+    votes = question_votes.filter(clear_vote=True)
+    choice = {
+        "choice": "Clear vote",
+        "votes": {
+            "total": {
+                "all": votes.count(),
+                "all_percent": perc(votes.count(), question_votes.count()),
+                "male": sum([v.user.is_male for v in votes]),
+                "female": sum([v.user.is_female for v in votes])
+            }
+        },
+        "users": [v.user for v in votes]
+    }
+    for yr in range(9, 14):
+        yr_votes = [v.user if v.user.grade and v.user.grade.number == yr else None for v in votes]
+        yr_votes = list(filter(None, yr_votes))
+        choice["votes"][yr] = {"all": len(yr_votes),
+                               "male": sum([u.is_male for u in yr_votes]),
+                               "female": sum([u.is_female for u in yr_votes])}
+    logger.debug(choice)
+    choices.append(choice)
+
+    choice = {
+        "choice": "Total",
+        "votes": {
+            "total": {
+                "all": question_votes.count(),
+                "users_all": users.count(),
+                "all_percent": perc(question_votes.count(), users.count()),
+                "male": sum([u.is_male for u in users]),
+                "female": sum([u.is_female for u in users]),
+            }
+        }
+    }
+    for yr in range(9, 14):
+        yr_votes = [u if u.grade and u.grade.number == yr else None for u in users]
+        yr_votes = list(filter(None, yr_votes))
+        choice["votes"][yr] = {"all": len(yr_votes),
+                               "male": sum([u.is_male for u in yr_votes]),
+                               "female": sum([u.is_female for u in yr_votes])}
+
+    choices.append(choice)
+
+    return {"question": q, "choices": choices}
+
+
 @login_required
 def poll_results_view(request, poll_id):
     if not request.user.has_admin_permission("polls"):
@@ -168,170 +334,12 @@ def poll_results_view(request, poll_id):
     except Poll.DoesNotExist:
         raise http.Http404
 
-    def fmt(num):
-        return int(100 * num) / 100
-
-    def perc(num, den):
-        if den == 0:
-            return 0
-        return int(10000 * num / den) / 100
-
     questions = []
     for q in poll.question_set.all():
         if q.type == "SAP":  # Split-approval; each person splits their one vote
-            question_votes = votes = Answer.objects.filter(question=q)
-            users = q.get_users_voted()
-            num_users_votes = {u.id: votes.filter(user=u).count() for u in users}
-            user_scale = {u.id: (1 / num_users_votes[u.id]) for u in users}
-            choices = []
-            for c in q.choice_set.all().order_by("num"):
-                votes = question_votes.filter(choice=c)
-                vote_users = set([v.user for v in votes])
-                choice = {
-                    "choice": c,
-                    "votes": {
-                        "total": {
-                            "all": len(vote_users),
-                            "all_percent": perc(
-                                len(vote_users), users.count()),
-                            "male": fmt(sum([v.user.is_male * user_scale[v.user.id] for v in votes])),
-                            "female": fmt(sum([v.user.is_female * user_scale[v.user.id] for v in votes]))
-                        }
-                    },
-                    "users": [v.user for v in votes]
-                }
-                for yr in range(9, 14):
-                    yr_votes = [v.user if v.user.grade and v.user.grade.number == yr else None for v in votes]
-                    yr_votes = list(filter(None, yr_votes))
-                    choice["votes"][yr] = {
-                        "all": len(set(yr_votes)),
-                        "male": fmt(sum([u.is_male * user_scale[u.id] for u in yr_votes])),
-                        "female": fmt(sum([u.is_female * user_scale[u.id] for u in yr_votes])),
-                    }
-                logger.debug(choice)
-                choices.append(choice)
-            """ Clear vote """
-            votes = question_votes.filter(clear_vote=True)
-            clr_users = set([v.user for v in votes])
-            choice = {
-                "choice": "Clear vote",
-                "votes": {
-                    "total": {
-                        "all": len(clr_users),
-                        "all_percent": perc(
-                            len(clr_users), users.count()),
-                        "male": fmt(sum([v.user.is_male * user_scale[v.user.id] for v in votes])),
-                        "female": fmt(sum([v.user.is_female * user_scale[v.user.id] for v in votes]))
-                    }
-                },
-                "users": clr_users
-            }
-            for yr in range(9, 14):
-                yr_votes = [v.user if v.user.grade and v.user.grade.number == yr else None for v in votes]
-                yr_votes = list(filter(None, yr_votes))
-                choice["votes"][yr] = {"all": len(yr_votes),
-                                       "male": fmt(sum([u.is_male * user_scale[u.id] for u in yr_votes])),
-                                       "female": fmt(sum([u.is_female * user_scale[u.id] for u in yr_votes]))}
-            logger.debug(choice)
-            choices.append(choice)
-
-            choice = {
-                "choice": "Total",
-                "votes": {
-                    "total": {
-                        "all": users.count(),
-                        "votes_all": question_votes.count(),
-                        "all_percent": perc(users.count(), users.count()),
-                        "male": sum([u.is_male for u in users]),
-                        "female": sum([u.is_female for u in users]),
-                    }
-                }
-            }
-            for yr in range(9, 14):
-                yr_votes = [u if u.grade and u.grade.number == yr else None for u in users]
-                yr_votes = list(filter(None, yr_votes))
-                choice["votes"][yr] = {
-                    "all": len(set(yr_votes)),
-                    "male": fmt(sum([u.is_male * user_scale[u.id] for u in yr_votes])),
-                    "female": fmt(sum([u.is_female * user_scale[u.id] for u in yr_votes]))
-                }
-
-            choices.append(choice)
-
-            question = {"question": q, "choices": choices, "user_scale": user_scale}
-            questions.append(question)
+            questions.append(handle_sap(q))
         elif q.is_choice():
-            question_votes = votes = Answer.objects.filter(question=q)
-            users = q.get_users_voted()
-            choices = []
-            for c in q.choice_set.all().order_by("num"):
-                votes = question_votes.filter(choice=c)
-                choice = {
-                    "choice": c,
-                    "votes": {
-                        "total": {
-                            "all": votes.count(),
-                            "all_percent": perc(votes.count(), question_votes.count()),
-                            "male": sum([v.user.is_male for v in votes]),
-                            "female": sum([v.user.is_female for v in votes])
-                        }
-                    },
-                    "users": [v.user for v in votes]
-                }
-                for yr in range(9, 14):
-                    yr_votes = [v.user if v.user.grade and v.user.grade.number == yr else None for v in votes]
-                    yr_votes = list(filter(None, yr_votes))
-                    choice["votes"][yr] = {"all": len(yr_votes),
-                                           "male": sum([u.is_male for u in yr_votes]),
-                                           "female": sum([u.is_female for u in yr_votes])}
-                logger.debug(choice)
-                choices.append(choice)
-            """ Clear vote """
-            votes = question_votes.filter(clear_vote=True)
-            choice = {
-                "choice": "Clear vote",
-                "votes": {
-                    "total": {
-                        "all": votes.count(),
-                        "all_percent": perc(votes.count(), question_votes.count()),
-                        "male": sum([v.user.is_male for v in votes]),
-                        "female": sum([v.user.is_female for v in votes])
-                    }
-                },
-                "users": [v.user for v in votes]
-            }
-            for yr in range(9, 14):
-                yr_votes = [v.user if v.user.grade and v.user.grade.number == yr else None for v in votes]
-                yr_votes = list(filter(None, yr_votes))
-                choice["votes"][yr] = {"all": len(yr_votes),
-                                       "male": sum([u.is_male for u in yr_votes]),
-                                       "female": sum([u.is_female for u in yr_votes])}
-            logger.debug(choice)
-            choices.append(choice)
-
-            choice = {
-                "choice": "Total",
-                "votes": {
-                    "total": {
-                        "all": question_votes.count(),
-                        "users_all": users.count(),
-                        "all_percent": perc(question_votes.count(), users.count()),
-                        "male": sum([u.is_male for u in users]),
-                        "female": sum([u.is_female for u in users]),
-                    }
-                }
-            }
-            for yr in range(9, 14):
-                yr_votes = [u if u.grade and u.grade.number == yr else None for u in users]
-                yr_votes = list(filter(None, yr_votes))
-                choice["votes"][yr] = {"all": len(yr_votes),
-                                       "male": sum([u.is_male for u in yr_votes]),
-                                       "female": sum([u.is_female for u in yr_votes])}
-
-            choices.append(choice)
-
-            question = {"question": q, "choices": choices}
-            questions.append(question)
+            questions.append(handle_choice(q))
         elif q.is_writing():
             answers = Answer.objects.filter(question=q)
             question = {"question": q, "answers": answers}
