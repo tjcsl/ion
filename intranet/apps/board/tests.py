ce1d6494fb7b3b03795d887e3a5904c6abfb82b1
Peter Foley <pefoley2@pefoley.com>
2016-03-22 23:41:30 -0400

    fix board test

--- intranet/apps/board/tests.py
+++ intranet/apps/board/tests.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.core.urlresolvers import reverse
+from ..users.models import Group, User
 
 from ...test.ion_test import IonTestCase
 
@@ -8,8 +9,16 @@ from ...test.ion_test import IonTestCase
 class BoardTest(IonTestCase):
     """Tests for the board module."""
 
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        # board is currently restricted to admins only.
+        group = Group.objects.get_or_create(name="admin_board")[0]
+        User.get_user(username='awilliam').groups.add(group)
+
     def test_get_board(self):
         self.login()
+
         response = self.client.get(reverse('board'))
         self.assertEqual(response.status_code, 200)
         response = self.client.get(reverse('board_class', args=[9001]))
@@ -25,5 +34,6 @@ class BoardTest(IonTestCase):
 
     def test_modify_board(self):
         self.login()
+
         response = self.client.post(reverse('modify_boardpost', args=[9001]))
         self.assertEqual(response.status_code, 404)
