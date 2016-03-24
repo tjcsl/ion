2353ef5199b6031f614a770ef3e74bf581234766
James Woglom <2016jwoglom@tjhsst.edu>
2016-03-23 23:29:00 -0400

    event toggle

---
+++ intranet/static/js/dashboard/events.js
@@ -0,0 +1,47 @@
+$(function() {
+    function eventToggle() {
+        var event = $(this).parent().parent().parent();
+        var eventContent = $(".event-toggle-content", event);
+        var icon = $(this).children(0);
+        var id = event.attr("data-id");
+
+        if (!id) {
+            console.error("Couldn't toggle invalid event ID");
+            return;
+        }
+
+        var hidden = event.hasClass("hidden");
+        var action = hidden ? "show" : "hide";
+        $.post("/events/" + action + "?" + id, {event_id: id}, function() {
+            console.info("event "+id+" "+action);
+        });
+        if (action === "show") {
+            icon.removeClass("fa-toggle-off")
+                    .addClass("fa-toggle-on")
+                    .attr("title", icon.attr("data-visible-title"));
+            setTimeout(function() {
+                event.removeClass("hidden");
+            }, 450);
+            eventContent.slideDown(350);
+        } else {
+            icon.removeClass("fa-toggle-on")
+                    .addClass("fa-toggle-off")
+                    .attr("title", icon.attr("data-hidden-title"));
+            setTimeout(function() {
+                event.addClass("hidden");
+            }, 450);
+            eventContent.slideUp(350);
+        }
+    };
+
+    $(".event[data-id] h3").click(function(e) {
+        if(e.target !== this) {return;}
+        var btn = $(".event-toggle", $(this));
+        eventToggle.call(btn);
+    });
+
+    $(".event[data-id] h3 .event-toggle").click(function(e) {
+        e.preventDefault();
+        eventToggle.call($(this));
+    });
+});
