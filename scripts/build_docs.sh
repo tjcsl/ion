d04f14d5b17379d7c6abc9814c3914e56073ffb0
Peter Foley <pefoley2@pefoley.com>
2016-04-06 11:46:20 -0400

    autogenerated

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -4,3 +4,9 @@ excludes="intranet/settings/secret.py intranet/urls.py intranet/apps/*/migration
 sphinx-apidoc -f -o build/.tmp intranet $excludes
 rsync --checksum build/.tmp/*.rst docs/sourcedoc/
 sphinx-build -W docs -d build/sphinx/doctrees build/sphinx/html
+if ! [ -z "`git status --porcelain`" ]; then
+    echo "Modified files found, did you forget to add a file?"
+    git status --porcelain
+    exit 1
+fi
+
