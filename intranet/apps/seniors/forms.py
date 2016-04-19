2c2153f875125ed87ea5c1e86f96711921edee35
James Woglom <2016jwoglom@tjhsst.edu>
2016-04-18 21:06:57 -0400

    sure labeling

--- intranet/apps/seniors/forms.py
+++ intranet/apps/seniors/forms.py
@@ -12,7 +12,9 @@ class SeniorForm(forms.ModelForm):
         self.fields["college"].help_text = "CollegeBoard CEEB numbers are in parentheses."
         self.fields["college"].empty_label = "Undecided"
         self.fields["college_sure"].label = "Sure?"
+        self.fields["college_sure"].help_text = "If you don't check this box, this school will be shown as unsure."
         self.fields["major_sure"].label = "Sure?"
+        self.fields["major_sure"].help_text = "If you don't check this box, this major will be shown as unsure."
 
     class Meta:
         model = Senior
