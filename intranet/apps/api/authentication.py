2f6da7bd5550ea84baddc369a9898af5afceea32
Peter Foley <pefoley2@pefoley.com>
2016-04-20 17:08:36 -0400

    hide password

--- intranet/apps/api/authentication.py
+++ intranet/apps/api/authentication.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import auth
+from django.views.decorators.debug import sensitive_variables
 
 from rest_framework import authentication, exceptions
 
 
 class ApiBasicAuthentication(authentication.BasicAuthentication):
 
+    @sensitive_variables('password')
     def authenticate_credentials(self, userid, password):
         """Authenticate the userid and password."""
 
