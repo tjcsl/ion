346fe5b1e48db9daea333332dfbaee0626b42e71
Eric Wang <ezwang1@gmail.com>
2016-06-09 20:14:00 -0400

    fixture script print more info

--- intranet/apps/api/management/commands/import_fixtures.py
+++ intranet/apps/api/management/commands/import_fixtures.py
@@ -11,6 +11,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("-f", "--folder", default=None, help="The folder to import fixtures from.")
+        parser.add_argument("-t", action="store_true", dest="test", default=False, help="List the fixture order without importing the fixtures.")
 
     def handle(self, *args, **options):
         verbosity = options.get("verbosity", 1)
@@ -31,10 +32,17 @@ class Command(BaseCommand):
         # Sort by first four characters of file name.
         fixtures = sorted(fixtures, key=lambda item: int(item.split("/")[-1][:4]))
 
+        if options["test"]:
+            for x in fixtures:
+                print(x[len(fixtures_folder):])
+            return
+
         # Import fixtures
         for x in fixtures:
             try:
                 call_command("loaddata", x)
+                if verbosity > 1:
+                    print("Loaded " + x)
             except Exception as e:
                 print("Failed to import %s (%s)" % (x, type(e).__name__))
                 if verbosity > 1:
