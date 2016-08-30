c45bed6e6489b1ddd866b0d23fbb2f4f8bef91e8
Peter Foley <pefoley2@pefoley.com>
2016-08-29 20:04:01 -0400

    try and fix wierd sphinx issues

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -3,6 +3,7 @@ echo "Building docs..."
 excludes="intranet/settings/secret.py intranet/urls.py intranet/apps/*/migrations"
 sphinx-apidoc -f -o build/.tmp intranet $excludes
 rsync --checksum build/.tmp/*.rst docs/sourcedoc/
+rm -f build/sphinx/searchindex.js
 sphinx-build -W docs -d build/sphinx/doctrees build/sphinx/html
 if ! [ -z "`git status --porcelain`" ]; then
     echo "Modified files found, did you forget to add a file?"
