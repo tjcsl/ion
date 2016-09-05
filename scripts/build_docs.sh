a14bb2633a348d2576aae9e326afb44b569103e1
Peter Foley <pefoley2@pefoley.com>
2016-09-04 21:52:28 -0400

    Revert "fix search index?"
    Fixed in 1.4.7
    (https://github.com/sphinx-doc/sphinx/commit/acd4d9f2ba42baec9e538b6f16f18efe2e595286)
    
    This reverts commit 003097307f4d86fe209c9f4855cc402a6d517973.

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -1,9 +1,9 @@
 set -e
 echo "Building docs..."
 excludes="intranet/settings/secret.py intranet/urls.py intranet/apps/*/migrations"
-sphinx-apidoc -f -o docs/sourcedoc intranet $excludes
-#rsync --checksum build/.tmp/*.rst docs/sourcedoc/
-#rm -f build/sphinx/html/searchindex.js
+sphinx-apidoc -f -o build/.tmp intranet $excludes
+rsync --checksum build/.tmp/*.rst docs/sourcedoc/
+rm -f build/sphinx/html/searchindex.js
 sphinx-build -W docs -d build/sphinx/doctrees build/sphinx/html
 if ! [ -z "`git status --porcelain`" ]; then
     echo "Modified files found, did you forget to add a file?"
