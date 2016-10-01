ab21c796022d0db61bfec89e3f00fe92e9f74ea4
Eric Wang <ezwang1@gmail.com>
2016-10-01 18:43:18 -0400

    add teacher management frontend

--- intranet/apps/eighth/urls.py
+++ intranet/apps/eighth/urls.py
@@ -58,7 +58,8 @@ eighth_admin_patterns = [
     url(r"^maintenance/clear_comments$", admin_maintenance.clear_comments_view, name="eighth_admin_maintenance_clear_comments"),
     url(r"^maintenance/start_of_year$", admin_maintenance.start_of_year_view, name="eighth_admin_maintenance_start_of_year"),
     url(r"^maintenance/sis_import$", admin_maintenance.sis_import, name="eighth_admin_maintenance_sis_import"),
-    url(r"^maintenance/teacher_management", admin_maintenance.teacher_management, name="eighth_admin_maintenance_teacher_management"),
+    url(r"^maintenance/teacher$", admin_maintenance.teacher_management, name="eighth_admin_maintenance_teacher"),
+    url(r"^maintenance/teacher/list$", admin_maintenance.teacher_list, name="eighth_admin_maintenance_teacher_list"),
 
     # Blocks
     url(r"^blocks/add$", blocks.add_block_view, name="eighth_admin_add_block"),
