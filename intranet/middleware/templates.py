6496bc038795c5c4b68ae99f451edbdfe7383984
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-11-10 00:20:04 -0500

    all links are now set with rel=noopener noreferrer to prevent malicious js from editing the referring page ## To make a commit, type your commit message and press SUPER-ENTER. To cancel ## the commit, close the window. To sign off on the commit, press SUPER-S. ## ## You may also reference or close a GitHub issue with this commit.  To do so, ## type `#` followed by the `tab` key.  You will be shown a list of issues ## related to the current repo.  You may also type `owner/repo#` plus the `tab` ## key to reference an issue in a different GitHub repo.

--- intranet/middleware/templates.py
+++ intranet/middleware/templates.py
@@ -9,6 +9,7 @@ logger = logging.getLogger(__name__)
 
 
 class StripNewlinesMiddleware(object):
+
     """Strip extra newlines from rendered templates to enhance readability."""
 
     def __init__(self, get_response):
@@ -25,6 +26,7 @@ class StripNewlinesMiddleware(object):
 
 
 class AdminSelectizeLoadingIndicatorMiddleware(object):
+
     """Automatically add a loading placeholder for Selectize inputs in admin templates.
 
     This is probably not a good practice, but it really needs to be done
@@ -48,4 +50,19 @@ class AdminSelectizeLoadingIndicatorMiddleware(object):
                 """
 
             response.content = re.sub(r'</select>', replacement, response.content.decode())
+        return response
+
+
+class NoReferrerMiddleware(object):
+
+    """Set all links to have rel='noreferrer noopener' to prevent malicious JS from editing what the user sees"""
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        is_html = (response["Content-Type"] == "text/html" or response["Content-Type"].startswith("text/html;"))
+        if is_html:
+            response.content = re.sub(r'<a(.*)>', r'<a rel="noopener noreferrer"\1>', response.content.decode())
         return response
