2f0d637f34722f67c4a38cc7ca0517ebc8ddf722
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-12-18 23:41:54 -0500

    override update view to disable skip_authorization

---
+++ intranet/apps/oauth/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from oauth2_provider.views.application import ApplicationUpdate
+
+
+class ApplicationUpdateView(ApplicationUpdate):
+    fields = ["name",
+              "client_id",
+              "client_secret",
+              "client_type",
+              "authorization_grant_type",
+              "redirect_uris"]
