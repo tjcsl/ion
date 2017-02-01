7c0fb4ec256b9444e892724653f3aca6efca4719
Eric Wang <ezwang1@gmail.com>
2017-01-31 21:37:49 -0500

    move waitlist to start date position

---
+++ intranet/static/js/eighth/waitlist.toggle.js
@@ -0,0 +1,23 @@
+$(function() {
+    $("#toggle-waitlist").click(function(e) {
+        e.preventDefault();
+        var target = $( e.target );
+        target.attr("disabled", true);
+        $.ajax(toggle_waitlist_endpoint, {
+            success: function() {
+                if(target.attr("title").indexOf("Disable") === -1) {
+                    target.attr("title", "Disable Waitlist");
+                    target.text("Disable Waitlist");
+                }
+                else {
+                    target.attr("title", "Enable Waitlist");
+                    target.text("Enable Waitlist");
+                }
+                target.attr("disabled", false);
+            },
+            error: function() {
+                alert("There was an error toggling the waitlist for the current session.");
+            }
+        });
+    });
+});
