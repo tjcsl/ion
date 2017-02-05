70e8098687c3975b2c01dce21d55cd8edcb5c190
ovkulkarni <ovkulkarni@users.noreply.github.com>
2017-02-05 14:16:56 -0500

    show correct age on birthday widget

--- intranet/apps/dashboard/views.py
+++ intranet/apps/dashboard/views.py
@@ -228,9 +228,9 @@ def find_birthdays(request):
                         "grade": {
                             "name": u.grade.name
                         },
-                        "age": (u.age + yr_inc) if u.age is not None else -1
+                        "age": u.age + yr_inc if u.age is not None else -1
                     } if u else {} for u in User.objects.users_with_birthday(today.month, today.day)],
-                    "inc": 0
+                    "inc": 1
                 },
                 "tomorrow": {
                     "date": tomorrow,
@@ -240,7 +240,7 @@ def find_birthdays(request):
                         "grade": {
                             "name": u.grade.name
                         },
-                        "age": (u.age - 1)
+                        "age": u.age + yr_inc if u.age is not None else -1
                     } for u in User.objects.users_with_birthday(tomorrow.month, tomorrow.day)],
                     "inc": 1
                 }
