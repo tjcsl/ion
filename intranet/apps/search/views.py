5fc8a31ac8cff65fa4d8a67da502ed3f241bc8f1
Naitian Zhou <nzhou2011@gmail.com>
2017-08-04 14:41:19 -0400

    Fix Search

--- intranet/apps/search/views.py
+++ intranet/apps/search/views.py
@@ -4,6 +4,7 @@ import logging
 
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import redirect, render
 
 from ..announcements.models import Announcement
@@ -18,244 +19,203 @@ logger = logging.getLogger(__name__)
 USE_SID_LDAP = True
 
 
-def do_ldap_query(q, admin=False):
-    pass
-    # c = LDAPConnection()
-    # result_dns = []
+def query(q, admin=False):
+    # If only a digit, search for student ID and user ID
+    results = []
+    if q.isdigit():
+        logger.debug("Digit search: {}".format(q))
+        sid_users = User.objects.filter(student_id=q)
+        uid_users = User.objects.filter(id=q)
+        for u in sid_users:
+            results.append(u)
 
-    # q = escape_filter_chars(q)
-    # # Allow wildcards
-    # q = q.replace(escape_filter_chars('*'), '*')
+        for u in uid_users:
+            results.append(u)
+    elif ":" in q:
+        logger.debug("Advanced search")
+        # A mapping between search keys and LDAP entires
+        map_attrs = {
+            "firstname": (
+                "first_name",
+                "nickname",),
+            "first": (
+                "first_name",
+                "nickname",),
+            "lastname": ("last_name",),
+            "last": ("sn",),
+            "nick": ("nickname",),
+            "nickname": ("nickname",),
+            "name": (
+                "last_name",
+                "middle_name",
+                "first_name",
+                "nickname",),
+            "city": ("address__city",),
+            "town": ("address__city",),
+            "middlename": ("middle_name",),
+            "middle": ("middle_name",),
+            "phone": ("phones__number",),
+            "homephone": ("phones__number",),
+            "cell": ("phones__number",),
+            "address": ("address__street",),
+            "zip": ("address__postal_code",),
+            "grade": ("graduation_year",),
+            "gradyear": ("graduation_year",),
+            "email": ("emails__address",),
+            "studentid": ("student_id",),
+            "sex": ("gender",),
+            "gender": ("gender",),
+            "id": ("id",),
+            "username": ("username",),
+            "counselor": ("counselor",),
+            "type": ("usertype",)
+        }
 
-    # # If only a digit, search for student ID and user ID
-    # if q.isdigit():
-    #     logger.debug("Digit search: {}".format(q))
-    #     if USE_SID_LDAP:
-    #         query = ("(&(|(tjhsstStudentId={0})" "(iodineUidNumber={0})" ")(|(objectClass=tjhsstStudent)(objectClass=tjhsstTeacher)))").format(q)
+        inner = ""
+        parts = q.split(" ")
+        # split each word
+        search_query = Q(pk=-1)  # Initial query to avoid an empty Q() object.
+        for p in parts:
+            # Check for less than/greater than, and replace =
+            sep = "__icontains"
+            if ":" in p:
+                cat, val = p.split(":")
+                sep = ""
+            elif "=" in p:
+                cat, val = p.split("=")
+                sep = ""
+            elif "<" in p:
+                cat, val = p.split("<")
+                sep = "__lte"
+            elif ">" in p:
+                cat, val = p.split(">")
+                sep = "__gte"
+            else:
+                logger.debug("Advanced fallback: {}".format(p))
+                # Fall back on regular searching (there's no key)
 
-    #         logger.debug("Running LDAP query: {}".format(query))
+                # Wildcards are already implied at the start and end
+                if p.endswith("*"):
+                    p = p[:-1]
+                if p.startswith("*"):
+                    p = p[1:]
 
-    #         res = c.search(settings.USER_DN, query, None)
-    #         for row in res:
-    #             dn = row["dn"]
-    #             result_dns.append(dn)
-    #     else:
-    #         sid_users = User.objects.filter(_student_id=q)
-    #         uid_users = User.objects.filter(id=q)
-    #         for u in sid_users:
-    #             result_dns.append(u.dn)
+                exact = False
+                if p.startswith('"') and p.endswith('"'):
+                    exact = True
+                    p = p[1:-1]
 
-    #         for u in uid_users:
-    #             result_dns.append(u.dn)
-    #         logger.debug("Running local DB query for UID and SID")
-    # elif ":" in q:
-    #     logger.debug("Advanced search")
-    #     # A mapping between search keys and LDAP entires
-    #     map_attrs = {
-    #         "firstname": (
-    #             "givenname",
-    #             "nickname",),
-    #         "first": (
-    #             "givenname",
-    #             "nickname",),
-    #         "lastname": ("sn",),
-    #         "last": ("sn",),
-    #         "nick": ("nickname",),
-    #         "nickname": ("nickname",),
-    #         "name": (
-    #             "sn",
-    #             "mname",
-    #             "givenname",
-    #             "nickname",),
-    #         "city": ("l",),
-    #         "town": ("l",),
-    #         "middlename": ("mname",),
-    #         "middle": ("mname",),
-    #         "phone": (
-    #             "homephone",
-    #             "mobile",),
-    #         "homephone": ("homephone",),
-    #         "cell": ("mobile",),
-    #         "address": ("street",),
-    #         "zip": ("postalcode",),
-    #         "grade": ("graduationYear",),
-    #         "gradyear": ("graduationYear",),
-    #         "email": ("mail",),
-    #         "studentid": ("tjhsstStudentId",),
-    #         "sex": ("sex",),
-    #         "gender": ("sex",),
-    #         "id": ("iodineUidNumber",),
-    #         "username": ("iodineUid",),
-    #         "counselor": ("counselor",),
-    #         "type": ("objectClass",)
-    #     }
+                if len(p) == 0:
+                    continue
 
-    #     inner = ""
-    #     parts = q.split(" ")
-    #     # split each word
-    #     for p in parts:
-    #         # Check for less than/greater than, and replace =
-    #         sep = "="
-    #         if ":" in p:
-    #             cat, val = p.split(":")
-    #         elif "=" in p:
-    #             cat, val = p.split("=")
-    #         elif "<" in p:
-    #             cat, val = p.split("<")
-    #             sep = "<="
-    #         elif ">" in p:
-    #             cat, val = p.split(">")
-    #             sep = ">="
-    #         else:
-    #             logger.debug("Advanced fallback: {}".format(p))
-    #             # Fall back on regular searching (there's no key)
+                default_categories = ["first_name", "last_name", "nickname", "id"]
+                if admin:
+                    default_categories.append("middle_name")
+                if exact:
+                    # No implied wildcard
+                    for cat in default_categories:
+                        search_query |= Q(**{cat: p})
 
-    #             # Wildcards are already implied at the start and end
-    #             if p.endswith("*"):
-    #                 p = p[:-1]
-    #             if p.startswith("*"):
-    #                 p = p[1:]
+                else:
+                    # Search firstname, lastname, uid, nickname (+ middlename if admin) with
+                    # implied wildcard at beginning and end of the search
+                    # string
+                    for cat in default_categories:
+                        search_query |= Q(**{"{}__icontains".fomrat(cat): p})
 
-    #             exact = False
-    #             if p.startswith('"') and p.endswith('"'):
-    #                 exact = True
-    #                 p = p[1:-1]
+                continue  # skip rest of processing
 
-    #             if len(p) == 0:
-    #                 continue
+            logger.debug("Advanced exact: {}".format(p))
+            if val.startswith('"') and val.endswith('"'):
+                # Already exact
+                val = val[1:-1]
 
-    #             if exact:
-    #                 # No implied wildcard
-    #                 inner += (("(|(givenName={0})"
-    #                            "(sn={0})"
-    #                            "(iodineUid={0})") + ("(mname={0})" if admin else "") + ("(nickname={0})"
-    #                                                                                     ")")).format(p)
-    #             else:
-    #                 # Search firstname, lastname, uid, nickname (+ middlename if admin) with
-    #                 # implied wildcard at beginning and end of the search
-    #                 # string
-    #                 inner += (("(|(givenName=*{0})"
-    #                            "(givenName={0}*)"
-    #                            "(sn=*{0})"
-    #                            "(sn={0}*)"
-    #                            "(iodineUid=*{0})"
-    #                            "(iodineUid={0}*)") + ("(mname=*{0})"
-    #                                                   "(mname={0}*)" if admin else "") + ("(nickname=*{0})"
-    #                                                                                       "(nickname={0}*)"
-    #                                                                                       ")")).format(p)
+            cat = cat.lower()
+            val = val.lower()
 
-    #             continue  # skip rest of processing
-    #         logger.debug("Advanced exact: {}".format(p))
-    #         if val.startswith('"') and val.endswith('"'):
-    #             # Already exact
-    #             val = val[1:-1]
+            # fix grade, because LDAP only stores graduation year
+            if cat == "grade" and val.isdigit():
+                val = "{}".format(Grade.year_from_grade(int(val)))
+            elif cat == "grade" and val == "staff":
+                cat = "type"
+                val = "teacher"
+            elif cat == "grade" and val == "student":
+                cat = "type"
+                val = "student"
 
-    #         cat = cat.lower()
-    #         val = val.lower()
+            if cat == "type" and val == "teacher":
+                val = "teacher"
+            elif cat == "type" and val == "student":
+                val = "student"
 
-    #         # fix grade, because LDAP only stores graduation year
-    #         if cat == "grade" and val.isdigit():
-    #             val = "{}".format(Grade.year_from_grade(int(val)))
-    #         elif cat == "grade" and val == "staff":
-    #             cat = "type"
-    #             val = "teacher"
-    #         elif cat == "grade" and val == "student":
-    #             cat = "type"
-    #             val = "student"
+            # replace sex:male with sex:m and sex:female with sex:f
+            if cat == "sex" or cat == "gender":
+                val = val[:1]
+                val = True if val == 'm' else False
 
-    #         if cat == "type" and val == "teacher":
-    #             val = "tjhsstTeacher"
-    #         elif cat == "type" and val == "student":
-    #             val = "tjhsstStudent"
+            # if an invalid key, ignore
+            if cat not in map_attrs:
+                continue
 
-    #         # replace sex:male with sex:m and sex:female with sex:f
-    #         if cat == "sex" or cat == "gender":
-    #             val = val[:1]
+            attrs = map_attrs[cat]
 
-    #         # if an invalid key, ignore
-    #         if cat not in map_attrs:
-    #             continue
+            inner += "(|"
+            # for each of the possible LDAP fields, add to the search query
+            for attr in attrs:
+                search_query |= Q(**{"{}{}".format(attr, sep): val})
 
-    #         attrs = map_attrs[cat]
+        try:
+            results = User.objects.filter(search_query)
+            for result in results:
+                results.append(result)
+        except:
+            # TODO: handle this error
+            pass
+    else:
+        logger.debug("Simple search")
+        # Non-advanced search; no ":"
+        parts = q.split(" ")
+        # split on each word
+        search_query = Q(pk=-1)  # Initial query to avoid an empty Q() object.
+        for p in parts:
+            exact = False
+            logger.debug(p)
+            if p.startswith('"') and p.endswith('"'):
+                exact = True
+                p = p[1:-1]
+            default_categories = ["first_name", "last_name", "nickname", "id"]
+            if admin:
+                default_categories.append("middle_name")
+            if exact:
+                logger.debug("Simple exact: {}".format(p))
+                # No implied wildcard
+                for cat in default_categories:
+                    search_query |= Q(**{cat: p})
+            else:
+                logger.debug("Simple wildcard: {}".format(p))
+                if p.endswith("*"):
+                    p = p[:-1]
+                if p.startswith("*"):
+                    p = p[1:]
+                # Search for first, last, middle, nickname uid, with implied
+                # wildcard at beginning and end
+                for cat in default_categories:
+                    search_query |= Q(**{"{}__icontains".format(cat): p})
 
-    #         inner += "(|"
-    #         # for each of the possible LDAP fields, add to the search query
-    #         for attr in attrs:
-    #             inner += "({}{}{})".format(attr, sep, val)
-    #         inner += ")"
+            logger.debug("Running query: {}".format(search_query))
 
-    #     query = "(&{}(|(objectClass=tjhsstStudent)(objectClass=tjhsstTeacher)))".format(inner)
+            res = User.objects.filter(search_query)
+            # if multiple words, delete those that weren't in previous searches
+            for user in res:
+                results.append(user)
 
-    #     logger.debug("Running LDAP query: {}".format(query))
+    # loop through the DNs saved and get actual user objects
+    users = []
+    for user in results:
+        if user.is_active:
+            users.append(user)
 
-    #     res = c.search(settings.USER_DN, query, None)
-    #     for row in res:
-    #         dn = row["dn"]
-    #         result_dns.append(dn)
-
-    # else:
-    #     logger.debug("Simple search")
-    #     # Non-advanced search; no ":"
-    #     parts = q.split(" ")
-    #     # split on each word
-    #     i = 0
-    #     for p in parts:
-    #         exact = False
-    #         logger.debug(p)
-    #         if p.startswith('"') and p.endswith('"'):
-    #             exact = True
-    #             p = p[1:-1]
-
-    #         if exact:
-    #             logger.debug("Simple exact: {}".format(p))
-    #             # No implied wildcard
-    #             query = (("(&(|(givenName={0})"
-    #                       "(sn={0})"
-    #                       "(iodineUid={0})") + ("(mname={0})"
-    #                                             if admin else "") + ("(nickname={0})"
-    #                                                                  ")(|(objectClass=tjhsstStudent)(objectClass=tjhsstTeacher)))")).format(p)
-    #         else:
-    #             logger.debug("Simple wildcard: {}".format(p))
-    #             if p.endswith("*"):
-    #                 p = p[:-1]
-    #             if p.startswith("*"):
-    #                 p = p[1:]
-    #             # Search for first, last, middle, nickname uid, with implied
-    #             # wildcard at beginning and end
-    #             query = (("(&(|(givenName=*{0})"
-    #                       "(givenName={0}*)"
-    #                       "(sn=*{0})"
-    #                       "(sn={0}*)"
-    #                       "(iodineUid=*{0})"
-    #                       "(iodineUid={0}*)") + ("(mname=*{0})"
-    #                                              "(mname={0}*)"
-    #                                              if admin else "") + ("(nickname=*{0})"
-    #                                                                   "(nickname={0}*)"
-    #                                                                   ")(|(objectClass=tjhsstStudent)(objectClass=tjhsstTeacher)))")).format(p)
-
-    #         logger.debug("Running LDAP query: {}".format(query))
-
-    #         res = c.search(settings.USER_DN, query, None)
-    #         new_dns = []
-    #         # if multiple words, delete those that weren't in previous searches
-    #         for row in res:
-    #             dn = row["dn"]
-    #             if i == 0:
-    #                 new_dns.append(dn)
-    #             elif dn in result_dns:
-    #                 new_dns.append(dn)
-
-    #         result_dns = new_dns
-    #         i += 1
-
-    # # loop through the DNs saved and get actual user objects
-    # users = []
-    # for dn in result_dns:
-    #     user = User.get_user(dn=dn)
-    #     if user.is_active:
-    #         users.append(user)
-
-    # return users
+    return users
 
 
 def get_search_results(q, admin=False):
@@ -264,65 +224,11 @@ def get_search_results(q, admin=False):
 
     for qu in q.split(" OR "):
         try:
-            users += do_ldap_query(qu, admin)
+            users += query(qu, admin)
         except ValueError:
             raise Exception("Invalid query")
 
     return False, users
-
-
-def do_classes_search(q):
-    c = LDAPConnection()
-    result_dns = []
-    q = escape_filter_chars(q)
-    q = q.replace(escape_filter_chars('*'), '*')
-    parts = q.split(" ")
-    # split on each word
-    i = 0
-    for p in parts:
-        exact = False
-        logger.debug(p)
-        if p.startswith('"') and p.endswith('"'):
-            exact = True
-            p = p[1:-1]
-
-        if exact:
-            logger.debug("Simple exact: {}".format(p))
-            # No implied wildcard
-            query = (("(&(|(tjhsstClassId={0})" "(cn={0})" ")(|(objectClass=tjhsstClass)))")).format(p)
-        else:
-            logger.debug("Simple wildcard: {}".format(p))
-            if p.endswith("*"):
-                p = p[:-1]
-            if p.startswith("*"):
-                p = p[1:]
-            # Search for first, last, middle, nickname uid, with implied
-            # wildcard at beginning and end
-            query = (("(&(|(tjhsstClassId=*{0})" "(tjhsstClassId={0}*)" "(cn=*{0})" "(cn={0}*)" ")(|(objectClass=tjhsstClass)))")).format(p)
-
-        logger.debug("Running LDAP query: {}".format(query))
-
-        res = c.search(settings.CLASS_DN, query, None)
-        new_dns = []
-        # if multiple words, delete those that weren't in previous searches
-        for row in res:
-            dn = row["dn"]
-            if i == 0:
-                new_dns.append(dn)
-            elif dn in result_dns:
-                new_dns.append(dn)
-
-        result_dns = new_dns
-        i += 1
-
-    # loop through the DNs saved and get actual user objects
-    classes = []
-    for dn in result_dns:
-        result_class = Class(dn=dn)
-        if result_class not in classes:
-            classes.append(result_class)
-    classes = sorted(classes, key=lambda c: str(c))
-    return classes
 
 
 def do_activities_search(q):
@@ -380,7 +286,6 @@ def search_view(request):
         activities = do_activities_search(q)
         announcements = do_announcements_search(q)
         events = do_events_search(q)
-        classes = do_classes_search(q)
 
         logger.debug(activities)
         logger.debug(announcements)
@@ -399,7 +304,6 @@ def search_view(request):
             "announcements": announcements,  # Announcement objects
             "events": events,  # Event objects
             "activities": activities,  # EighthActivity objects
-            "classes": classes  # Class from LDAP
         }
     else:
         context = {"search_results": None}
