88ace5098f8344adb34618535654b1de78f1ad6a
ovkulkarni <ovkulkarni@users.noreply.github.com>
2017-08-25 00:11:12 -0400

    allow characters in room

--- intranet/apps/users/class_urls.py
+++ intranet/apps/users/class_urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     url(r"^$", views.all_courses_view, name="all_courses"),
-    url(r"^/room/(?P<room_number>\d+)$", views.sections_by_room_view, name="room_sections"),
+    url(r"^/room/(?P<room_number>.*)$", views.sections_by_room_view, name="room_sections"),
     url(r"^/period/(?P<period_number>\d+)$", views.courses_by_period_view, name="period_courses"),
     url(r"^/course/(?P<course_id>.*)$", views.course_info_view, name="course_sections"),
     url(r"^/section/(?P<section_id>.*)$", views.course_section_view, name="class_section"),
