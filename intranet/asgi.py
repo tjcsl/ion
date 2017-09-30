c9261847bb8205677a8300f522f3c2ce812e692b
Naitian Zhou <nzhou2011@gmail.com>
2017-09-30 16:23:30 -0400

    Prepare asgi.py for Prod

--- intranet/asgi.py
+++ intranet/asgi.py
@@ -1,5 +1,5 @@
 import os
 import channels.asgi
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "intranet.settings")
 channel_layer = channels.asgi.get_channel_layer()
