98b3f6471dc8f6d8a99d6d8f1a2196a8eebfac00
Naitian Zhou <nzhou2011@gmail.com>
2018-01-01 15:34:07 -0500

    Add Route Import Script

---
+++ intranet/apps/bus/management/commands/import_routes.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import csv
+
+from django.core.management.base import BaseCommand
+
+from ...models import Route
+
+
+class Command(BaseCommand):
+    help = "Import Routes from routes.csv"
+
+    def handle(self, *args, **options):
+        reader = csv.reader(open("routes.csv", "r"))
+        for row in reader:
+            name, = row
+            Route.objects.create(route_name=name, status="o")
