2718f402c33d89301fef6820bdb9f192be414f0b
Dylan Jones <dylanjones2011@gmail.com>
2018-02-05 18:02:03 -0500

    try to fix formatting

--- intranet/apps/bus/api.py
+++ intranet/apps/bus/api.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from django.conf import settings
-
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 
 from .models import Route
 from .serializers import RouteSerializer
+
 
 class RouteList(generics.ListAPIView):
     """API endpoint that retrieves information about buses.
@@ -24,6 +22,7 @@ class RouteList(generics.ListAPIView):
         print("ListView used!")
         return Route.objects.all()
 
+
 class RouteDetail(generics.RetrieveAPIView):
     """API endpoint that retrieves information about a specific bus route.
 
@@ -38,12 +37,3 @@ class RouteDetail(generics.RetrieveAPIView):
     def get_queryset(self):
         print("RetrieveUsed")
         return Route.objects.all()
-
-    #def retrieve(self, request, *args, **kwargs):
-    #    # If they requested a specific route, only return that one
-    #    route = Route.objects.get(num=kwargs['num'])
-    #    serializer = self.get_serializer(route)
-    #    data = serializer.data
-    #    log.debug("data={}".format(data))
-    #    return Response(data)
-        
--- intranet/apps/bus/serializers.py
+++ intranet/apps/bus/serializers.py
@@ -2,12 +2,8 @@ from rest_framework import serializers
 
 from .models import Route
 
-class RouteSerializer(serializers.Serializer):
-    #route_name = models.CharField(max_length=30, unique=True)
-    #space = models.CharField(max_length=4, blank=True)
-    #bus_number = models.CharField(max_length=5, blank=True)
-    #status = models.CharField('arrival status', choices=ARRIVAL_STATUSES, max_length=1, default='o')
 
+class RouteSerializer(serializers.Serializer):
     route_name = serializers.CharField(max_length=30)
     space = serializers.CharField(max_length=4)
     bus_number = serializers.CharField(max_length=5)
