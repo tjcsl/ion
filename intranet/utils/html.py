500d8334b58085d56bd291636eee85ff85f29f7c
Naitian Zhou <nzhou2011@gmail.com>
2018-03-02 11:34:45 -0500

    Allow Styles in Announcement Images

--- intranet/utils/html.py
+++ intranet/utils/html.py
@@ -2,15 +2,48 @@
 
 import bleach
 
-ALLOWED_TAGS = ['a', 'abbr', 'acronym', 'b', 'br', 'blockquote', 'code', 'em', 'hr', 'i', 'li', 'ol', 'strong', 'ul', 'iframe', 'img', 'div', 'p']
+ALLOWED_TAGS = ['a',
+                'abbr',
+                'acronym',
+                'b',
+                'br',
+                'blockquote',
+                'code',
+                'em',
+                'hr',
+                'i',
+                'li',
+                'ol',
+                'strong',
+                'ul',
+                'iframe',
+                'img',
+                'div',
+                'p']
+
 ALLOWED_ATTRIBUTES = {
     'acronym': ['title'],
     'a': ['href', 'title'],
     'abbr': ['title'],
     'iframe': ['src', 'height', 'width', 'allowfullscreen', 'frameborder'],
-    'img': ['src', 'alt', 'title']
+    'img': ['src', 'alt', 'title', 'style']
 }
+
+ALLOWED_STYLES = ['width',
+                  'height',
+                  'float',
+                  'margin-left',
+                  'margin-right',
+                  'margin-top',
+                  'margin-bottom',
+                  'margin',
+                  'border',
+                  'border-style',
+                  'border-width']
 
 
 def safe_html(txt):
-    return bleach.linkify(bleach.clean(txt, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES))
+    return bleach.linkify(bleach.clean(txt,
+                                       tags=ALLOWED_TAGS,
+                                       attributes=ALLOWED_ATTRIBUTES,
+                                       styles=ALLOWED_STYLES))
