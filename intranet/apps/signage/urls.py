a66352c0afd1baad78ebc503698353826a6606ed
Naitian Zhou <nzhou2011@gmail.com>
2018-03-29 17:21:35 -0400

    Signage Tweaks

--- intranet/apps/signage/urls.py
+++ intranet/apps/signage/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     url(r"^/serviceworker\.js$", TemplateView.as_view(template_name="signage/serviceworker.js",
                                                       content_type="text/javascript"), name="signage-serviceworker"),
     url(r"^/display/(?P<display_id>[\w_-]+)?$", views.signage_display, name="signage_display"),
-    url(r"^/eighth(?:/(?P<block_id>\d+))?$", views.eighth_signage, name="eighth_signage"),
+    url(r"^/page/eighth(?:/(?P<block_id>\d+))?$", views.eighth_signage, name="eighth_signage"),
 ]
