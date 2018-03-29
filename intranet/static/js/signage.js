d27e38646bb523a05a8e253e895f3d9af44e7c8d
Naitian Zhou <nzhou2011@gmail.com>
2018-03-28 20:04:49 -0400

    Fancy Styles and Nifty Animations

--- intranet/static/js/signage.js
+++ intranet/static/js/signage.js
@@ -2,6 +2,7 @@
 
 var networkDataReceived = false;
 var offline = false;
+var active = null;
 
 updatePage = function (data) {
     console.log('Writing new page');
@@ -44,9 +45,20 @@ reloadPage = function () {
     fetchFromCache();
 };
 
+var setActive = function (id) {
+    $('.active').removeClass('active');
+    page = document.getElementById(id);
+    $(page).addClass('active');
+    active = id;
+};
+
 window.addEventListener('online', function () {
     console.log('online!');
     reloadPage();
+    if (active) {
+        console.log('setting active');
+        setActive(active);
+    }
 });
 
 window.addEventListener('offline', function () {
@@ -58,4 +70,22 @@ window.onload = function () {
     window.setTimeout(function () {
         reloadPage();
     }, 20 * 60 * 1000);
+
+    setInterval(function() {
+        var now = new Date();
+        var hr = now.getHours();
+        var ampm = 'AM';
+        if (hr === 0) {
+            hr = 12;
+        }
+        if (hr > 12) {
+            hr -= 12;
+            ampm = 'PM';
+        }
+        var min = now.getMinutes();
+        if (min < 10) {
+            min = '0' + min;
+        }
+        $('.time').html(hr + ':' + min + ' ' + ampm);
+    }, 1000);
 };
