a3e208bfbd98518b205906c2e076a222fbc0593b
Naitian Zhou <nzhou2011@gmail.com>
2018-03-28 22:35:33 -0400

    Add announcements app

--- intranet/apps/signage/pages.py
+++ intranet/apps/signage/pages.py
@@ -1,5 +1,12 @@
 # This file contains logic for server-side rendered pages
+from django.utils import timezone
+
+from ..announcements.models import Announcement
 
 
 def hello_world(page, sign):
     return {"message": "{} from {} says Hello".format(page.name, sign.name)}
+
+
+def announcements(page, sign):
+    return {"public_announcements": Announcement.objects.filter(groups__isnull=True, expiration_date__gt=timezone.now())}
