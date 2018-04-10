cc593e1ed3862ae6e45bb65e0b26cdd46e6553fa
Naitian Zhou <nzhou2011@gmail.com>
2018-04-09 21:18:46 -0400

    Add Page to Django Admin

--- intranet/apps/signage/admin.py
+++ intranet/apps/signage/admin.py
@@ -2,11 +2,16 @@
 
 from django.contrib import admin
 
-from .models import Sign
+from .models import Sign, Page
 
 
 class SignAdmin(admin.ModelAdmin):
     list_display = ('name', 'display')
 
 
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
 admin.site.register(Sign, SignAdmin)
+admin.site.register(Page, PageAdmin)
