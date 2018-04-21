b8b217d8d4a471762a11afe6f5023f1869487719
Peter Foley <pefoley2@pefoley.com>
2018-04-21 17:40:34 -0400

    django 2.0 fixes

--- intranet/apps/polls/tests.py
+++ intranet/apps/polls/tests.py
@@ -47,16 +47,16 @@ class ApiTest(IonTestCase):
 
         # Make sure the poll exists
         self.assertTrue(poll.exists())
-        self.assertEqual(Question.objects.filter(poll=poll).count(), 1)
-        self.assertEqual(Choice.objects.filter(question__poll=poll).count(), 3)
+        self.assertEqual(Question.objects.filter(poll=poll[0]).count(), 1)
+        self.assertEqual(Choice.objects.filter(question__poll=poll[0]).count(), 3)
 
         # Make sure that the user can vote in the poll
         response = self.client.post(reverse('poll_vote', kwargs={"poll_id": poll.first().id}), data={"question-1": 3})
 
         self.assertEqual(response.status_code, 200)
 
-        user_choice = Answer.objects.get(question=Question.objects.get(poll=poll), user=user).choice
-        self.assertEqual(user_choice, Choice.objects.get(question__poll=poll, info="Blue"))
+        user_choice = Answer.objects.get(question=Question.objects.get(poll=poll[0]), user=user).choice
+        self.assertEqual(user_choice, Choice.objects.get(question__poll=poll[0], info="Blue"))
 
         # Test poll deletion
         response = self.client.post(reverse('delete_poll', kwargs={"poll_id": poll.first().id}))
