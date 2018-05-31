ce469481ffb996ee389f89c82c37e3167673de2d
Travis <ahamilto+ion@tjhsst.edu>
2018-05-31 13:30:12 +0000

    Travis build 4444

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: 'v3 (Build #4439)',
+    VERSION: 'v3 (Build #4444)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
