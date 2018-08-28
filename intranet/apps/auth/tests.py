c47a1c441eb117df5169f4cb4b887776ee51ba5d
Theo Ouzhinski <touzhinski@gmail.com>
2018-08-28 19:33:46 -0400

    Add tests for auth

--- intranet/apps/auth/tests.py
+++ intranet/apps/auth/tests.py
@@ -2,8 +2,10 @@
 
 from io import StringIO
 
+from django.urls import reverse
 from django.core.management import call_command
 
+from ..users.models import User
 from ...test.ion_test import IonTestCase
 
 
@@ -15,3 +17,33 @@ class GrantAdminTest(IonTestCase):
         out = StringIO()
         call_command('grant_admin', 'awilliam', 'admin_all', stdout=out)
         self.assertEqual(out.getvalue().strip(), 'Added awilliam to admin_all')
+
+
+class LoginViewTest(IonTestCase):
+    """Tests of the login page (but not actually auth)"""
+
+    def test_login_page(self):
+        self.assertEqual(self.client.get(reverse("index")).status_code, 200)
+        self.assertEqual(self.client.get(reverse("about")).status_code, 200)
+        self.assertEqual(self.client.get(reverse("login")).status_code, 200)
+
+    def test_authentication(self):
+        User.objects.get_or_create(username="awilliam")
+        with self.settings(MASTER_PASSWORD='pbkdf2_sha256$24000$qp64pooaIEAc$j5wiTlyYzcMu08dVaMRus8Kyfvn5ZfaJ/Rn+Z/fH2Bw='):
+            response = self.client.post(reverse("index"), data={
+                "username": "awilliam",
+                "password": "dankmemes"
+            })
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_authentication_first_time(self):
+        user = User.objects.get_or_create(username="awilliam")[0]
+        user.first_login = False
+        with self.settings(MASTER_PASSWORD='pbkdf2_sha256$24000$qp64pooaIEAc$j5wiTlyYzcMu08dVaMRus8Kyfvn5ZfaJ/Rn+Z/fH2Bw='):
+            response = self.client.post(reverse("index"), data={
+                "username": "awilliam",
+                "password": "dankmemes"
+            })
+
+        self.assertEqual(response.status_code, 200)
