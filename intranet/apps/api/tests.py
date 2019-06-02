1d91820e1694d5e464a8831699ac7e50c954f6df
Theo Ouzhinski <touzhinski@gmail.com>
2019-06-02 18:46:27 -0400

    Fix tests

--- intranet/apps/api/tests.py
+++ intranet/apps/api/tests.py
@@ -21,44 +21,76 @@ class ApiTest(IonTestCase):
     """Tests for the api module."""
 
     def setUp(self):
-        self.user = User.objects.get_or_create(username="awilliam", graduation_year=(settings.SENIOR_GRADUATION_YEAR + 1))[0]
-        self.application = Application(name="Test Application", redirect_uris="http://localhost http://example.com http://example.it", user=self.user,
-                                       client_type=Application.CLIENT_CONFIDENTIAL, authorization_grant_type=Application.GRANT_AUTHORIZATION_CODE)
+        self.user = User.objects.get_or_create(
+            username="awilliam", graduation_year=(settings.SENIOR_GRADUATION_YEAR + 1)
+        )[0]
+        self.application = Application(
+            name="Test Application",
+            redirect_uris="http://localhost http://example.com http://example.it",
+            user=self.user,
+            client_type=Application.CLIENT_CONFIDENTIAL,
+            authorization_grant_type=Application.GRANT_AUTHORIZATION_CODE,
+        )
         self.application.save()
 
         self.client_credentials_application = Application(
-            name="Test Client Credentials Application", redirect_uris="http://localhost http://example.com http://example.it", user=self.user,
-            client_type=Application.CLIENT_CONFIDENTIAL, authorization_grant_type=Application.GRANT_CLIENT_CREDENTIALS)
+            name="Test Client Credentials Application",
+            redirect_uris="http://localhost http://example.com http://example.it",
+            user=self.user,
+            client_type=Application.CLIENT_CONFIDENTIAL,
+            authorization_grant_type=Application.GRANT_CLIENT_CREDENTIALS,
+        )
         self.client_credentials_application.save()
 
-        oauth2_settings._SCOPES = ['read', 'write']
+        oauth2_settings._SCOPES = ["read", "write"]
+
+    def make_token(self):
+        tok = AccessToken.objects.create(
+            user=self.user,
+            token="1234567890",
+            application=self.application,
+            scope="read write",
+            expires=timezone.now() + datetime.timedelta(days=1),
+        )
+        self.auth = "Bearer {}".format(tok.token)
 
     def test_get_emerg(self):
-        response = self.client.get(reverse('api_emerg_status'))
+        response = self.client.get(reverse("api_emerg_status"))
         self.assertEqual(response.status_code, 200)
 
     def test_get_profile(self):
-        self.login()
-        response = self.client.get(reverse('api_user_myprofile_detail'))
+        self.make_token()
+        response = self.client.get(
+            reverse("api_user_myprofile_detail"), HTTP_AUTHORIZATION=self.auth
+        )
         self.assertEqual(response.status_code, 200)
-        response = self.client.get(reverse('api_user_profile_detail', args=[9001]))
+        response = self.client.get(
+            reverse("api_user_profile_detail", args=[9001]), HTTP_AUTHORIZATION=self.auth
+        )
         self.assertEqual(response.status_code, 404)
 
     def test_get_announcements(self):
-        self.login()
-        response = self.client.get(reverse('api_announcements_list_create'))
+        self.make_token()
+        response = self.client.get(
+            reverse("api_announcements_list_create"), HTTP_AUTHORIZATION=self.auth
+        )
         self.assertEqual(response.status_code, 200)
-        response = self.client.get(reverse('api_announcements_detail', args=[9001]))
+        response = self.client.get(
+            reverse("api_announcements_detail", args=[9001]), HTTP_AUTHORIZATION=self.auth
+        )
         self.assertEqual(response.status_code, 404)
 
     def test_oauth_read(self):
-        tok = AccessToken.objects.create(user=self.user, token='1234567890', application=self.application, scope='read write',
-                                         expires=timezone.now() + datetime.timedelta(days=1))
-        auth = "Bearer {}".format(tok.token)
-        response = self.client.get(reverse('api_announcements_list_create'), HTTP_AUTHORIZATION=auth)
+
+        self.make_token()
+        response = self.client.get(
+            reverse("api_announcements_list_create"), HTTP_AUTHORIZATION=self.auth
+        )
         self.assertEqual(response.status_code, 200)
 
-        response = self.client.get(reverse('api_user_myprofile_detail'), HTTP_AUTHORIZATION=auth)
+        response = self.client.get(
+            reverse("api_user_myprofile_detail"), HTTP_AUTHORIZATION=self.auth
+        )
         self.assertEqual(response.status_code, 200)
         parsed_response = json.loads(response.content.decode())
         self.assertEqual(parsed_response["id"], int(self.user.id))
@@ -67,92 +99,103 @@ class ApiTest(IonTestCase):
     def test_oauth_write(self):
         self.make_admin()
 
-        tok = AccessToken.objects.create(user=self.user, token='1234567890', application=self.application, scope='read write',
-                                         expires=timezone.now() + datetime.timedelta(days=1))
+        self.make_token()
 
-        block = EighthBlock.objects.create(date=datetime.datetime(2015, 1, 1), block_letter='A')
+        block = EighthBlock.objects.create(date=datetime.datetime(2015, 1, 1), block_letter="A")
         room = EighthRoom.objects.create(name="room1", capacity=1)
 
-        act = EighthActivity.objects.create(name='Test Activity 1')
+        act = EighthActivity.objects.create(name="Test Activity 1")
         act.rooms.add(room)
         schact1 = EighthScheduledActivity.objects.create(activity=act, block=block)
 
-        auth = "Bearer {}".format(tok.token)
         response = self.client.post(
-            reverse('api_eighth_user_signup_list_myid'), {
-                "scheduled_activity": schact1.id,
-                "use_scheduled_activity": True
-            }, HTTP_AUTHORIZATION=auth)
+            reverse("api_eighth_user_signup_list_myid"),
+            {"scheduled_activity": schact1.id, "use_scheduled_activity": True},
+            HTTP_AUTHORIZATION=self.auth,
+        )
 
         self.assertEqual(response.status_code, 201)
         self.assertEqual(schact1.members.count(), 1)
 
     def test_oauth_client_credentials_read(self):
-        tok = AccessToken.objects.create(user=None, token='1234567890', application=self.client_credentials_application, scope='read write',
-                                         expires=timezone.now() + datetime.timedelta(days=1))
+        tok = AccessToken.objects.create(
+            user=None,
+            token="1234567890",
+            application=self.client_credentials_application,
+            scope="read write",
+            expires=timezone.now() + datetime.timedelta(days=1),
+        )
         auth = "Bearer {}".format(tok.token)
 
         # List announcements
-        response = self.client.get(reverse('api_announcements_list_create'), HTTP_AUTHORIZATION=auth)
+        response = self.client.get(
+            reverse("api_announcements_list_create"), HTTP_AUTHORIZATION=auth
+        )
         self.assertEqual(response.status_code, 200)
 
         # List emergency status
-        response = self.client.get(reverse('api_emerg_status'), HTTP_AUTHORIZATION=auth)
+        response = self.client.get(reverse("api_emerg_status"), HTTP_AUTHORIZATION=auth)
         self.assertEqual(response.status_code, 200)
 
         # List schedule
-        response = self.client.get(reverse('api_schedule_day_list'), HTTP_AUTHORIZATION=auth)
+        response = self.client.get(reverse("api_schedule_day_list"), HTTP_AUTHORIZATION=auth)
         self.assertEqual(response.status_code, 200)
 
         # List activities
-        response = self.client.get(reverse('api_eighth_activity_list'), HTTP_AUTHORIZATION=auth)
+        response = self.client.get(reverse("api_eighth_activity_list"), HTTP_AUTHORIZATION=auth)
         self.assertEqual(response.status_code, 200)
 
         # List blocks
-        response = self.client.get(reverse('api_eighth_block_list'), HTTP_AUTHORIZATION=auth)
+        response = self.client.get(reverse("api_eighth_block_list"), HTTP_AUTHORIZATION=auth)
         self.assertEqual(response.status_code, 200)
 
         # List specific block
-        block = EighthBlock.objects.create(date=datetime.datetime(2015, 1, 1), block_letter='A')
-        response = self.client.get(reverse('api_eighth_block_detail', kwargs={"pk": block.pk}), HTTP_AUTHORIZATION=auth)
+        block = EighthBlock.objects.create(date=datetime.datetime(2015, 1, 1), block_letter="A")
+        response = self.client.get(
+            reverse("api_eighth_block_detail", kwargs={"pk": block.pk}), HTTP_AUTHORIZATION=auth
+        )
         self.assertEqual(response.status_code, 200)
         resp = json.loads(response.content.decode())
         self.assertTrue("id" in resp)
 
         # Should not be able to list profile
-        response = self.client.get(reverse('api_user_myprofile_detail'), HTTP_AUTHORIZATION=auth)
+        response = self.client.get(reverse("api_user_myprofile_detail"), HTTP_AUTHORIZATION=auth)
         self.assertEqual(response.status_code, 403)
 
     def test_no_credentials_read(self):
         # Announcements should only be available to logged in users
-        response = self.client.get(reverse('api_announcements_list_create'))
+        response = self.client.get(reverse("api_announcements_list_create"))
         self.assertEqual(response.status_code, 401)
 
         # Activity list should only be available to logged in users
-        response = self.client.get(reverse('api_eighth_activity_list'))
+        response = self.client.get(reverse("api_eighth_activity_list"))
         self.assertEqual(response.status_code, 401)
 
         # Block list should only be available to logged in users
-        response = self.client.get(reverse('api_eighth_block_list'))
+        response = self.client.get(reverse("api_eighth_block_list"))
         self.assertEqual(response.status_code, 401)
 
     def test_api_root(self):
         # Should be able to read API root without authentication
-        response = self.client.get(reverse('api_root'))
+        response = self.client.get(reverse("api_root"))
         self.assertEqual(response.status_code, 200)
 
     def test_api_eighth_block_list(self):
-        tok = AccessToken.objects.create(user=self.user, token='1234567890', application=self.application, scope='read write',
-                                         expires=timezone.now() + datetime.timedelta(days=1))
-        auth = "Bearer {}".format(tok.token)
+        self.make_token()
 
-        response = self.client.get(reverse('api_eighth_block_list'), HTTP_AUTHORIZATION=auth)
+        response = self.client.get(reverse("api_eighth_block_list"), HTTP_AUTHORIZATION=self.auth)
         self.assertEqual(response.status_code, 200)
 
         # Test a good date
-        response = self.client.get(reverse('api_eighth_block_list')+"?start_date=2019-04-18", HTTP_AUTHORIZATION=auth)
+        response = self.client.get(
+            reverse("api_eighth_block_list") + "?start_date=2019-04-18",
+            HTTP_AUTHORIZATION=self.auth,
+        )
         self.assertEqual(response.status_code, 200)
 
         # Test a bad date
-        response = self.client.get(reverse('api_eighth_block_list')+"?start_date=2019-04-18bad", HTTP_AUTHORIZATION=auth)
+        response = self.client.get(
+            reverse("api_eighth_block_list") + "?start_date=2019-04-18bad",
+            HTTP_AUTHORIZATION=self.auth,
+        )
         self.assertEqual(response.status_code, 400)
--- intranet/apps/users/tests.py
+++ intranet/apps/users/tests.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from io import StringIO
+import datetime
 
 from django.conf import settings
 from django.core.management import call_command
 from django.urls import reverse
+from django.utils import timezone
+
+from oauth2_provider.models import get_application_model, AccessToken
+from oauth2_provider.settings import oauth2_settings
 
 from .models import User, Address
 from ...test.ion_test import IonTestCase
+
+Application = get_application_model()
 
 
 class DynamicGroupTest(IonTestCase):
@@ -16,30 +23,74 @@ class DynamicGroupTest(IonTestCase):
     def test_dynamic_groups(self):
         out = StringIO()
         with self.settings(SENIOR_GRADUATION_YEAR=2016):
-            User.objects.get_or_create(username="awilliam", graduation_year=settings.SENIOR_GRADUATION_YEAR + 1)
-            call_command('dynamic_groups', stdout=out)
+            User.objects.get_or_create(
+                username="awilliam", graduation_year=settings.SENIOR_GRADUATION_YEAR + 1
+            )
+            call_command("dynamic_groups", stdout=out)
         output = [
-            "2016: 0 users", "2016: Processed", "2017: 1 users", "2017: Processed", "2018: 0 users", "2018: Processed", "2019: 0 users",
-            "2019: Processed", "Done."
+            "2016: 0 users",
+            "2016: Processed",
+            "2017: 1 users",
+            "2017: Processed",
+            "2018: 0 users",
+            "2018: Processed",
+            "2019: 0 users",
+            "2019: Processed",
+            "Done.",
         ]
         self.assertEqual(out.getvalue().splitlines(), output)
 
 
 class ProfileTest(IonTestCase):
-
     def setUp(self):
-        user = User.objects.get_or_create(username="awilliam")[0]
-        address = Address.objects.get_or_create(street="6560 Braddock Rd", city="Alexandria", state="VA", postal_code="22312")[0]
-        user.properties._address = address
-        user.properties.save()
-        user.save()
+        self.user = User.objects.get_or_create(username="awilliam")[0]
+        address = Address.objects.get_or_create(
+            street="6560 Braddock Rd", city="Alexandria", state="VA", postal_code="22312"
+        )[0]
+        self.user.properties._address = address
+        self.user.properties.save()
+        self.user.save()
+        self.application = Application(
+            name="Test Application",
+            redirect_uris="http://localhost http://example.com http://example.it",
+            user=self.user,
+            client_type=Application.CLIENT_CONFIDENTIAL,
+            authorization_grant_type=Application.GRANT_AUTHORIZATION_CODE,
+        )
+        self.application.save()
+
+        self.client_credentials_application = Application(
+            name="Test Client Credentials Application",
+            redirect_uris="http://localhost http://example.com http://example.it",
+            user=self.user,
+            client_type=Application.CLIENT_CONFIDENTIAL,
+            authorization_grant_type=Application.GRANT_CLIENT_CREDENTIALS,
+        )
+        self.client_credentials_application.save()
+
+        oauth2_settings._SCOPES = ["read", "write"]
+
+    def make_token(self):
+        tok = AccessToken.objects.create(
+            user=self.user,
+            token="1234567890",
+            application=self.application,
+            scope="read write",
+            expires=timezone.now() + datetime.timedelta(days=1),
+        )
+        self.auth = "Bearer {}".format(tok.token)
 
     def test_get_profile(self):
         self.make_admin()
+        self.make_token()
         # Check for non-existant user.
-        response = self.client.get(reverse('api_user_profile_detail', args=[42]))
+        response = self.client.get(
+            reverse("api_user_profile_detail", args=[42]), HTTP_AUTHORIZATION=self.auth
+        )
         self.assertEqual(response.status_code, 404)
         # Get data for ourself.
-        response = self.client.get(reverse('api_user_myprofile_detail'))
+        response = self.client.get(
+            reverse("api_user_myprofile_detail"), HTTP_AUTHORIZATION=self.auth
+        )
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()['address']['postal_code'], '22312')
+        self.assertEqual(response.json()["address"]["postal_code"], "22312")
