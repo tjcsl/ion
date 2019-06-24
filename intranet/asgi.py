2175474105e5717aab55f156c80b8c786a7ac42b
Theo Ouzhinski <touzhinski@gmail.com>
2019-06-24 19:21:57 -0400

    docs: document channels files

--- intranet/asgi.py
+++ intranet/asgi.py
@@ -1,5 +1,9 @@
 import os
 import channels.asgi
 
+"""
+Sets up application for channels
+"""
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "intranet.settings")
 channel_layer = channels.asgi.get_channel_layer()
--- intranet/routing.py
+++ intranet/routing.py
@@ -1,4 +1,9 @@
 from channels.routing import route_class
 from .apps.bus.consumers import BusConsumer
 
+"""
+Defines routes for channels
+https://channels.readthedocs.io/en/latest/topics/routing.html
+"""
+
 channel_routing = [route_class(BusConsumer, path=r"^/bus/")]
