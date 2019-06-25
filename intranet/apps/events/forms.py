59a80715afcdd5b2ec7f5409cb5fb01dbae9aeb0
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-06-25 14:36:55 -0400

    refactor: improve event and poll forms

--- intranet/apps/events/forms.py
+++ intranet/apps/events/forms.py
@@ -6,7 +6,7 @@ from ..groups.models import Group
 
 
 class EventForm(forms.ModelForm):
-    def __init__(self, all_groups=False, *args, **kwargs):
+    def __init__(self, *args, all_groups=False, **kwargs):
         super(EventForm, self).__init__(*args, **kwargs)
         if not all_groups:
             self.fields["groups"].queryset = Group.objects.student_visible()
@@ -19,30 +19,24 @@ class EventForm(forms.ModelForm):
 
     class Meta:
         model = Event
-        exclude = [
-            "added",
-            "updated",
-            "user",
-            "scheduled_activity",
-            "announcement",
-            "attending",
-            "links",
-            "approved",
-            "rejected",
-            "approved_by",
-            "rejected_by",
+        fields = [
+            "title", "description", "time", "location", "groups", "show_attending",
+            "show_on_dashboard", "public", "category", "open_to",
         ]
 
 
 class AdminEventForm(forms.ModelForm):
-    def __init__(self, all_groups=False, *args, **kwargs):
+    def __init__(self, *args, all_groups=False, **kwargs):
         super(AdminEventForm, self).__init__(*args, **kwargs)
         if not all_groups:
             self.fields["groups"].queryset = Group.objects.student_visible()
 
     class Meta:
         model = Event
-        exclude = ["added", "updated", "user", "attending", "links", "approved", "rejected", "approved_by", "rejected_by"]
+        fields = [
+            "title", "description", "time", "location", "scheduled_activity", "announcement",
+            "groups", "show_attending", "show_on_dashboard", "public", "category", "open_to",
+        ]
         widgets = {"scheduled_activity": forms.NumberInput(), "announcement": forms.NumberInput()}
         help_texts = {
             "scheduled_activity": "OPTIONAL: Enter the ID of the scheduled activity -- it will be displayed above the event.",
--- intranet/apps/polls/forms.py
+++ intranet/apps/polls/forms.py
@@ -1,5 +1,3 @@
-from typing import List  # noqa
-
 from django import forms
 
 from .models import Poll
@@ -8,11 +6,6 @@ from ...utils.html import safe_html
 
 
 class PollForm(forms.ModelForm):
-
-    def __init__(self, all_groups=False, *args, **kwargs):
-        super(PollForm, self).__init__(*args, **kwargs)
-        self.fields["description"].widget = forms.Textarea()
-
     def clean_description(self):
         desc = self.cleaned_data["description"]
         # SAFE HTML
@@ -21,4 +14,7 @@ class PollForm(forms.ModelForm):
 
     class Meta:
         model = Poll
-        exclude = []  # type: List[str]
+        fields = ["title", "description", "start_time", "end_time", "visible", "groups"]
+        widgets = {
+            "description": forms.Textarea(),
+        }
