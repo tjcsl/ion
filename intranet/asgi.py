712e17664591336b3f9bff3f3dac3b4c7bd1ee3d
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-06-25 13:05:27 -0400

    style: move comments that are obviously module docstrings to the top

--- intranet/asgi.py
+++ intranet/asgi.py
@@ -1,9 +1,8 @@
-import os
-import channels.asgi
-
 """
 Sets up application for channels
 """
+import os
+import channels.asgi
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "intranet.settings")
 channel_layer = channels.asgi.get_channel_layer()
--- intranet/routing.py
+++ intranet/routing.py
@@ -1,9 +1,8 @@
-from channels.routing import route_class
-from .apps.bus.consumers import BusConsumer
-
 """
 Defines routes for channels
 https://channels.readthedocs.io/en/latest/topics/routing.html
 """
+from channels.routing import route_class
+from .apps.bus.consumers import BusConsumer
 
 channel_routing = [route_class(BusConsumer, path=r"^/bus/")]
