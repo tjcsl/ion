b0a6b9f6c517700264b660e2d41a4b80eaface6c
Theo Ouzhinski <touzhinski@gmail.com>
2019-06-25 19:29:39 -0400

    chore: put widgets & help_text in meta
    for AdminEventForm

--- intranet/apps/events/forms.py
+++ intranet/apps/events/forms.py
@@ -39,11 +39,12 @@ class AdminEventForm(forms.ModelForm):
         super(AdminEventForm, self).__init__(*args, **kwargs)
         if not all_groups:
             self.fields["groups"].queryset = Group.objects.student_visible()
-        self.fields["scheduled_activity"].widget = forms.NumberInput()
-        self.fields["scheduled_activity"].help_text = "OPTIONAL: Enter the ID of the scheduled activity -- it will be displayed above the event."
-        self.fields["announcement"].widget = forms.NumberInput()
-        self.fields["announcement"].help_text = "OPTIONAL: Enter the ID of the announcement -- it will be displayed above the event."
 
     class Meta:
         model = Event
         exclude = ["added", "updated", "user", "attending", "links", "approved", "rejected", "approved_by", "rejected_by"]
+        widgets = {"scheduled_activity": forms.NumberInput(), "announcement": forms.NumberInput()}
+        help_texts = {
+            "scheduled_activity": "OPTIONAL: Enter the ID of the scheduled activity -- it will be displayed above the event.",
+            "announcement": "OPTIONAL: Enter the ID of the announcement -- it will be displayed above the event.",
+        }
