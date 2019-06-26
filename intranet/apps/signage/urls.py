fca92fc9cffc9ecb9571653942f4cc1908878ed2
Theo Ouzhinski <touzhinski@gmail.com>
2019-06-26 10:00:26 -0400

    chore: remove unused eighth_signage route

--- intranet/apps/signage/urls.py
+++ intranet/apps/signage/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     url(r"^/serviceworker\.js$", TemplateView.as_view(template_name="signage/serviceworker.js", content_type="text/javascript"),
         name="signage-serviceworker"),
     url(r"^/display/(?P<display_id>[\w_-]+)?$", views.signage_display, name="signage_display"),
-    url(r"^/page/eighth(?:/(?P<block_id>\d+))?$", views.eighth_signage, name="eighth_signage"),
 ]
