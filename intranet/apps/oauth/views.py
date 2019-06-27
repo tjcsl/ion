ee96281822d102c28e3e4da91d2950cb73083b3d
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-06-26 23:23:53 -0400

    chore: disable two pylint warnings about too many ancestors
    Both cases are beyond our control

--- intranet/apps/oauth/views.py
+++ intranet/apps/oauth/views.py
@@ -1,5 +1,5 @@
 from oauth2_provider.views.application import ApplicationUpdate
 
 
-class ApplicationUpdateView(ApplicationUpdate):
+class ApplicationUpdateView(ApplicationUpdate):  # pylint: disable=too-many-ancestors; Beyond our control
     fields = ["name", "client_id", "client_secret", "client_type", "authorization_grant_type", "redirect_uris"]
