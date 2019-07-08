991794d9f5dc8bd8fe442af468cb6a4540cedea2
Theo Ouzhinski <touzhinski@gmail.com>
2019-07-08 18:38:08 -0400

    style: blackify welcome/views.py

--- intranet/apps/welcome/views.py
+++ intranet/apps/welcome/views.py
@@ -18,6 +18,7 @@ def teacher_welcome_view(request):
     context = {"first_login": request.session["first_login"] if "first_login" in request.session else False}
     return render(request, "welcome/teacher.html", context)
 
+
 @login_required
 def welcome_view(request):
     if request.user.is_teacher:
