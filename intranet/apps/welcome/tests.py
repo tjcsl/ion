bf99f988da8e27782d55ac9b9256e91d95a91621
Theo Ouzhinski <touzhinski@gmail.com>
2019-07-08 18:41:27 -0400

    fix(welcome): readd welcome_done

---
+++ intranet/apps/welcome/tests.py
@@ -0,0 +1,26 @@
+from django.urls import reverse
+
+from ...test.ion_test import IonTestCase
+
+
+class WelcomeTest(IonTestCase):
+    """Tests for the welcome module."""
+
+    def test_welcome_view_teacher(self):
+        user = self.login()
+        user.user_type = "teacher"
+        user.save()
+        response = self.client.get(reverse("welcome"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["first_login"], False)
+        self.assertTemplateUsed(response, "welcome/teacher.html")
+
+    def test_welcome_view_student(self):
+        user = self.login()
+        response = self.client.get(reverse("welcome"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "welcome/student.html")
+
+    def test_welcome_done(self):
+        user = self.login()
+        response = self.client.get(revese("done"))
--- intranet/apps/welcome/views.py
+++ intranet/apps/welcome/views.py
@@ -27,3 +27,9 @@ def welcome_view(request):
         return student_welcome_view(request)
     else:
         return redirect("index")
+
+@login_required
+def done_welcome_view(request):
+    request.user.seen_welcome = True
+    request.user.save()
+    return redirect("index")
