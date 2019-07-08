ca0a082e89f63ce8d4e8ac92d86e59229cafaaa1
Theo Ouzhinski <touzhinski@gmail.com>
2019-07-08 18:22:52 -0400

    chore: remove welcome_done view

--- intranet/apps/welcome/views.py
+++ intranet/apps/welcome/views.py
@@ -10,27 +10,13 @@ logger = logging.getLogger(__name__)
 @login_required
 def student_welcome_view(request):
     """Welcome/first run page for students."""
-    if not request.user.is_student:
-        return redirect("index")
-    # context = {"first_login": request.session["first_login"] if "first_login" in request.session else False}
-    # return render(request, "welcome/old_student.html", context)
     return dashboard_view(request, show_welcome=True)
 
 
 @login_required
 def teacher_welcome_view(request):
-    # if not request.is_teacher:
-    #   return redirect("index")
     context = {"first_login": request.session["first_login"] if "first_login" in request.session else False}
     return render(request, "welcome/teacher.html", context)
-
-
-@login_required
-def done_welcome_view(request):
-    request.user.seen_welcome = True
-    request.user.save()
-    return redirect("index")
-
 
 @login_required
 def welcome_view(request):
