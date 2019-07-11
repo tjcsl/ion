04dbbc14ed908dc85bc56f67f8d79f047103f280
Theo Ouzhinski <touzhinski@gmail.com>
2019-07-11 17:55:40 -0400

    chore(users): remove unused serializers

--- intranet/apps/users/serializers.py
+++ intranet/apps/users/serializers.py
@@ -5,27 +5,9 @@ from rest_framework import serializers
 from .models import Address
 
 
-class UserListSerializer(serializers.Serializer):
-    full_name = serializers.CharField(max_length=63)
-    short_name = serializers.CharField(max_length=31)
-    # display_name = serializers.
-    ion_id = serializers.IntegerField()
-    ion_username = serializers.CharField(max_length=15)
-
-    email = serializers.StringRelatedField(many=True)
-    content = serializers.CharField(max_length=200)
-    created = serializers.DateTimeField()
-
-
 class GradeSerializer(serializers.Serializer):
     number = serializers.IntegerField()
     name = serializers.CharField(max_length=20)
-
-
-class SubClassSerializer(serializers.Serializer):
-    section_id = serializers.CharField(max_length=500)
-    url = serializers.HyperlinkedIdentityField(view_name="api_user_class_detail")
-    name = serializers.CharField(max_length=500)
 
 
 class AddressSerializer(serializers.ModelSerializer):
@@ -125,14 +107,3 @@ class UserSerializer(serializers.ModelSerializer):
             "is_student",
             "absences",
         )
-
-
-class ClassSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=300)
-    class_id = serializers.CharField(max_length=20)
-    room_number = serializers.CharField(max_length=100)
-    course_length = serializers.IntegerField()
-    periods = serializers.ListField(child=serializers.IntegerField())
-    quarters = serializers.ListField(child=serializers.IntegerField())
-    teacher = CounselorTeacherSerializer()
-    students = StudentSerializer(many=True)
