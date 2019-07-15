688ed64c547a3a82f99015a39e9494ed6237d389
Theo Ouzhinski <touzhinski@gmail.com>
2019-07-15 12:51:59 -0400

    chore(pylint): disable protected-access

--- intranet/apps/users/tests.py
+++ intranet/apps/users/tests.py
@@ -137,7 +137,7 @@ class ProfileTest(IonTestCase):
         # Get data for ourself.
         response = self.client.get(reverse("api_user_myprofile_detail"), HTTP_AUTHORIZATION=self.auth)
         self.assertEqual(response.status_code, 200)
-        address = self.user.properties._address
+        address = self.user.properties._address # pylint: disable=protected-access
         self.assertEqual(response.json()["address"]["postal_code"], address.postal_code)
         self.assertEqual(response.json()["address"]["street"], address.street)
         self.assertEqual(response.json()["address"]["city"], address.city)
