b6efc99cde85e271bbbe76ca73f20434fcdb155e
Theo Ouzhinski <touzhinski@gmail.com>
2019-07-20 18:23:11 -0400

    style: blackify context_processors.py

--- intranet/apps/context_processors.py
+++ intranet/apps/context_processors.py
@@ -15,12 +15,12 @@ logger = logging.getLogger(__name__)
 
 def ion_base_url(request):
     """Return the base URL through request.build_absolute_uri for the index page."""
-    return {"ion_base_url": request.build_absolute_uri('/')}
+    return {"ion_base_url": request.build_absolute_uri("/")}
 
 
 def global_warning(request):
     """Display a global warning on all pages throughout the application."""
-    warning = settings.GLOBAL_WARNING if hasattr(settings, 'GLOBAL_WARNING') else None
+    warning = settings.GLOBAL_WARNING if hasattr(settings, "GLOBAL_WARNING") else None
 
     return {"global_warning": warning}
 
@@ -30,9 +30,19 @@ def nav_categorizer(request):
     falls under
     """
 
-    categories = [(r"^/$", "dashboard"), (r"^/announcements", "dashboard"), (r"^/eighth/admin", "eighth_admin"), (r"^/eighth", "eighth"),
-                  (r"^/events", "events"), (r"^/files", "files"), (r"^/printing", "printing"), (r"^/groups", "groups"), (r"^/polls", "polls"),
-                  (r"^/board", "board"), (r"/bus", "bus")]
+    categories = [
+        (r"^/$", "dashboard"),
+        (r"^/announcements", "dashboard"),
+        (r"^/eighth/admin", "eighth_admin"),
+        (r"^/eighth", "eighth"),
+        (r"^/events", "events"),
+        (r"^/files", "files"),
+        (r"^/printing", "printing"),
+        (r"^/groups", "groups"),
+        (r"^/polls", "polls"),
+        (r"^/board", "board"),
+        (r"/bus", "bus"),
+    ]
 
     for pattern, category in categories:
         p = re.compile(pattern)
@@ -47,7 +57,7 @@ def mobile_app(request):
 
     ctx = {}
     try:
-        ua = request.META.get('HTTP_USER_AGENT', '')
+        ua = request.META.get("HTTP_USER_AGENT", "")
 
         if "IonAndroid: gcmFrame" in ua:
             logger.debug("IonAndroid %s", request.user)
@@ -94,15 +104,15 @@ def global_custom_theme(request):
 
 def show_homecoming(request):
     """Show homecoming ribbon / scores """
-    return {'show_homecoming': settings.HOCO_START_DATE < datetime.date.today() and datetime.date.today() < settings.HOCO_END_DATE}
+    return {"show_homecoming": settings.HOCO_START_DATE < datetime.date.today() and datetime.date.today() < settings.HOCO_END_DATE}
 
 
 def _get_current_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',', 1)[0]
+        ip = x_forwarded_for.split(",", 1)[0]
     else:
-        ip = request.META.get('REMOTE_ADDR')
+        ip = request.META.get("REMOTE_ADDR")
 
     return ip
 
@@ -119,17 +129,17 @@ def show_bus_button(request):
     window = datetime.timedelta(hours=1)
     today = Day.objects.today()
     if settings.ENABLE_BUS_DRIVER:
-        return {'show_bus_nav': True}
+        return {"show_bus_nav": True}
     try:
         if today is None or today.day_type.no_school:
-            return {'show_bus_nav': is_bus_admin and settings.ENABLE_BUS_APP}
+            return {"show_bus_nav": is_bus_admin and settings.ENABLE_BUS_APP}
 
         end_of_day = Day.objects.today().end_time.date_obj(now.date())
         is_valid_time = end_of_day - window < now < end_of_day + window
-        return {'show_bus_nav': (is_bus_admin or is_valid_time) and settings.ENABLE_BUS_APP}
+        return {"show_bus_nav": (is_bus_admin or is_valid_time) and settings.ENABLE_BUS_APP}
 
     except AttributeError:
-        return {'show_bus_nav': is_bus_admin and settings.ENABLE_BUS_APP}
+        return {"show_bus_nav": is_bus_admin and settings.ENABLE_BUS_APP}
 
 
 def enable_dark_mode(request):
