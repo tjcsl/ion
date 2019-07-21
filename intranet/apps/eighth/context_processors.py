b56aafc4ac06b39477b6fcc99f163438d59a1d63
Theo Ouzhinski <touzhinski@gmail.com>
2019-07-20 20:55:39 -0400

    refactor(eighth): refactor eighth absence context
    processor

--- intranet/apps/eighth/context_processors.py
+++ intranet/apps/eighth/context_processors.py
@@ -41,12 +41,8 @@ def absence_count(request):
         absence_info = request.user.absence_info()
         num_absences = absence_info.count()
         show_notif = False
-        if num_absences > 0:
-            notif_seen = request.session.get("eighth_absence_notif_seen", False)
-            if not notif_seen:
-                for signup in absence_info:
-                    if signup.in_clear_absence_period():
-                        show_notif = True
+        if num_absences and not request.session.get("eighth_absence_notif_seen", False):
+            show_notif = any(signup.in_clear_absence_period() for signup in absence_info)
 
             if show_notif:
                 request.session["eighth_absence_notif_seen"] = True
