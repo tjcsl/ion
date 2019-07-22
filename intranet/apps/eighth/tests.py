7b51fb52d6dac976b6b3a41d3cf86da83c1596e4
Theo Ouzhinski <touzhinski@gmail.com>
2019-07-22 14:41:49 -0400

    fix(eighth): sort errors in SignupException

--- intranet/apps/eighth/tests.py
+++ intranet/apps/eighth/tests.py
@@ -505,15 +505,15 @@ class EighthExceptionTest(IonTestCase):
         # Test SignupException messages
         expected_messages_no_admin = []
         expected_messages_admin = []
-        for error in ["SignupForbidden", "ScheduledActivityCancelled"]:
+        for error in ["ScheduledActivityCancelled", "SignupForbidden"]:
             expected_messages_no_admin.append(SignupException._messages[error][0])  # pylint: disable=protected-access
             expected_messages_admin.append(SignupException._messages[error][1])  # pylint: disable=protected-access
-        self.assertCountEqual(signup_exception.messages(), expected_messages_no_admin)
-        self.assertCountEqual(signup_exception.messages(admin=True), expected_messages_admin)
+        self.assertEqual(signup_exception.messages(), expected_messages_no_admin)
+        self.assertEqual(signup_exception.messages(admin=True), expected_messages_admin)
         response_plain = signup_exception.as_response(html=False)
         self.assertEqual(response_plain.content.decode(), "\n".join(expected_messages_no_admin))
         self.assertEqual(response_plain["Content-Type"], "text/plain")
 
         # Test string representations
-        self.assertEqual(str(signup_exception), "SignupForbidden, ScheduledActivityCancelled")
+        self.assertEqual(str(signup_exception), "ScheduledActivityCancelled, SignupForbidden")
         self.assertEqual(repr(signup_exception), "SignupException({})".format(str(signup_exception)))
