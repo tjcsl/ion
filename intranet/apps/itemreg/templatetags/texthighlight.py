cf06b86e9d4068ff4244c7988fac95870e25047b
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-07-26 10:34:02 -0400

    fix(itemreg): fix itemreg search IndexErrors

--- intranet/apps/itemreg/templatetags/texthighlight.py
+++ intranet/apps/itemreg/templatetags/texthighlight.py
@@ -6,12 +6,10 @@ register = template.Library()
 @register.filter
 def highlight(str1, str2):
     """Highlight str1 with the contents of str2."""
-    print('------------------------------')
-    try:
+    if isinstance(str2, list):
         str2 = str2[0]
-    except IndexError:
-        str2 = None
-    if str1 and str2:
+
+    if str1 and str2 and isinstance(str2, str):
         return str1.replace(str2, "<b>{}</b>".format(str2))
     else:
         return str1
