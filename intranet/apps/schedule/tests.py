c8af9a2bc9b5a3fa254e227cad8d1fcc84e7d414
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-07-31 21:06:49 -0400

    fix(schedule): fix broken schedule test
    I think one of the fixes in #708 broke this test, but only because both
    the test and the method were broken in the same way

--- intranet/apps/schedule/tests.py
+++ intranet/apps/schedule/tests.py
@@ -10,7 +10,7 @@ class ScheduleTest(IonTestCase):
     def test_day(self):
         snow_daytype = DayType.objects.get_or_create(name="No School -- Snow Day", special=True)[0]
 
-        day = Day.objects.get_or_create(date=timezone.now().date(), day_type=snow_daytype)[0]
+        day = Day.objects.get_or_create(date=timezone.localdate(), day_type=snow_daytype)[0]
 
         # Test Snow Days
         self.assertEqual(Day.objects.today(), day)
