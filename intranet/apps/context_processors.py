tjcsl/ion

commit 55a870be564533e4d8e9fe70e7790ba6a88e3c2a

refactor(oauth): show apps in authorized_tokens view
Fixes #527

intranet/apps/context_processors.py
@@ -4,7 +4,9 @@ import re
 import binascii
 import os
 
+from django.core.urlresolvers import resolve
 from django.conf import settings
+from oauth2_provider.models import Application
 
 from intranet.apps.notifications.models import NotificationConfig
 from .schedule.models import Day
@@ -144,3 +146,14 @@ def show_bus_button(request):
 
 def enable_dark_mode(request):
     return {"dark_mode_enabled": dark_mode_enabled(request)}
+
+
+def oauth_toolkit(request):
+    if request.user.is_authenticated:
+        resolve_match = resolve(request.path)
+        if resolve_match.namespaces == ["oauth2_provider"] and resolve_match.url_name == "authorized-token-list":
+            applications_tokens = [(application, application.accesstoken_set.filter(user=request.user))
+                                   for application in Application.objects.filter(accesstoken__user=request.user).distinct()]
+            return {"applications_tokens": applications_tokens}
+
+    return {}

intranet/settings/__init__.py
@@ -338,6 +338,7 @@ TEMPLATES = [
                 "intranet.apps.context_processors.global_custom_theme",  # Sitewide custom themes (special events, etc)
                 "intranet.apps.context_processors.show_bus_button",
                 "intranet.apps.context_processors.enable_dark_mode",
+                "intranet.apps.context_processors.oauth_toolkit",  # Django OAuth Toolkit-related middleware
             ),
             "debug": True,  # Only enabled if DEBUG is true as well
             "loaders": ("django.template.loaders.filesystem.Loader", "django.template.loaders.app_directories.Loader"),

intranet/templates/oauth2_provider/authorized-tokens.html
@@ -5,16 +5,22 @@
     <div class="block-center">
     <h1>{% trans "Tokens" %}</h1>
         <ul>
-        {% for authorized_token in authorized_tokens %}
-            <li>
-                {{ authorized_token.application }}
-                (<a href="{% url 'oauth2_provider:authorized-token-delete' authorized_token.pk %}">revoke</a>)
+        {% for application, authorized_tokens in applications_tokens %}
+            <li>{{ application }}
+                <ul>
+                {% for authorized_token in authorized_tokens %}
+                    <li>
+                        Token #{{ authorized_token.id }}
+                        (<a href="{% url 'oauth2_provider:authorized-token-delete' authorized_token.pk %}">revoke</a>)
+                        <ul>
+                        {% for scope_name, scope_description in authorized_token.scopes.items %}
+                            <li>{{ scope_name }}: {{ scope_description }}</li>
+                        {% endfor %}
+                        </ul>
+                    </li>
+                {% endfor %}
+                </ul>
             </li>
-            <ul>
-            {% for scope_name, scope_description in authorized_token.scopes.items %}
-                <li>{{ scope_name }}: {{ scope_description }}</li>
-            {% endfor %}
-            </ul>
         {% empty %}
             <li>{% trans "There are no authorized tokens yet." %}</li>
         {% endfor %}

commit 468e130d1fac3e7013d3f7460eb7e57562f87f2d

fix: fail gracefully on 404s
Fixes a bug introduced in 55a870b

intranet/apps/context_processors.py
@@ -4,7 +4,7 @@ import re
 import binascii
 import os
 
-from django.core.urlresolvers import resolve
+from django.core.urlresolvers import resolve, Resolver404
 from django.conf import settings
 from django.utils import timezone
 from oauth2_provider.models import Application
@@ -151,10 +151,14 @@ def enable_dark_mode(request):
 
 def oauth_toolkit(request):
     if request.user.is_authenticated:
-        resolve_match = resolve(request.path)
-        if resolve_match.namespaces == ["oauth2_provider"] and resolve_match.url_name == "authorized-token-list":
-            applications_tokens = [(application, application.accesstoken_set.filter(user=request.user))
-                                   for application in Application.objects.filter(accesstoken__user=request.user).distinct()]
-            return {"applications_tokens": applications_tokens}
+        try:
+            resolve_match = resolve(request.path)
+        except Resolver404:
+            pass
+        else:
+            if resolve_match.namespaces == ["oauth2_provider"] and resolve_match.url_name == "authorized-token-list":
+                applications_tokens = [(application, application.accesstoken_set.filter(user=request.user))
+                                       for application in Application.objects.filter(accesstoken__user=request.user).distinct()]
+                return {"applications_tokens": applications_tokens}
 
     return {}