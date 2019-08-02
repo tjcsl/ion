b086442f7a23a29e65dbf090f542fd9ed6adaac5
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-08-02 14:35:28 -0400

    test(error): make 404 error page test check response content

--- intranet/apps/error/tests.py
+++ intranet/apps/error/tests.py
@@ -5,6 +5,10 @@ class ErrorPageTest(IonTestCase):
     """Tests that the error pages (currently just 404 errors) render properly."""
 
     def test_404_page(self):
-        self.assertEqual(self.client.get("/nonexistent").status_code, 404)
+        resp = self.client.get("/nonexistent")
+        self.assertEqual(resp.status_code, 404)
+        self.assertIn(b"The page you requested could not be found.", resp.content)
         self.login()
-        self.assertEqual(self.client.get("/nonexistent").status_code, 404)
+        resp = self.client.get("/nonexistent")
+        self.assertEqual(resp.status_code, 404)
+        self.assertIn(b"The page you requested could not be found.", resp.content)
