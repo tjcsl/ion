tjcsl/ion

commit b208f0489df0f4cfbbafd65adf8120fd9a9bf09c

feat(announcements): block announcements from containing the text "ION"
Closes #805

Also cleans up some of the announcement templates as necessary to make
this change possible

intranet/static/js/announcement.form.js
@@ -24,6 +24,13 @@ $(function() {
             }
         }
 
+        if($("#id_title").val().match(/\bION\b/) || editor.getData().match(/\bION\b/)) {
+            // People frequently write "ION" instead of the correct spelling, "Ion." See https://github.com/tjcsl/ion/issues/805
+            Messenger().error('We have detected the use of "ION" in all caps in your announcement. Please correct it to use "Ion", <a href="/docs/terminology" style="color:#7F7FFF">the official name</a>.');
+            e.preventDefault();
+            return;
+        }
+
         var button = $("button#submit_announcement");
         button.prop("disabled", true);
         button.append(" <i class=\"fa fa-spinner fa-spin\" aria-hidden=\"true\"></i>");

intranet/templates/announcements/approve.html
@@ -77,7 +77,7 @@
         Announcement Approval
     </h2>
 
-    <form action="" method="post">
+    <form action="" method="post" id="announcement_form">
     <table>
     <tr>
         <td colspan="2">
@@ -154,7 +154,7 @@
     {% if not req.posted %}
     <table>
     <tr><td>
-        <input type="submit" name="approve" value="{% if admin_approve %}Post{% else %}Approve{% endif %} Announcement">
+        <button type="submit" name="approve" id="submit_announcement">{% if admin_approve %}Post{% else %}Approve{% endif %} Announcement</button>
         </td>
         <td>
         <input type="submit" name="reject" value="Reject Announcement" onclick="return confirm('This will hide, but not delete, the request.')">

intranet/templates/announcements/request.html
@@ -146,11 +146,11 @@
     <br>
     We reserve the right to edit requests at our discretion (e.g. to correct formatting or spelling errors). Please make sure your post complies with the guidelines.
     <br><br>
-    <form action="" method="post">
+    <form action="" method="post" id="announcement_form">
         <table>
             {% csrf_token %}
             {{ form.as_table }}
-            <tr><td><input type="submit"></td></tr>
+            <tr><td><button type="submit" id="submit_announcement">Submit</button></td></tr>
         </table>
     </form>
 </div>

commit cf3d5cac234001405bf2951684b8e56074a01e4f

fix(announcements): fix bug preventing approval of announcements
Announcement approval was accidentally broken in b208f0489d.

intranet/static/js/announcement.form.js
@@ -32,7 +32,7 @@ $(function() {
         }
 
         var button = $("button#submit_announcement");
-        button.prop("disabled", true);
+        button.click(function(ev) { ev.preventDefault(); })
         button.append(" <i class=\"fa fa-spinner fa-spin\" aria-hidden=\"true\"></i>");
 
         if ($("input#id_title").val() === "") {

intranet/templates/announcements/approve.html
@@ -154,7 +154,7 @@
     {% if not req.posted %}
     <table>
     <tr><td>
-        <button type="submit" name="approve" id="submit_announcement">{% if admin_approve %}Post{% else %}Approve{% endif %} Announcement</button>
+        <button type="submit" name="approve" value="APPROVE" id="submit_announcement">{% if admin_approve %}Post{% else %}Approve{% endif %} Announcement</button>
         </td>
         <td>
         <input type="submit" name="reject" value="Reject Announcement" onclick="return confirm('This will hide, but not delete, the request.')">