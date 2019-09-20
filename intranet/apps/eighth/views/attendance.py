6d1b64e51d03adbc40f7961b0a79bd05e4d10b8b
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-09-20 13:02:56 -0400

    fix(eighth): fix attendance PDF code again

--- intranet/apps/eighth/views/attendance.py
+++ intranet/apps/eighth/views/attendance.py
@@ -615,11 +615,11 @@ def generate_roster_pdf(sched_act_ids):
         elements.append(Table(attendance_data, style=attendance_style, colWidths=[1.3 * inch, None, 0.8 * inch]))
         elements.append(Spacer(0, 15))
         instructions = """
-        <b>Highlight or circle</b> the names of students who are <b>absent</b>, and put an <b>"X"</b> next to those <b>present</b>.<br>
-        If a student arrives and their name is not on the roster, please send them to the <b>8th Period Office</b>.<br>
-        If a student leaves your activity early, please make a note. <b>Do not make any additions to the roster.</b><br>
+        <b>Highlight or circle</b> the names of students who are <b>absent</b>, and put an <b>"X"</b> next to those <b>present</b>.<br/>
+        If a student arrives and their name is not on the roster, please send them to the <b>8th Period Office</b>.<br/>
+        If a student leaves your activity early, please make a note. <b>Do not make any additions to the roster.</b><br/>
         Before leaving for the day, return the roster and any passes to 8th Period coordinator, Catherine Massar's mailbox in the
-        <b>main office</b>. For questions, please call extension 5046 or 5078. Thank you!<br>"""
+        <b>main office</b>. For questions, please call extension 5046 or 5078. Thank you!<br/>"""
 
         elements.append(Paragraph(instructions, styles["Normal"]))
 
