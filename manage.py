b371bba76c4d2365485819bd6231ede63900fd4d
Theo Ouzhinski <touzhinski@gmail.com>
2019-09-21 15:06:10 -0400

    style: blackify manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 import warnings
 
-warnings.simplefilter('default')
+warnings.simplefilter("default")
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "intranet.settings")
