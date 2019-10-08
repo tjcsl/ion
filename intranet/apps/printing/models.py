d9251843906f98384ac896004fa6a2629765d6e3
Theo Ouzhinski <touzhinski@gmail.com>
2019-10-08 18:46:58 -0400

    docs(printing): add docstrings to PrintJob model

--- intranet/apps/printing/models.py
+++ intranet/apps/printing/models.py
@@ -3,6 +3,21 @@ from django.db import models
 
 
 class PrintJob(models.Model):
+    """ Represents a submitted print job to Ion printing.
+
+    Attributes:
+        user (:obj:`User`): The user submitting the job.
+        printer (str): The printer to run the job on.
+        file (File): The file that the user submitted.
+        page_range (str): The page range to print.
+        time (:obj:`datetime.datetime`): The time the job
+            was submitted.
+        printed (bool): Whether the job was printed.
+        num_pages (int): The number of pages in this job.
+            This is calculated after converting the job.
+        duplex (bool): Whether to print duplex.
+        fit (bool): Whether to fit to page.
+    """
     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL)
     printer = models.CharField(max_length=100)
     file = models.FileField(upload_to="printing/")
