afd886cafdd6c6733186c415b9bc32443dd48bbf
Theo Ouzhinski <touzhinski@gmail.com>
2019-10-13 12:21:24 -0400

    chore(schedule): remove unneeded debug log statements

--- intranet/apps/schedule/notifications.py
+++ intranet/apps/schedule/notifications.py
@@ -15,7 +15,6 @@ def period_start_end_data(request):
     ctx = schedule_context(request, use_cache=False, show_tomorrow=False)
     blocks = ctx["sched_ctx"]["blocks"]
     point, block = at_period_point(blocks)
-    logger.debug((point, block))
     if point == 1:
         return {"title": "{} has started".format(block.name), "text": "{}".format(block)}
     elif point == 2:
@@ -28,7 +27,6 @@ def at_period_point(blocks):
     now = timezone.now()
     now = now.replace(second=0, microsecond=0)
     for b in blocks:
-        logger.debug(now, b.start.date_obj(now))
         if now == b.start.date_obj(now):
             return (1, b)
         elif now == b.end.date_obj(now):
