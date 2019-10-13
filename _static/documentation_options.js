6e9cf41bbba830599c677866fa56d6fa8daec06d
Travis <ahamilto+ion@tjhsst.edu>
2019-10-13 01:57:37 +0000

    Travis build 5380

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v3 (Build #5378)',
+    VERSION: 'v3 (Build #5380)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
