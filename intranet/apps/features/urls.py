c00d5ff07b7504fc2eeeaba7f17a222577b3d486
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-11-05 10:21:45 -0500

    refactor: remove Django 1.x-isms

--- intranet/apps/features/urls.py
+++ intranet/apps/features/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = "features"
+
 urlpatterns = [url(r"^/dismiss-announcement/(?P<feat_announcement_id>\d+)$", views.dismiss_feat_announcement_view, name="dismiss_feat_announcement")]
