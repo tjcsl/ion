9f34eae30276dac101924a8cc00ab9d8f25eaec3
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-11-16 15:39:46 -0500

    feat(signage): add signage eighth period page
    Closes #822
    
    Requires the following page configuration:
    - iframe: true
    - url: [base url]/signage/eighth
    - sandbox: false
    - template: [empty]
    - function: [empty]
    - button: <i class="nav-icon eighth-icon"></i>

--- intranet/apps/signage/urls.py
+++ intranet/apps/signage/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     url(r"^/serviceworker\.js$", RedirectView.as_view(url="/static/signage/serviceworker.js"), name="signage-serviceworker"),
     url(r"^/display/(?P<display_id>[\w_-]+)?$", views.signage_display, name="signage_display"),
+    url(r"^/eighth$", views.eighth, name="eighth"),
 ]
