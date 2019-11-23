52baa8fe0e7690c0a2aaa1ff1ab00b403887efd3
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-11-23 13:29:24 -0500

    fix: close incoming websocket connections that don't match a route
    Should actually fix the CancelledErrors mentioned in #891

--- intranet/routing.py
+++ intranet/routing.py
@@ -2,12 +2,27 @@
 Defines routes for channels
 https://channels.readthedocs.io/en/latest/topics/routing.html
 """
+from typing import Optional
 
 from channels.auth import AuthMiddlewareStack
+from channels.generic.websocket import WebsocketConsumer
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 from django.conf.urls import url
 
 from .apps.bus.consumers import BusConsumer
 
-application = ProtocolTypeRouter({"websocket": AuthMiddlewareStack(URLRouter([url(r"^bus/$", BusConsumer)]))})
+
+class WebsocketCloseConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+        self.close()
+
+    def receive(self, text_data: Optional[str] = None, bytes_data: Optional[bytes] = None):
+        pass
+
+    def disconnect(self, code):
+        pass
+
+
+application = ProtocolTypeRouter({"websocket": AuthMiddlewareStack(URLRouter([url(r"^bus/$", BusConsumer), url(r"^.*$", WebsocketCloseConsumer)]))})
