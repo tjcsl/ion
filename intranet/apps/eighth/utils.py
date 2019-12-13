0c08779bdc12d94f7af70291e023708d55124667
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-12-13 14:21:28 -0500

    refactor(eighth): make start date expire daily

--- intranet/apps/eighth/utils.py
+++ intranet/apps/eighth/utils.py
@@ -6,7 +6,7 @@ DATE_FORMAT = "%m-%d-%Y"
 
 
 def get_start_date(request):
-    if "start_date" in request.session:
+    if "start_date" in request.session and request.session.get("start_date_set_date") == timezone.localdate().strftime(DATE_FORMAT):
         date = request.session["start_date"]
         return datetime.strptime(date, DATE_FORMAT).date()
     else:
@@ -17,3 +17,4 @@ def get_start_date(request):
 
 def set_start_date(request, start_date):
     request.session["start_date"] = start_date.strftime(DATE_FORMAT)
+    request.session["start_date_set_date"] = timezone.localdate().strftime(DATE_FORMAT)
