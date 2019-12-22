b634eea30735476170f2f54a411a8845b8596298
Theo Ouzhinski <touzhinski@gmail.com>
2019-12-22 11:43:59 -0500

    feat(dataimport): add photo import script

--- intranet/apps/dataimport/management/commands/import_photos.py
+++ intranet/apps/dataimport/management/commands/import_photos.py
@@ -1,242 +1,56 @@
-import csv
-import json
+#!/usr/bin/env python3
+
+import io
 import os
 import sys
 
+from pathlib import Path
+
+from PIL import Image
+
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
+from intranet.apps.users.models import Photo
 
 class Command(BaseCommand):
-    help = "Import Lifetouch photos into LDAP. Uses the 'TJHSST-Intranet' photo data export format via the Lifetouch Portal."
+    help = "Imports photos from yearbook data export"
 
     def add_arguments(self, parser):
-        parser.add_argument("--pretend", action="store_true", dest="pretend", default=False, help="Don't save files to filesystem")
-        parser.add_argument(
-            "--root", type=str, dest="root_dir", default="/root/photos/1617/DataImages/", help="**absolute** path to outer DataImages folder for LDIF"
-        )
-        parser.add_argument(
-            "--local-root", type=str, dest="local_root_dir", default="/mnt/c/Users/James/DataImages/", help="path to DataImages folder on this host"
-        )
-        parser.add_argument("--grade-offset", type=int, dest="grade_offset", default=0, help="Grade offset, for importing previous year photos")
-        parser.add_argument("--skip-staff", action="store_true", dest="skip_staff", default=False, help="Skip staff")
-
-    def ask(self, q):
-        if input("{} [Yy]: ".format(q)).lower() != "y":
-            print("Abort.")
-            sys.exit()
-
-    def chk(self, q, test):
-        if test:
-            print("OK:", q)
-        else:
-            print("ERROR:", q)
-            print("Abort.")
-            sys.exit()
+        parser.add_argument('directory')
 
     def handle(self, *args, **options):
-        pretend = options["pretend"]  # pylint: disable=attribute-defined-outside-init
-        root_dir = options["root_dir"]  # pylint: disable=attribute-defined-outside-init
-        local_root_dir = options["local_root_dir"]  # pylint: disable=attribute-defined-outside-init
-        grade_offset = options["grade_offset"]  # pylint: disable=attribute-defined-outside-init
-        skip_staff = options["skip_staff"]  # pylint: disable=attribute-defined-outside-init
+        data = []
 
-        print("GRADE OFFSET: ", grade_offset)
-        print("\n".join(["{} => {}".format(i, self.calc_grade_offset(i, grade_offset)) for i in range(9, 13)]))
-        teacher_photo_index = 0
-
-        if os.path.isfile("user_sid_map.json"):
-            users = json.loads(open("user_sid_map.json", "r").read())
-        else:
-            users = {}
-
-            csv_path = "{}data.txt".format(local_root_dir)
-            with open(csv_path, "r") as csv_file:
-                csv_reader = csv.reader(csv_file, delimiter="\t", quotechar='"')
-                next(csv_reader)  # skip header
-                # StudentID, FirstName, LastName, Grade
-                for row in csv_reader:
-                    print(row)
-                    sid, fname, lname, grade = row
-                    grade = self.fix_grade(grade)
-                    if grade == "IGNORE":
-                        # ignore staff with pictures who aren't in LDAP
-                        teacher_photo_index += 1
-                        continue
-
-                    current_grade = self.calc_grade_offset(grade, grade_offset)
-                    if grade != "STA" and (int(current_grade) > 12 or int(current_grade) < 9):
-                        continue
-                    # the CSV includes only "missing-Student ID", but we need the
-                    # filename for each specific missing student id file.
-                    if sid.startswith("missing-"):
-                        sid = self.teacher_photo_name(teacher_photo_index)
-                        teacher_photo_index += 1
-
-                    entry = {"sid": sid, "fname": fname, "lname": lname, "grade": grade, "photo_name": sid}
-                    if grade != "STA":
-                        user_obj = self.get_from_sid(sid)
-                        # entry["user_obj"] = user_obj
-                    else:
-                        user_obj = self.get_staff_name(fname, lname)
-
-                    if not user_obj:
-                        print("INVALID USER OBJECT", entry)
-                        # if grade_offset != 0:
-                        #    continue
-
-                    entry["username"] = user_obj.username
-
-                    users[entry["username"]] = entry
-
-            if not pretend:
-                open("user_sid_map.json", "w").write(json.dumps(users))
-
-        print("Map loaded.")
-
-        ldifs = []
-
-        for uname in users:
-            udata = users[uname]
-            if skip_staff and udata["grade"] == "STA":
+        PHOTO_ROOT_DIRECTORY = options['directory']
+        PHOTO_ROOT_PATH = Path(PHOTO_ROOT_DIRECTORY)
+        sys.stdout.write("Preparing to import photos from directory {}\n".format(PHOTO_ROOT_DIRECTORY))
+        messages = []
+        all_photos = list(PHOTO_ROOT_PATH.glob("*.jpg"))
+        for path in all_photos:
+            try:
+                int(path.stem)
+            except ValueError:
+                print("IGNORING {}".format(path.name))
+                continue
+            sid = path.stem
+            user = get_user_model().objects.user_with_student_id(path.stem)
+            if user is None:
+                print("IGNORING {}".format(path.name))
                 continue
 
-            photo_yr = self.photo_title_year(udata["grade"])
+            grade_number = user.grade.number
+            img = Image.open(path)
+            img_arr = io.BytesIO()
+            img.save(img_arr, format="JPEG")
+            value = img_arr.getvalue()
+            message = "Creating photo for {} grade {}".format(user, grade_number)
+            print(message)
+            messages.append(message)
+            photo = Photo.objects.create(user=user, grade_number=grade_number, _binary=value)
+        with open("photos_created.txt", "w") as f:
+            f.write("\n".join(messages))
 
-            if photo_yr:
-                print("OK", uname, udata["grade"], photo_yr, users[uname])
-                ldif = self.add_photo_ldif({"photo": photo_yr, "iodineUid": uname, "path": self.get_photo_path(root_dir, udata["photo_name"])})
-
-                ldifs.append(ldif)
-
-                if udata["grade"] == "STA":
-                    ldif = self.teacher_default_photo_ldif({"iodineUid": uname})
-                    ldifs.append(ldif)
-            else:
-                print("SKIP", uname, udata["grade"], photo_yr)
-
-        if not pretend:
-            open("import_photos.ldif", "w").write("\n\n".join(ldifs))
-
-    def get_from_sid(self, sid):
-        # fix duplicate student ID map in LDAP
-        sid = int(sid)
-        if sid == 1582058:
-            return get_user_model().objects.get(id=33494)
-        if sid == 1163521:
-            return get_user_model().objects.get(id=32391)
-        return get_user_model().objects.user_with_student_id(sid)
-
-    def get_staff_name(self, fname, lname):
-        # ignore subnames in parens; wildcard for abbreviated names
-        fname = fname.split("(")[0] + "*"
-        lname = lname + "*"
-        u = get_user_model().objects.user_with_name(fname, lname)
-        if u:
-            return u
-        # hyphenated vs spaced subnames
-        u = get_user_model().objects.user_with_name(fname, lname.replace(" ", "-"))
-        if u:
-            return u
-        # non-spaced last name
-        u = get_user_model().objects.user_with_name(fname, lname.replace(" ", ""))
-        if u:
-            return u
-        # non-spaced first name
-        u = get_user_model().objects.user_with_name(fname.replace(" ", ""), lname)
-        if u:
-            return u
-        # middle name in first name
-        u = get_user_model().objects.user_with_name(fname.split(" ")[0], lname)
-        if u:
-            return u
-        # cathie => catherine
-        u = get_user_model().objects.user_with_name(fname.lower().replace("ie", "erine"), lname)
-        if u:
-            return u
-        # mike => michael
-        u = get_user_model().objects.user_with_name(fname.lower().replace("ke", "chael"), lname)
-        if u:
-            return u
-        # katrina => katy
-        u = get_user_model().objects.user_with_name(fname.lower().replace("trina", "ty"), lname)
-        if u:
-            return u
-        # kathleen => kathy
-        u = get_user_model().objects.user_with_name(fname.lower().replace("hleen", "hy"), lname)
-        if u:
-            return u
-        # If you got to here without a match... they probably aren't in LDAP.
-        # change "STA" to "IGNORE" in the csv
-        return None
-
-    def calc_grade_offset(self, grade, grade_offset):
-        if grade == "STA":
-            return grade
-        return int(grade) - (grade_offset or 0)
-
-    def fix_grade(self, grade):
-        grade = str(grade)
-        if grade == "NGA":
-            return "STA"
-        if grade[-2:] == "TH":
-            return grade[:-2]
-        if grade[-1:] == "T":
-            return grade[:-1]
-        if grade[-1:] == "H":
-            return grade[:-1]
-
-        return grade
-
-    def photo_title_year(self, grade):
-        gmap = {"9": "freshmanPhoto", "10": "sophomorePhoto", "11": "juniorPhoto", "12": "seniorPhoto", "STA": "freshmanPhoto"}
-        if str(grade) in gmap:
-            if grade != "STA" and int(grade) >= 9 and int(grade) <= 12:
-                return gmap[str(grade)]
-            elif grade == "STA":
-                return gmap[str(grade)]
-
-        return None
-
-    def teacher_photo_name(self, index):
-        if index == 0:
-            return "missing-Student ID"
-        return "missing-Student ID-{}".format(index)
-
-    def get_photo_path(self, root_dir, sid):
-        return "{}images/{}.jpg".format(root_dir, sid)
-
-    def delete_photo_ldif(self, data):
-        ldif = """
-dn: cn={photo},iodineUid={iodineUid},ou=people,dc=tjhsst,dc=edu
-changetype: delete""".format(
-            **data
-        )
-
-        return ldif
-
-    def add_photo_ldif(self, data):
-        ldif = """
-dn: cn={photo},iodineUid={iodineUid},ou=people,dc=tjhsst,dc=edu
-changetype: add
-objectClass: iodinePhoto
-cn: {photo}
-jpegPhoto:< file://{path}""".format(
-            **data
-        )
-
-        return ldif
-
-    def teacher_default_photo_ldif(self, data):
-        ldif = """
-dn: iodineUid={iodineUid},ou=people,dc=tjhsst,dc=edu
-changetype: modify
-replace: preferredPhoto
-preferredPhoto: freshmanPhoto
--
-replace: perm-showpictures-self
-perm-showpictures-self: TRUE""".format(
-            **data
-        )
-
-        return ldif
+        sys.stdout.write("Completed photo import.\n")
