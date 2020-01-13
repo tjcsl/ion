1a7daece2ce4ab8a72c1c2e381797fe33cac3650
anonymoose2 <anonymoose2@users.noreply.github.com>
2020-01-13 18:32:29 -0500

    refactor: django.conf.urls.url -> django.urls.re_path
    I missed some occurrences in 2aa3483adf763da4d3be5fceb5e0723ec068422c.

--- intranet/apps/users/courses_urls.py
+++ intranet/apps/users/courses_urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    url(r"^$", views.all_courses_view, name="all_courses"),
-    url(r"^/room/(?P<room_number>.*)$", views.sections_by_room_view, name="room_sections"),
-    url(r"^/period/(?P<period_number>\d+)$", views.courses_by_period_view, name="period_courses"),
-    url(r"^/course/(?P<course_id>.*)$", views.course_info_view, name="course_sections"),
-    url(r"^/section/(?P<section_id>.*)$", views.section_view, name="section_info"),
+    re_path(r"^$", views.all_courses_view, name="all_courses"),
+    re_path(r"^/room/(?P<room_number>.*)$", views.sections_by_room_view, name="room_sections"),
+    re_path(r"^/period/(?P<period_number>\d+)$", views.courses_by_period_view, name="period_courses"),
+    re_path(r"^/course/(?P<course_id>.*)$", views.course_info_view, name="course_sections"),
+    re_path(r"^/section/(?P<section_id>.*)$", views.section_view, name="section_info"),
 ]
