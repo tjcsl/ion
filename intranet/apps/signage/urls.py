dca6040c6da36a44f56fd7e6e622540e9ad2b406
anonymoose2 <anonymoose2@users.noreply.github.com>
2020-01-21 22:25:23 -0500

    feat(signage): add Prometheus metrics about which signs are online

--- intranet/apps/signage/urls.py
+++ intranet/apps/signage/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # This MUST match the SignageConsumer entry in intranet/routing.py
     re_path(r"^/display/(?P<display_id>[\w_-]+)?$", views.signage_display, name="signage_display"),
     re_path(r"^/eighth$", views.eighth, name="eighth"),
+    re_path(r"^/prometheus-metrics$", views.prometheus_metrics, name="prometheus_metrics"),
 ]
