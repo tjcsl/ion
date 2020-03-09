1982661c5dd5508d6cd01b41b3f86644d220b1ff
anonymoose2 <anonymoose2@users.noreply.github.com>
2020-03-09 11:30:02 -0400

    refactor(emerg): fix links on FCPS emergency announcements
    Also strip unsafe tags while we're at it.

--- intranet/utils/html.py
+++ intranet/utils/html.py
@@ -1,3 +1,4 @@
+import urllib.parse
 from typing import Mapping, Optional, Tuple, Union
 
 import bleach
@@ -54,3 +55,18 @@ def nullify_links(text: str) -> str:
 
     """
     return bleach.linkify(text, [link_removal_callback])
+
+
+def safe_fcps_emerg_html(text: str, base_url: str) -> str:
+    def translate_link_attr(  # pylint: disable=unused-argument
+        attrs: Mapping[Union[str, Tuple[Optional[str]]], str], new: bool = False
+    ) -> Optional[Mapping[Union[str, Tuple[Optional[str]]], str]]:
+        for key in tuple(attrs.keys()):
+            if isinstance(key, tuple) and "href" in key:
+                # Translate links that don't specify a protocol/host
+                # For example, /a/b will be translated to (something like) https://fcps.edu/a/b
+                attrs[key] = urllib.parse.urljoin(base_url, attrs[key])
+
+        return attrs
+
+    return bleach.linkify(bleach.clean(text, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES, styles=ALLOWED_STYLES), [translate_link_attr])
