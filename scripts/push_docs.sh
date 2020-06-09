a3fc5255603197042ae01c8fec6127f36dc5220f
anonymoose2 <anonymoose2@users.noreply.github.com>
2020-06-09 10:15:30 -0400

    ci: switch from Travis to GitHub Actions

--- scripts/push_docs.sh
+++ scripts/push_docs.sh
@@ -1,21 +1,23 @@
+#!/bin/bash
 set -e
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-  echo "Pushing docs to Github Pages"
 
-  git config --global user.email "ahamilto+ion@tjhsst.edu"
-  git config --global user.name "Travis"
+echo "Pushing docs to GitHub Pages"
 
-  git clone --depth=50 --branch=gh-pages https://${GH_TOKEN}@github.com/tjcsl/ion.git gh-pages
-  rm -rf gh-pages/*
-  cd gh-pages
-  cp -R ../build/sphinx/html/* .
-  git add -A .
-  latest=$(git log -1 --pretty=%s|sed "s/Travis build \([0-9]\+\)/\1/")
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
-  if [ "$latest" -ge "$TRAVIS_BUILD_NUMBER" ]; then
-      echo "Not overwriting newer docs."
-  else
-      git push origin gh-pages |& sed s/${GH_TOKEN}/[secure]/g
-      echo "Pushed docs to Github Pages"
-  fi
+git config --global user.email "intranet@tjhsst.edu"
+git config --global user.name "GitHub Actions"
+
+git clone --depth=50 --branch=gh-pages "https://${GH_TOKEN}@github.com/tjcsl/ion.git" gh-pages
+rm -rf gh-pages/*
+cd gh-pages
+cp -R ../build/sphinx/html/* .
+git add -A .
+latest=$(git log -1 --pretty=%s|sed "s/GitHub Actions build \([0-9]\+\)/\1/")
+git commit -m "GitHub Actions build $GITHUB_RUN_NUMBER"
+if [ "$latest" -ge "$GITHUB_RUN_NUMBER" ]; then
+  echo "Not overwriting newer docs."
+else
+  # This will echo the remote URL, which includes the GitHub token.
+  # However, GitHub Actions will redact it automatically.
+  git push origin gh-pages
+  echo "Pushed docs to GitHub Pages"
 fi
