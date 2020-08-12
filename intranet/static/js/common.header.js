d7b6c960f8a14a67ae545e80316a11ce6079813e
dasbundesland <anurag@kamalkumar.com>
2020-08-12 18:23:09 -0400

    feat: declutter nav dropdown menu
    closes #904

--- intranet/static/js/common.header.js
+++ intranet/static/js/common.header.js
@@ -56,10 +56,15 @@ $(function() {
     $(".has-dropdown").click(function(event) {
         var menu_clicked = $(this).children(".dropdown-menu");
         var already_open = $(".dropdown-open .dropdown-menu")
+        var clicked_subgroups = menu_clicked.children(".has-dropdown-subgroup");
 
         if ($(this).hasClass("dropdown-open")) {
-            // Closing open menu
+            // Closing open menu and any open subgroups
             closeMenu(menu_clicked);
+            clicked_subgroups.children(".dropdown-subgroup").hide();
+            clicked_subgroups.children("a.subgroup-arrow-parent").children("i.subgroup-arrow").addClass("fa-caret-down");
+            clicked_subgroups.children("a.subgroup-arrow-parent").children("i.subgroup-arrow").removeClass("fa-caret-left");
+
             $(document).unbind("click tap", clickOutside);
             $(".dropdown-menu").unbind("click", clickInside);
             $(".has-dropdown").removeClass("dropdown-open");
@@ -74,6 +79,14 @@ $(function() {
             $(".has-dropdown").removeClass("dropdown-open");
             menu_clicked.parent().addClass("dropdown-open");
         }
+    });
+
+    $(".has-dropdown-subgroup").click(function(event) {
+        var subgroup = $(this).children(".dropdown-subgroup");
+        var arrow = $(this).children("a.subgroup-arrow-parent").children("i.subgroup-arrow");
+        subgroup.toggle();
+        arrow.toggleClass("fa-caret-down");
+        arrow.toggleClass("fa-caret-left");
     });
 
     arrowPosition = function() {
