1b0a24e62d9cf7d21b080c560130c8bf8466dc3b
Ethan Nguyen <etnguyen03@hotmail.com>
2021-02-09 13:44:36 -0500

    test(bus): test both morning and afternoon views

--- intranet/apps/bus/tests.py
+++ intranet/apps/bus/tests.py
@@ -1,4 +1,9 @@
+import datetime
+from unittest.mock import patch
+
+from django.conf import settings
 from django.urls import reverse
+from django.utils import timezone
 
 from ...test.ion_test import IonTestCase
 from .models import Route
@@ -10,7 +15,25 @@ class BusTest(IonTestCase):
     def test_bus(self):
         self.login()
 
-        self.assertEqual(self.client.get(reverse("bus")).status_code, 200)
+        # Test morning
+        morning_tz = timezone.make_aware(datetime.datetime(3000, 1, 1, hour=settings.BUS_PAGE_CHANGEOVER_HOUR - 1, minute=0, second=0))
+        with patch("django.utils.timezone.localtime", return_value=morning_tz) as m:
+            response = self.client.get(reverse("bus"))
+            self.assertEqual(response.status_code, 200)
+
+            self.assertTemplateUsed(response, template_name="bus/morning.html")
+
+        m.assert_called()
+
+        # Test afternoon
+        afternoon_tz = timezone.make_aware(datetime.datetime(3000, 1, 1, hour=settings.BUS_PAGE_CHANGEOVER_HOUR + 1, minute=0, second=0))
+        with patch("django.utils.timezone.localtime", return_value=afternoon_tz) as m:
+            response = self.client.get(reverse("bus"))
+            self.assertEqual(response.status_code, 200)
+
+            self.assertTemplateUsed(response, template_name="bus/home.html")
+
+        m.assert_called()
 
     def test_routes(self):
         route = Route.objects.get_or_create(route_name="JT-101", bus_number="JT-101")[0]
