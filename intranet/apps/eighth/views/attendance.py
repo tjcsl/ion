tjcsl/ion

commit 5c7b22c41e1eb8b00bfa445178d33bafbd111eba

feat(eighth): allow attendance taking with google meet csv

intranet/apps/eighth/views/attendance.py
@@ -1,7 +1,7 @@
 import csv
+import io
 import logging
 from html import escape
-from io import BytesIO
 
 from cacheops import invalidate_obj
 from formtools.wizard.views import SessionWizardView
@@ -300,6 +300,29 @@ def take_attendance_view(request, scheduled_activity_id):
 
         present_user_ids = list(request.POST.keys())
 
+        if request.FILES["attendance"]:
+            try:
+                csv_file = request.FILES["attendance"].read().decode("utf-8")
+                data = csv.DictReader(io.StringIO(csv_file))
+
+                unfound_users = []
+                for u in data:
+                    name = u["Name"].split(" ")
+                    user = get_user_model().objects.filter(first_name=name[0], last_name=name[1], student_id=u["Email"].split("@")[0])
+                    if len(user) != 1:
+                        unfound_users.append(u["Name"])
+                    else:
+                        present_user_ids.append(user[0])
+                if unfound_users:
+                    messages.success(
+                        request,
+                        "Took attendance from file, but could not find signups for {} who were present according to the Google Meet file.".format(
+                            ", ".join(unfound_users)
+                        ),
+                    )
+            except (csv.Error, ValueError):
+                messages.error(request, "Could not interpret file. Did you upload a Google Meet attendance report without modification?")
+
         csrf = "csrfmiddlewaretoken"
         if csrf in present_user_ids:
             present_user_ids.remove(csrf)
@@ -507,7 +530,7 @@ def generate_roster_pdf(sched_act_ids):
 
     """
 
-    pdf_buffer = BytesIO()
+    pdf_buffer = io.BytesIO()
     h_margin = 1 * inch
     v_margin = 0.5 * inch
     doc = SimpleDocTemplate(pdf_buffer, pagesize=letter, rightMargin=h_margin, leftMargin=h_margin, topMargin=v_margin, bottomMargin=v_margin)

intranet/apps/eighth/tests.py
@@ -1,4 +1,6 @@
+import csv
 import datetime
+import tempfile
 
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -308,6 +310,37 @@ class EighthTest(EighthAbstractTest):
 
         # Make sure activity is marked as attendance taken.
         self.assertTrue(EighthScheduledActivity.objects.get(id=schact1.id).attendance_taken)
+
+    def test_take_attendance_google_meet_csv(self):
+        """ Make sure taking attendence through an uploaded Google Meet file works. """
+        self.make_admin()
+        user1 = get_user_model().objects.create(
+            username="user1", graduation_year=get_senior_graduation_year() + 1, student_id=12345, first_name="Test", last_name="User"
+        )
+
+        block1 = self.add_block(date="3000-11-11", block_letter="A")
+        room1 = self.add_room(name="room1", capacity=1)
+
+        act1 = self.add_activity(name="Test Activity 1")
+        act1.rooms.add(room1)
+
+        schact1 = self.schedule_activity(act1.id, block1.id)
+        schact1.attendance_taken = False
+        schact1.add_user(user1)
+        schact1.save()
+
+        with tempfile.NamedTemporaryFile(mode="w+") as f:
+            writer = csv.DictWriter(f, fieldnames=["Name", "Email"])
+            writer.writeheader()
+            writer.writerow({"Name": "Test User", "Email": "12345@fcpsschools.net"})
+            f.seek(0)
+            self.client.post(reverse("eighth_take_attendance", args=[schact1.id]), {"attendance": f})
+
+        # Make sure attendance has been marked as taken.
+        self.assertTrue(EighthScheduledActivity.objects.get(id=schact1.id).attendance_taken)
+
+        # Make sure EighthSignup object hasn't been marked absent.
+        self.assertFalse(EighthSignup.objects.get(user=user1, scheduled_activity=schact1).was_absent)
 
     def test_take_attendance_cancelled(self):
         """ Make sure students in a cancelled activity are marked as absent when the button is pressed. """

intranet/static/css/eighth.attendance.scss
@@ -102,7 +102,7 @@ form .accept-all-passes {
     margin-bottom: 25px;
     @media (min-width: 900px) {
         position: absolute;
-        left: 650px;
+        left: 750px;
         &.eighth-admin {
             top: 266px;
         }
@@ -113,7 +113,7 @@ form .accept-all-passes {
     @media (min-width: 1100px) {
         &.has-passes {
             position: absolute;
-            left: 650px;
+            left: 750px;
             background: #f0f0f0;
             padding: 0 30px;
 

intranet/templates/eighth/take_attendance.html
@@ -362,7 +362,7 @@
                 {% endif %}
                 </div>
 
-                <form class="attendance-form" action="" method="post">
+                <form class="attendance-form" action="" method="post" enctype="multipart/form-data">
                     {% csrf_token %}
                     <table class="take-attendance-roster{% if not members %} no-members{% endif %}">
                         <thead>
@@ -432,6 +432,13 @@
                             {% endfor %}
                         </tbody>
                         <tfoot class="print-hide">
+                            <tr>
+                                <td colspan="5">
+                                    <br>
+                                    <b>Take Attendance with Google Meet CSV Report:</b>
+                                    <input type="file" name="attendance" accept=".csv">
+                                </td>
+                            </tr>
                             <tr>
                                 <td colspan="5">
                                 {% if no_edit_perm %}

commit 2efd1f02a16461e84bdda24c293daae76d4820ae

fix(eighth): fix bug preventing regular attendance
Introduced in 5c7b22c41e1eb8b00bfa445178d33bafbd111eba

intranet/apps/eighth/views/attendance.py
@@ -300,7 +300,7 @@ def take_attendance_view(request, scheduled_activity_id):
 
         present_user_ids = list(request.POST.keys())
 
-        if request.FILES["attendance"]:
+        if request.FILES.get("attendance"):
             try:
                 csv_file = request.FILES["attendance"].read().decode("utf-8")
                 data = csv.DictReader(io.StringIO(csv_file))
@@ -326,6 +326,9 @@ def take_attendance_view(request, scheduled_activity_id):
         csrf = "csrfmiddlewaretoken"
         if csrf in present_user_ids:
             present_user_ids.remove(csrf)
+
+        if "attendance" in present_user_ids:
+            present_user_ids.remove("attendance")
 
         absent_signups = EighthSignup.objects.filter(scheduled_activity=scheduled_activity).exclude(user__in=present_user_ids)
         absent_signups.update(was_absent=True)