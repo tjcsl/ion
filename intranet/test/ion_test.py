568eedffecff23d3b36f543f24562222e01e6807
Ethan Nguyen <etnguyen03@hotmail.com>
2021-02-14 11:43:24 -0500

    chore(test): add typing for login and make_admin methods

--- intranet/test/ion_test.py
+++ intranet/test/ion_test.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 from ..apps.groups.models import Group
+from ..apps.users.models import User
 
 
 class IonTestCase(TestCase):
@@ -13,14 +14,14 @@ class IonTestCase(TestCase):
     def tearDownClass(cls):
         super().tearDownClass()
 
-    def login(self, username="awilliam"):
+    def login(self, username: str = "awilliam") -> User:
         # We need to add the user to the db before trying to login as them.
         user = get_user_model().objects.get_or_create(username=username)[0]
         with self.settings(MASTER_PASSWORD="pbkdf2_sha256$24000$qp64pooaIEAc$j5wiTlyYzcMu08dVaMRus8Kyfvn5ZfaJ/Rn+Z/fH2Bw="):
             self.client.login(username=username, password="dankmemes")
         return user
 
-    def make_admin(self, username="awilliam"):
+    def make_admin(self, username: str = "awilliam") -> User:
         user = self.login(username=username)
         # Make user an eighth admin
         group = Group.objects.get_or_create(name="admin_all")[0]
