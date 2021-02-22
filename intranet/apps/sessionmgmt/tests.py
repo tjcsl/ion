9cb38725e5367391a101a74da28e6b4eed7712f0
Ethan Nguyen <etnguyen03@hotmail.com>
2021-02-22 13:05:06 -0500

    test(sessionmgmt): add session mgmt index and global logout test

--- intranet/apps/sessionmgmt/tests.py
+++ intranet/apps/sessionmgmt/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase  # pylint: disable=unused-import # noqa
+from django.urls import reverse
 
-# Create your tests here.
+from ...test.ion_test import IonTestCase
+
+
+class SessionMgmtTestCase(IonTestCase):
+    def test_index_view(self):
+        self.login()
+        response = self.client.get(reverse("sessionmgmt"))
+        self.assertEqual(200, response.status_code)
+
+    def test_global_logout_view(self):
+        self.login()
+        response = self.client.post(reverse("global_logout"), data={"global_logout": "GLOBAL_LOGOUT"}, follow=True)
+        self.assertEqual(200, response.status_code)
+
+        # A test to see if we're on the login page
+        self.assertIn("Login", str(response.content))
+        self.assertIn("js/login.js", str(response.content))
