70abe0beb276ee00ccd4e9314838376b6934de47
Ethan Nguyen <etnguyen03@hotmail.com>
2021-02-22 12:28:43 -0500

    test(error): add 503 and CSRF tests

--- intranet/apps/error/tests.py
+++ intranet/apps/error/tests.py
@@ -1,3 +1,6 @@
+from django.test import Client
+from django.urls import reverse
+
 from ...test.ion_test import IonTestCase
 
 
@@ -12,3 +15,16 @@ class ErrorPageTest(IonTestCase):
         resp = self.client.get("/nonexistent")
         self.assertEqual(resp.status_code, 404)
         self.assertIn(b"The page you requested could not be found.", resp.content)
+
+    def test_503_page(self):
+        with self.settings(MAINTENANCE_MODE=True):
+            resp = self.client.get(reverse("login"))
+            self.assertEqual(resp.status_code, 503)
+            self.assertIn(b"This site is currently undergoing maintenance", resp.content)
+
+    def test_csrf_page(self):
+        csrf_client = Client(enforce_csrf_checks=True)
+
+        resp = csrf_client.post(reverse("login"), data={"sdlfkjsf": "sdlfjsdlfsd"})
+        self.assertEqual(resp.status_code, 403)
+        self.assertIn(b"If making a scripted request", resp.content)
