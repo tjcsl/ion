d7a792b66dbc7e383f31f6a0cfb8a4df3242be84
lauren <pmd7211@gmail.com>
2021-04-30 19:19:38 -0400

    fix(eighth): catch backslashes in activity scheduling

--- intranet/apps/eighth/forms/admin/scheduling.py
+++ intranet/apps/eighth/forms/admin/scheduling.py
@@ -50,3 +50,12 @@ class ScheduledActivityForm(forms.ModelForm):
             "comments": forms.Textarea(attrs={"rows": 2, "cols": 30}),
             "admin_comments": forms.Textarea(attrs={"rows": 2, "cols": 30}),
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        cleaned_data["title"] = cleaned_data["title"].replace("\\", "/")
+        cleaned_data["comments"] = cleaned_data["comments"].replace("\\", "/")
+        cleaned_data["admin_comments"] = cleaned_data["admin_comments"].replace("\\", "/")
+
+        return cleaned_data
