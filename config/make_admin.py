39e52f163030928cb69432a887185846cdda6fd9
NotFish232 <99909994+NotFish232@users.noreply.github.com>
2022-04-08 12:05:09 -0400

    reformatted

--- config/make_admin.py
+++ config/make_admin.py
@@ -1,10 +1,10 @@
+from intranet.apps.groups.models import Group
+from intranet.apps.users.models import User
 import os
 import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "intranet.settings")
 django.setup()
-from intranet.apps.users.models import User
-from intranet.apps.groups.models import Group
 
 USERNAME = "<YOURUSERNAME>"
 if not User.objects.filter(username=USERNAME).exists():
