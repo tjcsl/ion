490617d0d3d123a6799341f7c179ace6dbdb0a9d
lauren <pmd7211@gmail.com>
2022-08-05 10:00:02 -0400

    fix(oauth): don't require PKCE

--- intranet/settings/__init__.py
+++ intranet/settings/__init__.py
@@ -526,6 +526,7 @@ OAUTH2_PROVIDER = {
     "SCOPES": {"read": "Read scope", "write": "Write scope"},
     # OAuth refresh tokens expire in 30 days
     "REFRESH_TOKEN_EXPIRE_SECONDS": 60 * 60 * 24 * 30,
+    "PKCE_REQUIRED": False
 }
 OAUTH2_PROVIDER_APPLICATION_MODEL = "oauth.CSLApplication"
 
