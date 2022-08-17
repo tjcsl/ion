3f3079dd27467c6ff833066d6e8030528270fd79
Alan Zhu <90356452+alanzhu1@users.noreply.github.com>
2022-08-17 17:48:45 -0400

    refactor: focus searchbar on keydown and better tab nav

--- intranet/static/js/dashboard/common.js
+++ intranet/static/js/dashboard/common.js
@@ -3,10 +3,19 @@ $(function() {
     $('.dash-warning').click(function (e) {
         $(this).toggleClass('collapsed');
     });
+
     $('.widget.extra-widgets-show').click(function() {
         $('body').addClass('show-extra-widgets');
     });
+
     $(".new-feature-close").click(function(e) {
         $(e.target).closest(".new-feature").hide("slow");
+    });
+
+    $("body").on("keydown", function(e) {
+        if(e.keyCode === 9) {
+            e.preventDefault();
+        }
+        $("#searchbox").focus();
     });
 });
