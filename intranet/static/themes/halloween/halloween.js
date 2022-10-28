aaf48a6faaeca5e39230c9f59128ae97ac9cb2e7
Alan Zhu <2025azhu@tjhsst.edu>
2022-10-28 08:01:06 -0400

    fix(themes): halloween bug fixes
    Search bar unclickable on mobile
    Small text-shadow change
    Change mobile meta theme-color attribute to orange
    Optimizations
    Make logout button visible

--- intranet/static/themes/halloween/halloween.js
+++ intranet/static/themes/halloween/halloween.js
@@ -1,13 +1,13 @@
 $(function() {
-    $("head").first().append("<script nomodule src='/static/js/vendor/js.cookie.min.js'></script>");
-    $("head").first().append("<script nomodule src='/static/themes/halloween/halloween-cookie.js'></script>");
     $("<i>").addClass("ghost fas fa-ghost").appendTo($("body"));
     $("<i>").addClass("broom fas fa-broom").appendTo($("body"));
     $("<i>").addClass("hat-wizard fas fa-hat-wizard").appendTo($("body"));
+    $("meta[name=theme-color]").attr("content", "orange");
 
     let background_images = ["default", "login", "bg1", "bg2", "bg3", "bg4", "bg5", "bg6"]
     let random_bg = background_images[Math.floor(Math.random() * background_images.length)];
 
+    $("head").first().append("<script nomodule src='/static/js/vendor/js.cookie.min.js'></script>");
     let seenDefaultBg = Cookies.get("seen-default-halloween-bg") == "1" ? "1" : "0";
     if (seenDefaultBg == "0") {
         Cookies.set("seen-default-halloween-bg", "1", {expires: 7});
@@ -18,4 +18,6 @@ $(function() {
         random_bg = "login";
     }
     $("body").css("background-image", "url(/static/themes/halloween/bg/" + random_bg + ".jpg)");
+
+    $("head").first().append("<script nomodule src='/static/themes/halloween/halloween-cookie.js'></script>");
 });
