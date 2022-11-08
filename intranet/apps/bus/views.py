3a101cfe1a95d383a675828a1c534d5a12f90b2d
Alan Zhu <2025azhu@tjhsst.edu>
2022-11-08 11:28:53 -0500

    fix(bus): mobile zindex, other fixes
    Fix bus map overlaying search bar on mobile
    Set bus status as "not arrived yet" if not arrived 5 min before end time
    Remove unneeded polls stylesheet
    Remove console.log()s

--- intranet/apps/bus/views.py
+++ intranet/apps/bus/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
@@ -5,6 +7,7 @@ from django.shortcuts import render
 from django.utils import timezone
 
 from ..auth.decorators import deny_restricted
+from ..schedule.models import Day
 from .models import Route
 
 
@@ -41,10 +44,20 @@ def afternoon(request, on_home=False):
     if not settings.ENABLE_BUS_APP:
         raise Http404("Bus app not enabled.")
     is_bus_admin = request.user.has_admin_permission("bus")
+
+    now = timezone.localtime()
+    day = Day.objects.today()
+    if day is not None and day.end_time is not None:
+        end_of_day = day.end_time.date_obj(now.date())
+    else:
+        end_of_day = datetime.datetime(now.year, now.month, now.day, settings.SCHOOL_END_HOUR, settings.SCHOOL_END_MINUTE)
+
     ctx = {
         "admin": is_bus_admin,
         "enable_bus_driver": settings.ENABLE_BUS_DRIVER,
         "changeover_time": settings.BUS_PAGE_CHANGEOVER_HOUR,
+        "school_end_hour": end_of_day.hour,
+        "school_end_time": end_of_day.minute,
         "on_home": on_home,
     }
     return render(request, "bus/home.html", context=ctx)
