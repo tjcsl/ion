56771b6123c20a9a9ff5591b30bc1a38eb7501a9
Alan Zhu <2025azhu@tjhsst.edu>
2022-12-11 20:34:35 -0500

    refactor(themes): fix piday, halloween disable on nav
    refactor: display the disable Halloween theme button
    as part of the nav bar on mobile
    fix: only show piday theme on auth page, not on every page

--- intranet/static/themes/halloween/halloween-cookie.js
+++ intranet/static/themes/halloween/halloween-cookie.js
@@ -11,6 +11,23 @@ $(function() {
             "<a style='background: linear-gradient(to bottom, #08080B 0%, #0F0F0F 100%); color: orange !important' class='toggle-halloween-theme btn-link' onclick='toggleHalloweenCookie()'><i class='fas fa-ghost'></i>  &nbsp;Turn On Halloween Theme</a>"
         );
     }
+    if (window.innerWidth < 1000) {
+        $(".toggle-halloween-theme").hide();
+        let navbar = $("ul.nav");
+
+        $(navbar).append($(`
+            <li>
+                <a class='toggle-halloween-theme' onclick='toggleHalloweenCookie()'>
+                    <i class='fas fa-ghost' style="font-size: 16pt; position: relative; top: 3px; left: 6px;"></i>
+                    <span class="halloween-nav-text" style="position: relative; bottom: 9px; left: 15px;">
+                        Turn` + (enabled == 1 ? " Off" : " On") + `
+                        <br>
+                        Halloween Theme
+                    </span>
+                </a>
+            </li>
+        `));
+    }
 });
 
 function toggleHalloweenCookie() {
