b53513e9b91d24f7b122f9eba8c58f9e8eb1836b
Alan Zhu <2025azhu@tjhsst.edu>
2022-12-24 17:50:06 -0500

    feat(api): add AUP and disable access for selected users

--- intranet/apps/auth/rest_permissions.py
+++ intranet/apps/auth/rest_permissions.py
@@ -4,3 +4,8 @@ from rest_framework import permissions
 class DenyRestrictedPermission(permissions.BasePermission):
     def has_permission(self, request, view) -> bool:
         return request.user and request.user.is_authenticated and not request.user.is_restricted
+
+
+class ApiAndOauthPermission(permissions.BasePermission):
+    def has_permission(self, request, view) -> bool:
+        return request.user and request.user.is_authenticated and not request.user.is_restricted and request.user.oauth_and_api_access
