fa651e9454cc82828632a900e34aa9ad4b677737
Alan Zhu <2025azhu@tjhsst.edu>
2022-12-24 13:16:16 -0500

    feat(users): add custom users admin
    custom displays, filters, searching

--- intranet/apps/users/admin.py
+++ intranet/apps/users/admin.py
@@ -2,7 +2,47 @@ from django.contrib import admin
 
 from ..users.models import Course, Section, User, UserProperties
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    # Render is_active using checkmarks or crosses
+    def user_active(self, obj):
+        return obj.is_active
+
+    user_active.boolean = True
+
+    list_display = (
+        "username",
+        "first_name",
+        "middle_name",
+        "last_name",
+        "nickname",
+        "user_type",
+        "is_superuser",
+        "user_active",
+    )
+    list_filter = (
+        "graduation_year",
+        "user_type",
+        "is_superuser",
+        "user_locked",
+        "gender",
+        "receive_news_emails",
+        "receive_eighth_emails",
+        "receive_schedule_notifications",
+        "bus_route",
+        "counselor",
+    )
+    search_fields = (
+        "username",
+        "first_name",
+        "middle_name",
+        "last_name",
+        "nickname",
+        "student_id",
+    )
+
+
+admin.site.register(User, UserAdmin)
 admin.site.register(UserProperties)
 admin.site.register(Course)
 admin.site.register(Section)
