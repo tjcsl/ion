1880fed5c8fe536e4f0dfcd81a09c9e75bdff1af
Alan Zhu <2025azhu@tjhsst.edu>
2023-01-07 23:10:37 -0500

    feat(dev): make dev super fast
    Reduces average page load time from over 5 sec to 0.5 sec.
    Restarts web server on crash (due to python errors while coding).
    Disables DEBUG.
    Uses WhiteNoise for static files.

--- config/docker/entrypoint.sh
+++ config/docker/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 echo "---- Running entrypoint script ----"
 
-echo "Starting web server..."
-echo ""
+echo "Performing pre-startup tasks..."
+export DEBUG=FALSE  # HUGE performance benefits. Manually enable if you need to debug.
+python3 config/docker/entrypoint.py &  # For initial setup
+sass --watch intranet/static/css:intranet/collected_static/css &  # Automatically compile modified scss files
 
+echo "Starting web server..."
 cat << END
+
 ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗
 ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝
 ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗ 
@@ -18,6 +22,12 @@ cat << END
         ██║   ██║   ██║         ██║██║   ██║██║╚██╗██║╚═╝
         ██║   ╚██████╔╝         ██║╚██████╔╝██║ ╚████║██╗
         ╚═╝    ╚═════╝          ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝
+
 END
 
-python3 manage.py runserver 0.0.0.0:8080
+# Wrap the run command in a loop so that it restarts if it crashes, e.g. due to a syntax error
+while :  # while true
+do 
+    python3 manage.py run 0.0.0.0:8080  # Custom run command that skips system checks for performance
+    sleep 1
+done
