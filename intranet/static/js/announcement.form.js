bb48b8e835d378e4dbc7e0e46162bfe8c8ec1fc1
Alan Zhu <2025azhu@tjhsst.edu>
2023-08-17 20:19:53 -0400

    chore: change ION alerts to open link in new tab

--- intranet/static/js/announcement.form.js
+++ intranet/static/js/announcement.form.js
@@ -26,7 +26,7 @@ $(function() {
 
         if($("#id_title").val().match(/\bION\b/) || editor.getData().match(/\bION\b/)) {
             // People frequently write "ION" instead of the correct spelling, "Ion." See https://github.com/tjcsl/ion/issues/805
-            Messenger().error('We have detected the use of "ION" in all caps in your announcement. Please correct it to use "Ion", <a href="/docs/terminology" style="color:#7F7FFF">the official name</a>.');
+            Messenger().error('We have detected the use of "ION" in all caps in your announcement. Please correct it to use "Ion", <a href="/docs/terminology" target="_blank" style="color:#7F7FFF">the official name</a>.');
             e.preventDefault();
             return;
         }
