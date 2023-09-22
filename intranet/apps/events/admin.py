8a5823a071e96dc4f49831e4a68d546ef8ee3fc8
Krishnan Shankar <krishnans2006@gmail.com>
2023-09-21 20:11:55 -0400

    fix(events): fix events admin load timeout

--- intranet/apps/events/admin.py
+++ intranet/apps/events/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Event, Link
 
-admin.site.register([Event, Link])
+
+class EventAdmin(admin.ModelAdmin):
+    raw_id_fields = ("scheduled_activity", "announcement")
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Link)
