9ff96f945541fc7b9846ab1413e79663ff765e26
Alan Zhu <2025azhu@tjhsst.edu>
2024-03-27 13:50:22 -0400

    fix(logs): fix pagination not working with queries

--- intranet/static/js/logs.js
+++ intranet/static/js/logs.js
@@ -1,7 +1,25 @@
+function goToPage(pageNum) {
+    let url = window.location.href;
+    if (url.includes("page=")) {
+        url = url.replace(/page=\d+/, "page=" + pageNum);
+    }
+    else {
+        url = "?page=" + pageNum + "&" + url.split("?")[1] || "";
+    }
+    window.location.href = url;
+}
+
 $(function() {
     $("tr.shade:even").css("background-color", "#e7e7e7");
     $(".raw-json-header, .iframe-container-header").click(function () {
         $(this).next().slideToggle("fast");
         $(this.children[1]).toggleClass("fa-angle-up fa-angle-down");
+    });
+    $(".pagination a").click(function () {
+        goToPage($(this).attr("data-page"));
+    });
+    $(".pagination form").submit(function (e) {
+        e.preventDefault();
+        goToPage($(this).find("input").val());
     });
 });
