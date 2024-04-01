eb811443518c298f2405a952150a517529a6fbd8
Krishnan Shankar <krishnans2006@gmail.com>
2024-04-01 14:13:57 -0400

    refactor(themes): extend april fools 2024 duration

--- intranet/utils/helpers.py
+++ intranet/utils/helpers.py
@@ -239,7 +239,7 @@ def get_theme_name() -> str:
     elif (today.month == 10 and 27 <= today.day <= 31) or (today.month == 11 and today.day == 1):
         return "halloween"
     # Enable when a new idea for April Fools 2024 comes around
-    elif (today.month == 3 and (28 <= today.day <= 31)) or (today.month == 4 and (1 <= today.day <= 3)):
+    elif (today.month == 3 and (30 <= today.day <= 31)) or (today.month == 4 and (1 <= today.day <= 7)):
         return "april_fools"
 
     return None
