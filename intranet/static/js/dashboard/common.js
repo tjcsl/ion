51e5290b026a8e5c9291e75dacf6de0a0803f319
Alan Zhu <90356452+alanzhu0@users.noreply.github.com>
2025-04-20 13:31:28 -0400

    fix: revert refactor: focus searchbar on keydown and better tab nav
    Revert ca3e01723e

--- intranet/static/js/dashboard/common.js
+++ intranet/static/js/dashboard/common.js
@@ -3,17 +3,7 @@ $(function() {
     $('.widget.extra-widgets-show').click(function() {
         $('body').addClass('show-extra-widgets');
     });
-
     $(".new-feature-close").click(function(e) {
         $(e.target).closest(".new-feature").hide("slow");
-    });
-
-    $("body").on("keydown", function(e) {
-        if(e.keyCode === 9) {
-            e.preventDefault();
-        }
-        if(!$(".dashboard-textinput").is(":focus")) {
-            $("#searchbox").focus();
-        }
     });
 });
